cd4e54300e73235fd15b45f016c127e9a0da5bf8
Michael Uno <michael@michaeluno.jp>
2013-04-14 23:46:57 +0900

    Changed the demo plugin version number to 1.0.4.1.

--- admin-page-framework-demo-plugin.php
+++ admin-page-framework-demo-plugin.php
@@ -5,7 +5,7 @@
 	Description: Demonstrates the features of the Admin Page Framework class.
 	Author: Michael Uno
 	Author URI: http://michaeluno.jp
-	Version: 1.0.4
+	Version: 1.0.4.1
 	Requirements: PHP 5.2.4 or above, WordPress 3.2 or above.
 */
 
