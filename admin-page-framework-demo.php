c4279aa0dfb686a403bbd9ed5e41555faced86a2
Michael Uno <michael@michaeluno.jp>
2013-12-24 04:40:37 +0900

    Modified the displaying messages of the basic usage demo class.

--- admin-page-framework-demo.php
+++ admin-page-framework-demo.php
@@ -36,11 +36,20 @@ class APF_BasicUsage extends AdminPageFramework {
 	
 	public function do_apf_first_page() {	// do_ + {page slug}
 		?>
-			<h3><?php _e( 'Page Title', 'admin-page-framework-demo' ); ?></h3>
-			<p><?php _e( 'Hi there!', 'admin-page-framework-demo' ); ?></p>
+			<h3><?php _e( 'do_ + {...} Action Hooks', 'admin-page-framework-demo' ); ?></h3>
+			<p><?php _e( 'Hi there! This text is inserted by the <code>do_{page slug}</code> action hook and the callback method.', 'admin-page-framework-demo' ); ?></p>
 		<?php
-		// $this->oDebug->dumpArray( $this->oProp->aPages );
-		// $this->oDebug->dumpArray( AdminPageFramework_PageLoadInfo_Page::$aClassNames );
+
+	}
+	
+	public function content_apf_second_page( $sContent ) {	// content_ + {page slug}
+		
+		return $sContent 
+			. "<h3>" . __( 'content_ + {...} Filter Hooks', 'admin-page-framework-demo' ) . "</h3>"
+			. "<p>" 
+				. __( 'This message is inserted by the <code>content_{page slug}</code> filter.', 'admin-page-framework-demo' ) 
+			. "</p>";
+		
 	}
 	
 }
