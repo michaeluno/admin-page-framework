1c703770f07ff61774c9e27ed3bb308b431c27fc
Michael Uno <michael@michaeluno.jp>
2014-01-08 14:36:12 +0900

    Fixed a bug that array values were inserted in the generateAttributes() method.

--- class/utility/AdminPageFramework_WPUtility.php
+++ class/utility/AdminPageFramework_WPUtility.php
@@ -335,7 +335,11 @@ class AdminPageFramework_WPUtility extends AdminPageFramework_Utility {
 	 */
 	static public function generateAttributes( array $aAttributes ) {
 		
-		$aAttributes = array_map( 'esc_attr', $aAttributes );
+		foreach( $aAttributes as $sAttribute => &$asProperty ) {
+			if ( is_array( $asProperty ) || is_object( $asProperty ) )
+				unset( $aAttributes[ $sAttribute ] );
+			$asProperty = esc_attr( $asProperty );	 // $aAttributes = array_map( 'esc_attr', $aAttributes );	// this also converts arrays into string value, Array.
+		}		
 		return parent::generateAttributes( $aAttributes );
 		
 	}	
