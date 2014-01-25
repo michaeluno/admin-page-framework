e1d31e09bbf1236ee39d78daf8f80a63fb344c75
Michael Uno <michael@michaeluno.jp>
2014-01-26 02:05:43 +0900

    Fixed a bug that the base styles of the post type class did not load in the post-new.php page.

--- development/head_tag/AdminPageFramework_HeadTag_PostType.php
+++ development/head_tag/AdminPageFramework_HeadTag_PostType.php
@@ -22,7 +22,7 @@ class AdminPageFramework_HeadTag_PostType extends AdminPageFramework_HeadTag_Met
 		// If it's not the post type's post listing page or the taxtonomy page
 		if ( 
 			! (
-				in_array( $GLOBALS['pagenow'], array( 'edit.php', 'edit-tags.php' ) ) 
+				in_array( $GLOBALS['pagenow'], array( 'edit.php', 'edit-tags.php', 'post-new.php' ) ) 
 				&& ( isset( $_GET['post_type'] ) && $_GET['post_type'] == $this->oProp->sPostType )				
 			)
 		) return;	
@@ -60,7 +60,7 @@ class AdminPageFramework_HeadTag_PostType extends AdminPageFramework_HeadTag_Met
 		// If it's not the post type's post listing page
 		if ( 
 			! (
-				in_array( $GLOBALS['pagenow'], array( 'edit.php', 'edit-tags.php' ) ) 
+				in_array( $GLOBALS['pagenow'], array( 'edit.php', 'edit-tags.php', 'post-new.php' ) ) 
 				&& ( isset( $_GET['post_type'] ) && $_GET['post_type'] == $this->oProp->sPostType )				
 			)
 		) return;	
