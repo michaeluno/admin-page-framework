d405b9393584e84698a15b489c691c623a81520c
Michael Uno <michael@michaeluno.jp>
2014-02-12 00:36:26 +0900

    Fixed a bug that the featured image's media selector in post definition page broke when a field type that uses the custom media uplaoder was added. Fixes #69.

--- admin-page-framework-demo.php
+++ admin-page-framework-demo.php
@@ -5,7 +5,7 @@
 	Description: Demonstrates the features of the Admin Page Framework class.
 	Author: Michael Uno
 	Author URI: http://michaeluno.jp
-	Version: 3.0.0b20
+	Version: 3.0.0b21
 	Requirements: PHP 5.2.4 or above, WordPress 3.3 or above.
 */ 
 
