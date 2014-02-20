83bfa8abeb256428109c7c03af86c3edc61db66b
Michael Uno <michael@michaeluno.jp>
2014-02-20 18:05:25 +0900

    Divided the utility classes into smaller classes.

--- development/utility/AdminPageFramework_Utility.php
+++ development/utility/AdminPageFramework_Utility.php
@@ -9,36 +9,8 @@ if ( ! class_exists( 'AdminPageFramework_Utility' ) ) :
  * @subpackage		Utility
  * @internal
  */
-abstract class AdminPageFramework_Utility extends AdminPageFramework_Utility_Array {
-	
-	/**
-	 * Converts non-alphabetic characters to underscore.
-	 * 
-	 * @access			public
-	 * @since			2.0.0
-	 * @remark			it must be public 
-	 * @return			string|null			The sanitized string.
-	 */ 
-	public static function sanitizeSlug( $sSlug ) {
-		return is_null( $sSlug )
-			? null
-			: preg_replace( '/[^a-zA-Z0-9_\x7f-\xff]/', '_', trim( $sSlug ) );
-	}	
-	
-	/**
-	 * Converts non-alphabetic characters to underscore except hyphen(dash).
-	 * 
-	 * @access			public
-	 * @since			2.0.0
-	 * @remark			it must be public 
-	 * @return			string			The sanitized string.
-	 */ 
-	public static function sanitizeString( $sString ) {
-		return is_null( $sString )
-			? null
-			: preg_replace( '/[^a-zA-Z0-9_\x7f-\xff\-]/', '_', $sString );
-	}	
-		
+abstract class AdminPageFramework_Utility extends AdminPageFramework_Utility_Path {
+			
 	/**
 	 * Retrieves the query value from the given URL with a key.
 	 * 
@@ -52,88 +24,6 @@ abstract class AdminPageFramework_Utility extends AdminPageFramework_Utility_Arr
 		return isset( $aQuery[ $sQueryKey ] ) ? $aQuery[ $sQueryKey ] : null;
 		
 	}
-	
-	/**
-	 * Checks if the passed value is a number and set it to the default if not.
-	 * 
-	 * This is useful for form data validation. If it is a number and exceeds the set maximum number, 
-	 * it sets it to the maximum value. If it is a number and is below the minimum number, it sets to the minimum value.
-	 * Set a blank value for no limit.
-	 * 
-	 * @since			2.0.0
-	 * @return			string|integer			A numeric value will be returned. 
-	 */ 
-	static public function fixNumber( $nToFix, $nDefault, $nMin="", $nMax="" ) {
-
-		if ( ! is_numeric( trim( $nToFix ) ) ) return $nDefault;
-		if ( $nMin !== "" && $nToFix < $nMin ) return $nMin;
-		if ( $nMax !== "" && $nToFix > $nMax ) return $nMax;
-		return $nToFix;
-		
-	}		
-	
-	/**
-	 * Calculates the relative path from the given path.
-	 * 
-	 * This function is used to generate a template path.
-	 * 
-	 * @since			2.1.5
-	 * @author			Gordon
-	 * @author			Michael Uno,			Modified variable names and spacing.
-	 * @see				http://stackoverflow.com/questions/2637945/getting-relative-path-from-absolute-path-in-php/2638272#2638272
-	 */
-	static public function getRelativePath( $from, $to ) {
-		
-		// some compatibility fixes for Windows paths
-		$from = is_dir( $from ) ? rtrim( $from, '\/') . '/' : $from;
-		$to   = is_dir( $to )   ? rtrim( $to, '\/') . '/'   : $to;
-		$from = str_replace( '\\', '/', $from );
-		$to   = str_replace( '\\', '/', $to );
-
-		$from     = explode( '/', $from );
-		$to       = explode( '/', $to );
-		$relPath  = $to;
-
-		foreach( $from as $depth => $dir ) {
-			// find first non-matching dir
-			if( $dir === $to[ $depth ] ) {
-				// ignore this directory
-				array_shift( $relPath );
-			} else {
-				// get number of remaining dirs to $from
-				$remaining = count( $from ) - $depth;
-				if( $remaining > 1 ) {
-					// add traversals up to first matching dir
-					$padLength = ( count( $relPath ) + $remaining - 1 ) * -1;
-					$relPath = array_pad( $relPath, $padLength, '..' );
-					break;
-				} else {
-					$relPath[ 0 ] = './' . $relPath[ 0 ];
-				}
-			}
-		}
-		return implode( '/', $relPath );
-		
-	}
-	
-	/**
-	 * Attempts to find the caller scrip path.
-	 * 
-	 * @since			3.0.0
-	 * @return			string
-	 */
-	static public function getCallerScriptPath( $asRedirectedFiles=array( __FILE__ ) ) {
-		
-		$aRedirectedFiles = ( array ) $asRedirectedFiles;
-		$aRedirectedFiles[] = __FILE__;
-		$sCallerFilePath = '';
-		foreach( debug_backtrace() as $aDebugInfo )  {			
-			$sCallerFilePath = $aDebugInfo['file'];
-			if ( in_array( $sCallerFilePath, $aRedirectedFiles ) ) continue;
-			break;	// the first found item.
-		}
-		return $sCallerFilePath;
-	}	
 	
 	/**
 	 * Generates the string of attributes to be embedded in an HTML tag from an associative array.
@@ -158,20 +48,5 @@ abstract class AdminPageFramework_Utility extends AdminPageFramework_Utility_Arr
 		
 	}	
 	
-	/**
-	 * Compresses CSS rules.
-	 * 
-	 * @since			3.0.0
-	 */
-	static public function minifyCSS( $sCSSRules ) {
-		
-		return str_replace( 
-			array( "\r\n", "\r", "\n", "\t", '  ', '    ', '    '), 	// remove line breaks, tab, and white sspaces.
-			'', 
-			preg_replace( '!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $sCSSRules )	// remove comments
-		);
-		
-	}	
-		
 }
 endif;
---
+++ development/utility/AdminPageFramework_WPUtility_Page.php
@@ -0,0 +1,104 @@
+<?php
+if ( ! class_exists( 'AdminPageFramework_WPUtility_Page' ) ) :
+/**
+ * Provides utility methods to detect types of admin pages which use WordPress functions and classes.
+ *
+ * @since			2.0.0
+ * @extends			AdminPageFramework_Utility
+ * @package			AdminPageFramework
+ * @subpackage		Utility
+ * @internal
+ */
+class AdminPageFramework_WPUtility_Page extends AdminPageFramework_WPUtility_HTML {
+	
+	/**
+	 * Attempts to retrieve the current admin post type
+	 * 
+	 * @since			3.0.0
+	 */
+	static public function getCurrentPostType() {
+				 
+		static $_sCurrentPostType;
+		
+		// Since the current page will be the same throughout the execution of the script, if once it's found, there is no need to find it again.
+		if ( $_sCurrentPostType ) return $_sCurrentPostType;
+		
+		// Check to see if a post object exists
+		if ( isset( $GLOBALS['post'], $GLOBALS['post']->post_type ) && $GLOBALS['post']->post_type ) {
+			$_sCurrentPostType = $GLOBALS['post']->post_type;
+			return $_sCurrentPostType;
+		}
+		 
+		// Check if the current type is set
+		if ( isset( $GLOBALS['typenow'] ) && $GLOBALS['typenow'] ) {
+			$_sCurrentPostType = $GLOBALS['typenow'];
+			return $_sCurrentPostType;
+		}
+		 
+		// Check to see if the current screen is set
+		if ( isset( $GLOBALS['current_screen']->post_type ) && $GLOBALS['current_screen']->post_type ) {
+			$_sCurrentPostType = $GLOBALS['current_screen']->post_type;
+			return $_sCurrentPostType;
+		}
+		 
+		// Finally make a last ditch effort to check the URL query for type
+		if ( isset( $_REQUEST['post_type'] ) ) {
+			$_sCurrentPostType = sanitize_key( $_REQUEST['post_type'] );
+			return $_sCurrentPostType;
+		}
+		
+		// If the post is set, find the post type from it. If will perform a database query if necessary.
+		if ( isset( $_GET['post'] ) && $_GET['post'] ) {
+			$_sCurrentPostType = get_post_type( $_GET['post'] );
+			return $_sCurrentPostType;
+		}
+		
+		return null;
+		
+	}
+
+	/**
+	 * Checks if the current page is post editing page that belongs to the given post type(s).
+	 * 
+	 * @since			3.0.0
+	 * @param			array|string			The post type slug(s) to check. If this is empty, the method just checks the current page is a post definition page.
+	 * Otherwise, it will check if the page belongs to the given post type(s).
+	 * @return			boolean
+	 */
+	static public function isPostDefinitionPage( $asPostTypes=array() ) {
+		
+		$_aPostTypes = ( array ) $asPostTypes;
+
+		// If it's not the post definition page, 
+		if ( ! in_array( $GLOBALS['pagenow'], array( 'post.php', 'post-new.php', ) ) ) return false;
+
+		// If the parameter is empty, 
+		if ( empty( $_aPostTypes ) ) return true;
+
+		// If the parameter of the post type is set and it's in the given post types, 
+		if ( in_array( self::getCurrentPostType(), $_aPostTypes ) ) return true;				
+
+		// Otherwise,
+		return false;
+		
+	}		
+	
+	/**
+	 * Checks if the current page is in the post listing page of the given page slug(s).
+	 * 
+	 * @since			3.0.0
+	 */
+	static public function isPostListingPage( $asPostTypes=array() ) {
+				
+		if ( $GLOBALS['pagenow'] != 'edit.php' ) return false;
+		
+		$_aPostTypes = is_array( $asPostTypes ) ? $asPostTypes : array( $asPostTypes );
+		
+		if ( ! isset( $_GET['post_type'] )  ) return in_array( 'post', $_aPostTypes );
+
+		return in_array( $_GET['post_type'], $_aPostTypes );
+		
+	}
+	
+}
+endif;
