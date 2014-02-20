44cc084d6f15ec5a73fb9c29180aa33f8838aa81
Michael Uno <michael@michaeluno.jp>
2014-02-20 22:32:59 +0900

    Divided the setting class into four.

--- development/page/AdminPageFramework.php
+++ development/page/AdminPageFramework.php
@@ -220,7 +220,7 @@ if ( ! class_exists( 'AdminPageFramework' ) ) :
  * @package			AdminPageFramework
  * @subpackage		Page
  */
-abstract class AdminPageFramework extends AdminPageFramework_Setting {
+abstract class AdminPageFramework extends AdminPageFramework_Setting_Validation {
 		
 	/**
 	 * Registers necessary callbacks ans sets up internal components including properties.
