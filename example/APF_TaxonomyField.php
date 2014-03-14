afa73a761732c49a4ee5a8011a9d46639b0534ec
Michael Uno <michael@michaeluno.jp>
2014-03-14 21:12:32 +0900

    Added an example of implementing a customizing sort column algorithm.

--- example/APF_TaxonomyField.php
+++ example/APF_TaxonomyField.php
@@ -41,28 +41,40 @@ class APF_TaxonomyField extends AdminPageFramework_TaxonomyField {
 				'title'			=>	__( 'Image Upload', 'admin-page-framework-demo' ),
 			)
 		);			
-		
-	}
 	
+		// Customize the sorting algorithm of the terms of a custom column.
+		add_filter( 'get_terms', array( $this, 'replyToSortCustomColumn' ), 10, 3 );
+	
+	}
+		
 	/*
 	 * ( optional ) modify the columns of the term listing table
 	 */
-	public function sortable_columns_APF_TaxonomyField( $aColumn ) {	// sortable_column_{extended class name}
+	public function sortable_columns_APF_TaxonomyField( $aColumn ) {	// sortable_column_{instantiated class name}
 		
-		return array( 'thumbnail' => __( 'Thumbnail', 'admin-page-framework-demo' ) ) + $aColumn;
+		return array( 
+				'custom' => 'custom',
+			) 
+			+ $aColumn;
 		
 	}
 
-	public function columns_APF_TaxonomyField( $aColumn ) {	// column_{extended class name}
+	public function columns_APF_TaxonomyField( $aColumn ) {	// column_{instantiated class name}
 		
-		return array( 'cb' => $aColumn['cb'], 'thumbnail' => __( 'Thumbnail', 'admin-page-framework-demo' ) ) + $aColumn;
+		unset( $aColumn['description'] );
+		return array( 
+				'cb' => $aColumn['cb'],
+				'thumbnail' => __( 'Thumbnail', 'admin-page-framework-demo' ),
+				'custom' => __( 'Custom Column', 'admin-page-framework-demo' ),
+			) 
+			+ $aColumn;
 		
 	}
 	
 	/*
 	 * ( optional ) output the stored option to the custom column
 	 */	
-	public function cell_APF_TaxonomyField( $sCellHTML, $sColumnSlug, $iTermID ) {	// cell_{extended class name}
+	public function cell_APF_TaxonomyField( $sCellHTML, $sColumnSlug, $iTermID ) {	// cell_{instantiated class name}
 		
 		if ( ! $iTermID || $sColumnSlug != 'thumbnail' ) return $sCellHTML;
 		
@@ -73,15 +85,47 @@ class APF_TaxonomyField extends AdminPageFramework_TaxonomyField {
 		
 	}
 	
+	public function cell_APF_TaxonomyField_custom( $sCellHTML, $iTermID ) {	// cell_{instantiated class name}_{cell slug}
+		
+		// Using AdminPageFramework::getOption() is another way to retrieve an option value.
+		return AdminPageFramework::getOption( 'APF_TaxonomyField', array( $iTermID, 'text_field' ) );
+		// . __( 'Term ID', 'admin-page-framework-demo' ) . ': ' . $iTermID . '<br />'
+			
+		
+	}
+	
 	/*
 	 * ( optional ) Use this method to insert your custom text.
 	 */
-	public function do_APF_TaxonomyField() {	// do_{extended class name}
+	public function do_APF_TaxonomyField() {	// do_{instantiated class name}
 		?>
-			<p><?php _e( 'This text is inserted with the <code>do_{extended class name}</code> hook.', 'admin-page-framework-demo' ) ?></p>
+			<p><?php _e( 'This text is inserted with the <code>do_{instantiated class name}</code> hook.', 'admin-page-framework-demo' ) ?></p>
 		<?php		
 	}
 
+	/**
+	 * Customizes the sorting algorithm of a custom column.
+	 */
+	public function replyToSortCustomColumn( $aTerms, $aTaxonomies, $aArgs ) {
+		
+		if ( 'edit-tags.php' == $GLOBALS['pagenow'] && isset( $_GET{'orderby'} ) && 'custom' == $_GET{'orderby'} ) {
+			usort( $aTerms, array( $this, '_replyToSortByCustomOptionValue' ) );
+		}
+		return $aTerms;
+		
+	}
+		public function _replyToSortByCustomOptionValue( $oTermA, $oTermB ) {
+			
+			$_sClassName = get_class( $this );	// the instantiated class name is the option key by default.
+			$_sTextFieldA = AdminPageFramework::getOption( $_sClassName, array( $oTermA->term_id, 'text_field' ) );
+			$_sTextFieldB = AdminPageFramework::getOption( $_sClassName, array( $oTermB->term_id, 'text_field' ) );
+			return isset( $_GET['order'] ) && 'asc' == $_GET['order']
+				? strnatcmp( $_sTextFieldA, $_sTextFieldB )
+				: strnatcmp( $_sTextFieldB, $_sTextFieldA );
+			
+		}	
+	
+	
 	/*
 	 * ( optional ) Use this method to validate submitted option values.
 	 */
