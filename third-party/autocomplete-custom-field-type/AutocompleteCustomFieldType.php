e9c2a11a8b350ce5a13252225c03c6677ca41c96
Michael Uno <michael@michaeluno.jp>
2014-04-28 01:15:53 +0900

    Added a method to check if the site visitor is logged in.

--- third-party/autocomplete-custom-field-type/AutocompleteCustomFieldType.php
+++ third-party/autocomplete-custom-field-type/AutocompleteCustomFieldType.php
@@ -54,12 +54,8 @@ class AutoCompleteCustomFieldType extends AdminPageFramework_FieldType {
 		 * If the request key is set in the url and it yields 'autocomplete', return a JSON output and exit.
 		 */
 		if ( isset( $_GET['request'] ) && 'autocomplete' == $_GET['request'] ) {
-			
-			if ( ! function_exists( 'is_user_logged_in' ) ) {
-				include( ABSPATH . "wp-includes/pluggable.php" ); 			
-			}
-			
-			if ( is_user_logged_in() ) :
+						
+			if ( $this->_isLoggedIn() ) :
 			
 				$_aGet = $_GET;
 				unset( $_aGet['request'], $_aGet['page'], $_aGet['tab'], $_aGet['settings-updated'] );
@@ -94,6 +90,28 @@ class AutoCompleteCustomFieldType extends AdminPageFramework_FieldType {
 		}		
 		
 	}
+		private function _isLoggedIn() {
+			
+			if ( ! is_multisite() ) {				
+				if ( ! function_exists( 'is_user_logged_in' ) ) {
+					include_once( ABSPATH . "wp-includes/pluggable.php" ); 			
+				}	
+				return is_user_logged_in();
+			}
+			
+			// For multi-sites
+			if ( ! function_exists( 'get_currentuserinfo' )  ) {
+				include_once( ABSPATH . "wp-includes/pluggable.php" ); 			
+			}	
+			if ( ! function_exists( 'is_user_member_of_blog' )  ) {
+				include_once( ABSPATH . "wp-includes/user.php" ); 			
+			}			
+			global $current_user;
+			get_currentuserinfo();
+			return is_user_member_of_blog( $current_user->ID );
+			
+		}
+	
 		/**
 		 * Modifies the WordPress database query.
 		 */
