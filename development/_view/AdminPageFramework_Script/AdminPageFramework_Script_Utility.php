5d895dda10682a3157f3c9ff8352701263a073f1
Michael Uno <michael@michaeluno.jp>
2014-04-29 08:40:34 +0900

    Changed the position of semi-colon.

--- development/_view/AdminPageFramework_Script/AdminPageFramework_Script_Utility.php
+++ development/_view/AdminPageFramework_Script/AdminPageFramework_Script_Utility.php
@@ -23,12 +23,12 @@ class AdminPageFramework_Script_Utility {
 			$.fn.reverse = [].reverse;
 		
 			$.fn.formatPrintText = function() {
-				var aArgs = arguments;
+				var aArgs = arguments;				
 				return aArgs[ 0 ].replace( /{(\d+)}/g, function( match, number ) {
 					return typeof aArgs[ parseInt( number ) + 1 ] != 'undefined'
 						? aArgs[ parseInt( number ) + 1 ]
-						: match
-				;});
+						: match;
+				});
 			};
 		}( jQuery ));";
 		
