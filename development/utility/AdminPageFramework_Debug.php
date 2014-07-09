f706383ed7366f568708a5c4696b1e31c2275ff1
Michael Uno <michael@michaeluno.jp>
2014-07-10 08:14:09 +0900

    Added the log() method.

--- development/utility/AdminPageFramework_Debug.php
+++ development/utility/AdminPageFramework_Debug.php
@@ -50,10 +50,12 @@ class AdminPageFramework_Debug {
 	 * Logs the given array output into the given file.
 	 * 
 	 * @since			2.1.1
-	 * @since			3.0.3			Changed the default log location and file name.
+	 * @since			3.0.3		Changed the default log location and file name.
+	 * @deprecated		3.1.0		Use the log() method instead
 	 */
 	static public function logArray( $asArray, $sFilePath=null ) {
 		
+		self::log( $asArray, $sFilePath );
 		static $_iPageLoadID;
 		$_iPageLoadID = $_iPageLoadID ? $_iPageLoadID : uniqid();		
 		
@@ -71,5 +73,34 @@ class AdminPageFramework_Debug {
 		);			
 			
 	}	
+	
+	/**
+	 * Logs the given variable output to a file.
+	 * 
+	 * @remark		The alias of the logArray() method.
+	 * @since		3.1.0
+	 **/
+	static public function log( $v, $sFilePath=null ) {
+				
+		static $_iPageLoadID;	// identifies the page load.
+		$_iPageLoadID		= $_iPageLoadID ? $_iPageLoadID : uniqid();		
+		$_oCallerInfo		= debug_backtrace();
+		$_sCallerFunction	= isset( $_oCallerInfo[ 1 ]['function'] ) ? $_oCallerInfo[ 1 ]['function'] : '';
+		$_sCallerClasss		= isset( $_oCallerInfo[ 1 ]['class'] ) ? $_oCallerInfo[ 1 ]['class'] : '';
+		$sFilePath 			= ! $sFilePath
+			? WP_CONTENT_DIR . DIRECTORY_SEPARATOR . get_class() . '_' . $_sCallerClasss . '_' . date( "Ymd" ) . '.log'
+			: ( true === $sFilePath
+				? WP_CONTENT_DIR . DIRECTORY_SEPARATOR . get_class() . '_' . date( "Ymd" ) . '.log'
+				: $sFilePath
+			);
+		$_sHeading = date( "Y/m/d H:i:s", current_time( 'timestamp' ) ) . ' ' 
+			. "{$_iPageLoadID} {$_sCallerClasss}::{$_sCallerFunction} " 
+			. AdminPageFramework_Utility::getCurrentURL();
+		file_put_contents( 
+			$sFilePath, 
+			$_sHeading . PHP_EOL . print_r( $v, true ) . PHP_EOL . PHP_EOL,
+			FILE_APPEND 
+		);			
+	}		
 }
 endif;
