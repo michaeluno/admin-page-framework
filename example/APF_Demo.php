adfc6bdabff47abec38e4b8a60ecb4e04d442b00
Michael Uno <michael@michaeluno.jp>
2014-08-01 06:27:19 +0900

    Changed the field type used in the field definition callback example.

--- example/APF_Demo.php
+++ example/APF_Demo.php
@@ -680,7 +680,7 @@ class APF_Demo extends AdminPageFramework {
 				'tip'			=>	__( 'The description key can be omitted though.', 'admin-page-framework-demo' ),
 				'type'			=>	'checkbox',
 				'label'			=>	__( 'This is a check box.', 'admin-page-framework-demo' ) 
-					. ' ' . __( 'Check box\'s label can be a single string, not an array of multiple labels.', 'admin-page-framework-demo' ),
+					. ' ' . __( 'Check box\'s label can be a single string, not an array of multiple labels.', 'admin-page-framework-demo' ),	//'syntax fixer
 				'default'		=>	false,
 			),	
 			array(	// Multiple checkbox items - for multiple checkbox items, set an array to the 'label' element.
@@ -1441,6 +1441,7 @@ class APF_Demo extends AdminPageFramework {
 			array(
 				'field_id'		=>	'apf_post_titles',
 				'type'			=>	'checkbox',
+				'label_min_width'	=>	'100%',
 			),				
 			array()
 		);
