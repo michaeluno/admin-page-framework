e5dc43f545e19c78e89a9af7ff80c97415e31684
Michael Uno <michael@michaeluno.jp>
2014-08-10 06:14:31 +0900

    Updated the autoloader class to have some options.

--- development/utility/AdminPageFramework_RegisterClasses.php
+++ development/utility/AdminPageFramework_RegisterClasses.php
@@ -27,47 +27,48 @@ class AdminPageFramework_RegisterClasses {
 	 * 
 	 */
 	static protected $_aStructure_RecursiveOptions = array(
-		'is_recursive'	=>	true,
-		'exclude_dirs'	=>	array(),	
+		'is_recursive'			=>	true,
+		'exclude_dir_paths'		=>	array(),
+		'exclude_dir_names'		=>	array( 'asset', 'assets', 'css', 'js', 'image', 'images', 'license', 'document', 'documents' ),
+		'allowed_extensions'	=>	array( 'php', ),	// 'inc'
 	);
 	
 	/**
 	 * Sets up properties and performs registering classes.
 	 * 
-	 * param			array			$sClassDirPath		the target directory path to scan
+	 * param			array			$sClassDirPath		the target directory path to scan	 * 
+	 * param			array			$aSearchOptions		The recursive settings
+	 * 		array(
+	 * 			'is_recursive'	=> true,		// determines whether the scan should be performed recursively.
+	 * 			'exclude_dir_paths' => array(),		// set excluding directory paths without ending slash with numeric keys.
+	 * 			'exclude_dir_names' => array(),	// set excluding directory names.
+	 * 			'allowed_extensions' =>	array(),
+	 * 		)
 	 * param			array			$aClasses			the link to the array storing registered classes outside this object.
 	 * The structure of %aClasses must be consist of elements of a key-value pair of a file path and the key of the class name.
 	 * array(
 	 * 	'MyClassName' => 'MyClassName.php',
 	 * 	'MyClassName2' => 'MyClassName2.php',
 	 * )
-	 * 
-	 * param			array			The recursive settings
-	 * 		array(
-	 * 			'is_recursive'	=> true,		// determines whether the scan should be performed recursively.
-	 * 			'exclude_dirs' => array(),		// set excluding dir paths without ending slash with numeric keys.
-	 * 		)
-	 * @remark			The directory paths set for the 'exclude_dirs' option should use the system directory separator.
+	 * @remark			The directory paths set for the 'exclude_dir_paths' option should use the system directory separator.
 	 */
-	function __construct( $sClassDirPath, $aClasses=array(), $aAllowedExtensions=array( 'php', 'inc' ), $aRecursiveOptions=array( 'is_recursive' => true, 'exclude_dirs' => array() ), $aAllowedExtensions=array( 'php', 'inc' ) ) {
+	function __construct( $sClassDirPath, array $aSearchOptions=array(), array $aClasses=array() ) {
 			
-		$aRecursiveOptions = $aRecursiveOptions + self::$_aStructure_RecursiveOptions;
-		$this->_aClasses = $aClasses + $this->composeClassArray( $sClassDirPath, $aAllowedExtensions, $aRecursiveOptions );		
-		$this->registerClasses();
+		$this->_aClasses = $aClasses + $this->_constructClassArray( $sClassDirPath, $aSearchOptions + self::$_aStructure_RecursiveOptions );
+		$this->_registerClasses();
 		
 	}
 	
 	/**
 	 * Sets up the array consisting of class paths with the key of file name w/o extension.
 	 */
-	protected function composeClassArray( $sClassDirPath, $aAllowedExtensions, $aRecursiveOptions ) {
-		
-		$_aFilePaths = $this->getFilePaths( $sClassDirPath, $aAllowedExtensions, $aRecursiveOptions ); 
+	protected function _constructClassArray( $sClassDirPath, array $aSearchOptions ) {
+				
+		$_aFilePaths = $this->getFilePaths( $sClassDirPath, $aSearchOptions );
 		$_aClasses = array();
 		foreach( $_aFilePaths as $_sFilePath ) {
 			$_aClasses[ pathinfo( $_sFilePath, PATHINFO_FILENAME ) ] = $_sFilePath;	// the file name without extension will be assigned to the key
 		}
-
 		return $_aClasses;
 			
 	}
@@ -82,39 +83,41 @@ class AdminPageFramework_RegisterClasses {
 			  2 => string '.../class/MyClass3.php'
 			  ...
 		 * 
-		 * @since			3.1.0
 		 */
-		protected function getFilePaths( $sClassDirPath, $aAllowedExtensions, $aRecursiveOptions ) {
+		protected function getFilePaths( $sClassDirPath, array $aSearchOptions ) {
 			
-			$sClassDirPath = rtrim( $sClassDirPath, '\\/' ) . DIRECTORY_SEPARATOR;	// ensures the trailing (back/)slash exists. 
+			$sClassDirPath			= rtrim( $sClassDirPath, '\\/' ) . DIRECTORY_SEPARATOR;	// ensures the trailing (back/)slash exists. 
+			$_aAllowedExtensions	= $aSearchOptions['allowed_extensions'];
+			$_aExcludeDirPaths		= ( array ) $aSearchOptions['exclude_dir_paths'];
+			$_aExcludeDirNames		= ( array ) $aSearchOptions['exclude_dir_names'];
+			$_bIsRecursive			= $aSearchOptions[ 'is_recursive' ];
 			
 			if ( defined( 'GLOB_BRACE' ) ) {	// in some OSes this flag constant is not available.
-				$_aFilePaths = $aRecursiveOptions['is_recursive']
-					? $this->doRecursiveGlob( $sClassDirPath . '*.' . $this->getGlobPatternExtensionPart( $aAllowedExtensions ), GLOB_BRACE, $aRecursiveOptions['exclude_dirs'] )
-					: ( array ) glob( $sClassDirPath . '*.' . $this->getGlobPatternExtensionPart( $aAllowedExtensions ), GLOB_BRACE );
+				$_aFilePaths = $_bIsRecursive
+					? $this->doRecursiveGlob( $sClassDirPath . '*.' . $this->_getGlobPatternExtensionPart( $_aAllowedExtensions ), GLOB_BRACE, $_aExcludeDirPaths, $_aExcludeDirNames )
+					: ( array ) glob( $sClassDirPath . '*.' . $this->_getGlobPatternExtensionPart( $_aAllowedExtensions ), GLOB_BRACE );
 				return array_filter( $_aFilePaths );	// drop non-value elements.	
 			} 
 				
 			// For the Solaris operation system.
 			$_aFilePaths = array();
-			foreach( $aAllowedExtensions as $__sAllowedExtension ) {
+			foreach( $_aAllowedExtensions as $__sAllowedExtension ) {
 								
-				$__aFilePaths = $aRecursiveOptions['is_recursive']
-					? $this->doRecursiveGlob( $sClassDirPath . '*.' . $__sAllowedExtension, 0, $aRecursiveOptions['exclude_dirs'] )
+				$__aFilePaths = $_bIsRecursive
+					? $this->doRecursiveGlob( $sClassDirPath . '*.' . $__sAllowedExtension, 0, $_aExcludeDirPaths, $_aExcludeDirNames )
 					: ( array ) glob( $sClassDirPath . '*.' . $__sAllowedExtension );
 
 				$_aFilePaths = array_merge( $__aFilePaths, $_aFilePaths );
 				
 			}
-			$_aFilePaths = array_filter( $_aFilePaths );
-			return array_unique( $_aFilePaths );
+			return array_unique( array_filter( $_aFilePaths ) );
 			
 		}
-		
+	
 		/**
-		 * Composes the file pattern of the file extension part used for the glob() function with the given file extensions.
+		 * Constructs the file pattern of the file extension part used for the glob() function with the given file extensions.
 		 */
-		protected function getGlobPatternExtensionPart( $aExtensions=array( 'php', 'inc' ) ) {
+		protected function _getGlobPatternExtensionPart( array $aExtensions=array( 'php', 'inc' ) ) {
 			return empty( $aExtensions ) 
 				? '*'
 				: '{' . implode( ',', $aExtensions ) . '}';
@@ -123,24 +126,23 @@ class AdminPageFramework_RegisterClasses {
 		/**
 		 * The recursive version of the glob() function.
 		 */
-		protected function doRecursiveGlob( $sPathPatten, $iFlags=0, $asExcludeDirs=array() ) {
+		protected function doRecursiveGlob( $sPathPatten, $nFlags=0, array $aExcludeDirs=array(), array $aExcludeDirNames=array() ) {
 
-			$_aFiles = glob( $sPathPatten, $iFlags );	
-			$_aFiles = is_array( $_aFiles ) ? $_aFiles : array();	// glob() can return false.
-			$_aDirs = glob( dirname( $sPathPatten ) . DIRECTORY_SEPARATOR . '*', GLOB_ONLYDIR|GLOB_NOSORT );
-			$_aDirs = is_array( $_aDirs ) ? $_aDirs : array();
+			$_aFiles	= glob( $sPathPatten, $nFlags );	
+			$_aFiles	= is_array( $_aFiles ) ? $_aFiles : array();	// glob() can return false.
+			$_aDirs		= glob( dirname( $sPathPatten ) . DIRECTORY_SEPARATOR . '*', GLOB_ONLYDIR|GLOB_NOSORT );
+			$_aDirs		= is_array( $_aDirs ) ? $_aDirs : array();
 			foreach ( $_aDirs as $_sDirPath ) {
 
-				if ( in_array( $_sDirPath, ( array ) $asExcludeDirs ) ) continue;
+				if ( in_array( $_sDirPath, $aExcludeDirs ) ) { continue; }
+				if ( in_array( pathinfo( $_sDirPath, PATHINFO_DIRNAME ), $aExcludeDirNames ) ) { continue; }
 				
-				$_aFiles = array_merge( $_aFiles, $this->doRecursiveGlob( $_sDirPath . DIRECTORY_SEPARATOR . basename( $sPathPatten ), $iFlags, $asExcludeDirs ) );
+				$_aFiles = array_merge( $_aFiles, $this->doRecursiveGlob( $_sDirPath . DIRECTORY_SEPARATOR . basename( $sPathPatten ), $nFlags, $aExcludeDirs ) );
 				
 			}
-		
 			return $_aFiles;
 			
-		}
-		 
+		}		
 	
 	/**
 	 * Performs registration of the callback.
@@ -148,16 +150,20 @@ class AdminPageFramework_RegisterClasses {
 	 * This registers the method to be triggered when an unknown class is instantiated. 
 	 * 
 	 */
-	protected function registerClasses() {
-		spl_autoload_register( array( $this, 'replyToAutoLoader' ) );
+	protected function _registerClasses() {
+		spl_autoload_register( array( $this, '_replyToAutoLoad' ) );
 	}	
 		/**
 		 * Responds to the PHP auto-loader and includes the passed class based on the previously stored path associated with the class name in the constructor.
 		 */
-		public function replyToAutoLoader( $sCalledUnknownClassName ) {			
-			if ( array_key_exists( $sCalledUnknownClassName, $this->_aClasses ) &&  file_exists( $this->_aClasses[ $sCalledUnknownClassName ] ) ) {
-				include( $this->_aClasses[ $sCalledUnknownClassName ] );
+		public function _replyToAutoLoad( $sCalledUnknownClassName ) {			
+			if ( ! array_key_exists( $sCalledUnknownClassName, $this->_aClasses ) ) {
+				return;
 			}
+			if ( ! file_exists( $this->_aClasses[ $sCalledUnknownClassName ] ) ) {
+				return;
+			}
+			include( $this->_aClasses[ $sCalledUnknownClassName ] );
 		}
 	
 }
