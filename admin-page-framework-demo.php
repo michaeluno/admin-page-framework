83699355a2dfdd824309a830c5bcf5c073361b6b
Michael Uno <michael@michaeluno.jp>
2014-08-10 13:43:26 +0900

    - Advanced the version number. - Applied the changes to the minified version.

--- admin-page-framework-demo.php
+++ admin-page-framework-demo.php
@@ -5,7 +5,7 @@
 	Description:	Demonstrates the features of the Admin Page Framework class.
 	Author:			Michael Uno
 	Author URI:		http://michaeluno.jp
-	Version:		3.1.3b02
+	Version:		3.1.3b04
 	Requirements:	PHP 5.2.4 or above, WordPress 3.3 or above.
 */ 
 
--- development/admin-page-framework.php
+++ development/admin-page-framework.php
@@ -14,13 +14,12 @@
  * @remarks				To use the framework, 1. Extend the class 2. Override the setUp() method. 3. Use the hook functions.
  * @remarks				Requirements: WordPress 3.3 or above, PHP 5.2.4 or above.
  * @remarks				The documentation employs the <a href="http://en.wikipedia.org/wiki/PHPDoc">PHPDOc(DocBlock)</a> syntax.
- * @version				3.1.3b03
+ * @version				3.1.3b04
  */
-
 if ( ! class_exists( 'AdminPageFramework_Registry_Base' ) ) :
 abstract class AdminPageFramework_Registry_Base {
 	
-	const Version		= '3.1.3b03';	// <--- DON'T FORGET TO CHANGE THIS AS WELL!!
+	const Version		= '3.1.3b04';	// <--- DON'T FORGET TO CHANGE THIS AS WELL!!
 	const Name			= 'Admin Page Framework';
 	const Description	= 'Provides plugin and theme developers with simpler means of creating option pages, custom post types, ant meta boxes.';
 	const URI			= 'http://en.michaeluno.jp/admin-page-framework';
@@ -56,22 +55,12 @@ if ( ! class_exists( 'AdminPageFramework_Bootstrap' ) ) :
 /**
  * Loads the Admin Page Framework library.
  * 
- * @info
- * Library Name: Admin Page Framework
- * Library URI: http://wordpress.org/extend/plugins/admin-page-framework/
- * Author:  Michael Uno
- * Author URI: http://michaeluno.jp
- * Version: 3.1.3b03
- * Requirements: WordPress 3.3 or above, PHP 5.2.4 or above.
- * Description: Provides simpler means of building administration pages for plugin and theme developers.
  * @copyright	  	2013-2014 (c) Michael Uno
  * @license		  	MIT <http://opensource.org/licenses/MIT>
  * @see			    http://wordpress.org/plugins/admin-page-framework/
  * @see			    https://github.com/michaeluno/admin-page-framework
  * @link		    http://en.michaeluno.jp/admin-page-framework
  * @since		  	3.0.0
- * @remark			The minifier script will refer this comment section to create the comment header. So don't remove the @info section.
- * @remark			This class will not be included in the minifiled version.
  * @package			AdminPageFramework
  * @subpackage		Utility
  * @internal
