8d69c8c762041474063f079cba645440a94b4b41
Michael Uno <michael@michaeluno.jp>
2014-08-10 12:13:37 +0900

    Updated the minifier script.

--- development/admin-page-framework.php
+++ development/admin-page-framework.php
@@ -2,7 +2,7 @@
 /**
  * Admin Page Framework
  * 
- * Provides plugin and theme developers with simpler means of creating option pages, custom post types, ant meta boxes. 
+ * Provides plugin and theme developers with simpler means of creating option pages, custom post types, ant meta boxes.
  * 
  * @author				Michael Uno <michael@michaeluno.jp>
  * @copyright			2013-2014 (c) Michael Uno
@@ -14,7 +14,7 @@
  * @remarks				To use the framework, 1. Extend the class 2. Override the setUp() method. 3. Use the hook functions.
  * @remarks				Requirements: WordPress 3.3 or above, PHP 5.2.4 or above.
  * @remarks				The documentation employs the <a href="http://en.wikipedia.org/wiki/PHPDoc">PHPDOc(DocBlock)</a> syntax.
- * @version				3.1.3b02
+ * @version				3.1.3b03
  */
 
 if ( ! class_exists( 'AdminPageFramework_Bootstrap' ) ) :
@@ -26,7 +26,7 @@ if ( ! class_exists( 'AdminPageFramework_Bootstrap' ) ) :
  * Library URI: http://wordpress.org/extend/plugins/admin-page-framework/
  * Author:  Michael Uno
  * Author URI: http://michaeluno.jp
- * Version: 3.1.3b02
+ * Version: 3.1.3b03
  * Requirements: WordPress 3.3 or above, PHP 5.2.4 or above.
  * Description: Provides simpler means of building administration pages for plugin and theme developers.
  * @copyright	  	2013-2014 (c) Michael Uno
@@ -45,10 +45,23 @@ final class AdminPageFramework_Bootstrap {
 	
 	function __construct( $sLibraryPath ) {
 		
-		if ( ! defined( 'ABSPATH' ) ) return; // the minifier script will include this file ( but it does not include WordPress ) to use the reflection class to extract the docblock
-		$_sDirPath = dirname( $sLibraryPath );
-		include( $_sDirPath . '/utility/AdminPageFramework_RegisterClasses.php' );
-		new AdminPageFramework_RegisterClasses( $_sDirPath );
+		// The minifier script will include this file ( but it does not include WordPress ) to use the reflection class to extract the docblock
+		if ( ! defined( 'ABSPATH' ) ) {
+			return; 
+		}
+		
+		// If the autoloader class exists, it means the framework has been loaded somewhere else.
+		if ( class_exists( 'AdminPageFramework_RegisterClasses' ) ) {
+			return;
+		}
+		
+		// Load the classes. For the minified version, the autoloader class should not be located in the utility folder.
+		$_sDirPath				= dirname( $sLibraryPath );
+		$_sAutoLoaderClassPath	= $_sDirPath . '/utility/AdminPageFramework_RegisterClasses.php';
+		if ( file_exists( $_sAutoLoaderClassPath ) ) {
+			include( $_sAutoLoaderClassPath );
+			new AdminPageFramework_RegisterClasses( $_sDirPath );
+		}
 		
 	}
 	
@@ -59,12 +72,20 @@ endif;
 if ( ! class_exists( 'AdminPageFramework_Registry' ) ) :
 abstract class AdminPageFramework_Registry_Base {
 	
-	
+	const Version		= '3.1.3b03';	// <--- DON'T FORGET TO CHANGE THIS AS WELL!!
+	const Name			= 'Admin Page Framework';
+	const Description	= 'Provides plugin and theme developers with simpler means of creating option pages, custom post types, ant meta boxes.';
+	const URI			= 'http://en.michaeluno.jp/admin-page-framework';
+	const Author		= 'Michael Uno';
+	const AuthorURI		= 'http://en.michaeluno.jp/';
+	const Copyright		= 'Copyright (c) 2013-2014, Michael Uno';
+	const License		= 'MIT	<http://opensource.org/licenses/MIT>';
+	const Contributors	= '';	
 }
 endif;
 if ( ! class_exists( 'AdminPageFramework_Registry' ) ) :
 /**
- * 
+ * Defines the framework common information.
  */
 final class AdminPageFramework_Registry extends AdminPageFramework_Registry_Base {
 		
