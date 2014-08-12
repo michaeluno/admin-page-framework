e8d217afb84b2ecc82d0295228934ec7f4fbd88e
Michael Uno <michael@michaeluno.jp>
2014-08-12 09:39:33 +0900

    Added support of embedded links in custom taxonomy pages created by the framework.

--- development/factory/AdminPageFramework_PostType/AdminPageFramework_PostType.php
+++ development/factory/AdminPageFramework_PostType/AdminPageFramework_PostType.php
@@ -83,7 +83,7 @@ abstract class AdminPageFramework_PostType extends AdminPageFramework_PostType_C
 		$this->oProp				= new AdminPageFramework_Property_PostType( 
 			$this, 
 			$sCallerPath ? trim( $sCallerPath ) : ( 
-				( is_admin() && isset( $GLOBALS['pagenow'] ) && in_array( $GLOBALS['pagenow'], array( 'edit.php', 'post.php', 'post-new.php', 'plugins.php' ) ) )
+				( is_admin() && isset( $GLOBALS['pagenow'] ) && in_array( $GLOBALS['pagenow'], array( 'edit.php', 'post.php', 'post-new.php', 'plugins.php', 'tags.php', 'edit-tags.php', ) ) )
 					? AdminPageFramework_Utility::getCallerScriptPath( __FILE__ ) 
 					: null 
 				), 	// this is important to attempt to find the caller script path here when separating the library into multiple files.	
