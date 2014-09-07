64743b8b38ea7140462d2df033c39afcf98496cd
Michael Uno <michael@michaeluno.jp>
2014-09-07 17:13:02 +0900

    - Added the 'stopped_sorting_fields' callback hook. - Refactored the code.

--- development/_view/AdminPageFramework_Script/AdminPageFramework_Script_RegisterCallback.php
+++ development/_view/AdminPageFramework_Script/AdminPageFramework_Script_RegisterCallback.php
@@ -19,18 +19,19 @@ class AdminPageFramework_Script_RegisterCallback {
 
     static public function getjQueryPlugin() {
         
-        /**
-         * 
-         * @param string sFieldType     the field type
-         * @param sID     the field container tag ID
-         * @param iCallType     the function call type that indicates whether it's from repeatable sections or not.
-         */
         return "(function ( $ ) {
-                        
-            // The method that gets triggered when a repeatable field add button is pressed.
+            
+            // Callback containers.
+            $.fn.aAPFAddRepeatableFieldCallbacks        = [];            
+            $.fn.aAPFRemoveRepeatableFieldCallbacks     = [];
+            $.fn.aAPFSortedFieldsCallbacks              = [];            
+            $.fn.aAPFStoppedSortingFieldsCallbacks      = [];
+            
+            /**
+             * Gets triggered when a repeatable field add button is pressed.
+             */
             $.fn.callBackAddRepeatableField = function( sFieldType, sID, iCallType, iSectionIndex, iFieldIndex ) {
                 var oThisNode = this;
-                if ( ! $.fn.aAPFAddRepeatableFieldCallbacks ) $.fn.aAPFAddRepeatableFieldCallbacks = [];
                 $.fn.aAPFAddRepeatableFieldCallbacks.forEach( function( hfCallback ) {
                     if ( jQuery.isFunction( hfCallback ) ) { 
                         hfCallback( oThisNode, sFieldType, sID, iCallType, iSectionIndex, iFieldIndex ); 
@@ -38,10 +39,11 @@ class AdminPageFramework_Script_RegisterCallback {
                 });
             };
             
-            // The method that gets triggered when a repeatable field remove button is pressed.
+            /**
+             * Gets triggered when a repeatable field remove button is pressed.
+             */
             $.fn.callBackRemoveRepeatableField = function( sFieldType, sID, iCallType, iSectionIndex, iFieldIndex ) {
                 var oThisNode = this;
-                if ( ! $.fn.aAPFRemoveRepeatableFieldCallbacks ) $.fn.aAPFRemoveRepeatableFieldCallbacks = [];
                 $.fn.aAPFRemoveRepeatableFieldCallbacks.forEach( function( hfCallback ) {
                     if ( jQuery.isFunction( hfCallback ) ) { 
                         hfCallback( oThisNode, sFieldType, sID, iCallType, iSectionIndex. iFieldIndex );
@@ -49,37 +51,53 @@ class AdminPageFramework_Script_RegisterCallback {
                 });
             };
 
-            // The method that gets triggered when a sortable field is dropped and the sort event occurred
+            /**
+             * Gets triggered when a sortable field is dropped and the sort event occurred
+             */
             $.fn.callBackSortedFields = function( sFieldType, sID, iCallType ) {
-                var nodeThis = this;
-                if ( ! $.fn.aAPFSortedFieldsCallbacks ) $.fn.aAPFSortedFieldsCallbacks = [];
+                var oThisNode = this;
                 $.fn.aAPFSortedFieldsCallbacks.forEach( function( hfCallback ) {
                     if ( jQuery.isFunction( hfCallback ) ) { 
-                        hfCallback( nodeThis, sFieldType, sID, iCallType ); 
+                        hfCallback( oThisNode, sFieldType, sID, iCallType ); 
                     }
                 });
             };
+
+            /**
+             * Gets triggered when sorting fields stopped.
+             * @since   3.1.6
+             */
+            $.fn.callBackStoppedSortingFields = function( sFieldType, sID, iCallType ) {
+                var oThisNode = this;
+                $.fn.aAPFStoppedSortingFieldsCallbacks.forEach( function( hfCallback ) {
+                    if ( jQuery.isFunction( hfCallback ) ) { 
+                        hfCallback( oThisNode, sFieldType, sID, iCallType ); 
+                    }
+                });
+            };            
             
-            // The method that registers callbacks. This will be called in each field type definition class.
+            /**
+             * Registers callbacks. This will be called in each field type definition class.
+             */
             $.fn.registerAPFCallback = function( oOptions ) {
                 
                 // This is the easiest way to have default options.
-                var oSettings = $.extend({
-                    // The user specifies the settings with the following options.
-                    added_repeatable_field: function() {},
-                    removed_repeatable_field: function() {},
-                    sorted_fields: function() {},
-                }, oOptions );
-
-                // Set up arrays to store callback functions
-                if( ! $.fn.aAPFAddRepeatableFieldCallbacks ) $.fn.aAPFAddRepeatableFieldCallbacks = [];
-                if( ! $.fn.aAPFRemoveRepeatableFieldCallbacks ) $.fn.aAPFRemoveRepeatableFieldCallbacks = [];
-                if( ! $.fn.aAPFSortedFieldsCallbacks ) $.fn.aAPFSortedFieldsCallbacks = [];
+                var oSettings = $.extend(
+                    {
+                        // The user specifies the settings with the following options.
+                        added_repeatable_field:     function() {},
+                        removed_repeatable_field:   function() {},
+                        sorted_fields:              function() {},
+                        stopped_sorting_fields:     function() {},
+                    }, 
+                    oOptions 
+                );
 
                 // Store the callback functions
                 $.fn.aAPFAddRepeatableFieldCallbacks.push( oSettings.added_repeatable_field );
                 $.fn.aAPFRemoveRepeatableFieldCallbacks.push( oSettings.removed_repeatable_field );
                 $.fn.aAPFSortedFieldsCallbacks.push( oSettings.sorted_fields );
+                $.fn.aAPFStoppedSortingFieldsCallbacks.push( oSettings.stopped_sorting_fields );
                 
                 return;
 
