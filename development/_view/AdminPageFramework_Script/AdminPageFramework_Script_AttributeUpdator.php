403d60152be67133f178f0b206e5657460777674
Michael Uno <michael@michaeluno.jp>
2014-09-10 20:49:13 +0900

    Fixed code formatting.

--- development/_view/AdminPageFramework_Script/AdminPageFramework_Script_AttributeUpdator.php
+++ development/_view/AdminPageFramework_Script/AdminPageFramework_Script_AttributeUpdator.php
@@ -26,80 +26,81 @@ class AdminPageFramework_Script_AttributeUpdator {
         
             /**
              * Increments a first/last found digit with the prefix of underscore in a specified attribute value.
-             * if the bFirstOccurence is false, the last found one will be replaced.
+             * if the biFirstOccurence is false, the last found one will be replaced.
              */
-            $.fn.incrementIDAttribute = function( sAttribute, bFirstOccurence ) {     
+            $.fn.incrementIDAttribute = function( sAttribute, biFirstOccurence ) {     
                 return this.attr( sAttribute, function( iIndex, sValue ) {    
-                    return updateID( iIndex, sValue, 1, bFirstOccurence );
+                    return updateID( iIndex, sValue, 1, biFirstOccurence );
                 }); 
             };
             /**
              * Increments a first/last found digit enclosed in [] in a specified attribute value.
              */
-            $.fn.incrementNameAttribute = function( sAttribute, bFirstOccurence ) {     
+            $.fn.incrementNameAttribute = function( sAttribute, biFirstOccurence ) {     
                 return this.attr( sAttribute, function( iIndex, sValue ) {    
-                    return updateName( iIndex, sValue, 1, bFirstOccurence );
+                    return updateName( iIndex, sValue, 1, biFirstOccurence );
                 }); 
             };
     
             /**
              * Decrements a first/last found digit with the prefix of underscore in a specified attribute value.
              */
-            $.fn.decrementIDAttribute = function( sAttribute, bFirstOccurence ) {
+            $.fn.decrementIDAttribute = function( sAttribute, biFirstOccurence ) {
                 return this.attr( sAttribute, function( iIndex, sValue ) {
-                    return updateID( iIndex, sValue, -1, bFirstOccurence );
+                    return updateID( iIndex, sValue, -1, biFirstOccurence );
                 }); 
             };     
             /**
              * Decrements a first/last found digit enclosed in [] in a specified attribute value.
              */
-            $.fn.decrementNameAttribute = function( sAttribute, bFirstOccurence ) {
+            $.fn.decrementNameAttribute = function( sAttribute, biFirstOccurence ) {
                 return this.attr( sAttribute, function( iIndex, sValue ) {
-                    return updateName( iIndex, sValue, -1, bFirstOccurence );
+                    return updateName( iIndex, sValue, -1, biFirstOccurence );
                 }); 
             };     
             
             /* Sets the current index to the ID attribute. Used for sortable fields. */
-            $.fn.setIndexIDAttribute = function( sAttribute, iIndex, bFirstOccurence ){
+            $.fn.setIndexIDAttribute = function( sAttribute, iIndex, biFirstOccurence ){
                 return this.attr( sAttribute, function( i, sValue ) {
-                    return updateID( iIndex, sValue, 0, bFirstOccurence );
+                    return updateID( iIndex, sValue, 0, biFirstOccurence );
                 });
             };
             /* Sets the current index to the name attribute. Used for sortable fields. */
-            $.fn.setIndexNameAttribute = function( sAttribute, iIndex, bFirstOccurence ){
+            $.fn.setIndexNameAttribute = function( sAttribute, iIndex, biFirstOccurence ){
                 return this.attr( sAttribute, function( i, sValue ) {
-                    return updateName( iIndex, sValue, 0, bFirstOccurence );
+                    return updateName( iIndex, sValue, 0, biFirstOccurence );
                 });
             };     
             
             /* Local Function Literals */    
-            var updateID = function( iIndex, sID, bIncrement, bFirstOccurence ) {
-                if ( typeof sID === 'undefined' ) return sID;
-                var sNeedlePrefix = ( typeof bFirstOccurence === 'undefined' ) || ! bFirstOccurence ? '(.+)': '(.+?)';
-                var sNeedle = new RegExp( sNeedlePrefix + '__(\\\d+)(?=([_-]|$))' ); // triple escape - not sure why but on a separate test script, double escape was working
+            var updateID = function( iIndex, sID, iIncrementType, biFirstOccurence ) {
+                if ( 'undefined' === typeof sID ) { return sID; }
+                var sNeedlePrefix   = ( typeof biFirstOccurence === 'undefined' ) || ! biFirstOccurence ? '(.+)': '(.+?)';
+                var sNeedle         = new RegExp( sNeedlePrefix + '__(\\\d+)(?=([_-]|$))' ); // triple escape - not sure why but on a separate test script, double escape was working
                 return sID.replace( sNeedle, function ( sFullMatch, m0, m1 ) {
-                    if ( bIncrement === 1 )
+                    if ( 1 === iIncrementType ) {
                         return m0 + '__' + ( Number( m1 ) + 1 );
-                    else if ( bIncrement === -1 )
+                    } else if ( -1 === iIncrementType ) {
                         return m0 + '__' + ( Number( m1 ) - 1 );
-                    else 
+                    } else {
                         return m0 + '__' + ( iIndex );
+                    }
                 });
             }
-            var updateName = function( iIndex, sName, bIncrement, biFirstOccurence ) {
-                if ( typeof sName === 'undefined' ) return sName;
-                var sNeedlePrefix = ( typeof biFirstOccurence === 'undefined' ) || ! biFirstOccurence ? '(.+)': '(.+?)';
-                var sNeedle = biFirstOccurence === -1    
+            var updateName = function( iIndex, sName, iIncrementType, biFirstOccurence ) {
+                if ( 'undefined' === typeof sName ) { return sName; }
+                var sNeedlePrefix   = ( typeof biFirstOccurence === 'undefined' ) || ! biFirstOccurence ? '(.+)': '(.+?)';
+                var sNeedle         = biFirstOccurence === -1    
                     ? new RegExp( '(.+)' + '\\\[(\\\d+)(?=\\\].+\\\[\\\d+(?=\\\]))' ) // -1 is for the second occurrence from the last; for taxonomy field type
                     : new RegExp( sNeedlePrefix + '\\\[(\\\d+)(?=\\\])' ); // triple escape - not sure why but on a separate test script, double escape was working
                 return sName.replace( sNeedle, function ( sFullMatch, m0, m1 ) {
-                                
-                    if ( bIncrement === 1 )
+                    if ( iIncrementType === 1 ) {
                         return m0 + '[' + ( Number( m1 ) + 1 );
-                    else if ( bIncrement === -1 )
+                    } else if ( iIncrementType === -1 ) {
                         return m0 + '[' + ( Number( m1 ) - 1 );
-                    else 
+                    } else {
                         return m0 + '[' + ( iIndex );
+                    }
                 });
             }
                 
