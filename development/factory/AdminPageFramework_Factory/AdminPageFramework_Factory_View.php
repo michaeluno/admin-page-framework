a345fe31803e7e3403acc069d592cb5f39703c01
Michael Uno <michael@michaeluno.jp>
2014-09-14 12:17:58 +0900

    Fixed code formatting.

--- development/factory/AdminPageFramework_Factory/AdminPageFramework_Factory_View.php
+++ development/factory/AdminPageFramework_Factory/AdminPageFramework_Factory_View.php
@@ -11,8 +11,8 @@ if ( ! class_exists( 'AdminPageFramework_Factory_View' ) ) :
  * Provides methods for views.
  * 
  * @abstract
- * @since 3.0.4
- * @subpackage Factory
+ * @since       3.0.4
+ * @subpackage  Factory
  * @internal
  */
 abstract class AdminPageFramework_Factory_View extends AdminPageFramework_Factory_Model {
@@ -92,7 +92,7 @@ abstract class AdminPageFramework_Factory_View extends AdminPageFramework_Factor
         $_oField = new AdminPageFramework_FormField( $aField, $this->oProp->aOptions, $this->_getFieldErrors(), $this->oProp->aFieldTypeDefinitions, $this->oMsg ); // currently the error array is not supported for meta-boxes     
         return $this->oUtil->addAndApplyFilters(
             $this,
-            array(     'field_' . $this->oProp->sClassName . '_' . $aField['field_id'] ), // field_ + {extended class name} + _ {field id}
+            array( 'field_' . $this->oProp->sClassName . '_' . $aField['field_id'] ), // field_ + {extended class name} + _ {field id}
             $_oField->_getFieldOutput(), // field output
             $aField // the field array
         );     
