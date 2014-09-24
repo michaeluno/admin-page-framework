0b8bc23428ede38c5d7822c465a44e850d436d80
Michael Uno <michael@michaeluno.jp>
2014-09-25 02:18:45 +0900

    - Fixed an error that caused an undefined warning with the split method. - Fixed code formatting.

--- development/_view/AdminPageFramework_Script/AdminPageFramework_Script_Widget.php
+++ development/_view/AdminPageFramework_Script/AdminPageFramework_Script_Widget.php
@@ -31,35 +31,38 @@ class AdminPageFramework_Script_Widget {
                 $( document ).ajaxComplete( function( event, XMLHttpRequest, ajaxOptions ) {
        
                     // determine which ajax request is this (we're after \"save-widget\")
-                    var request = {}, pairs = ajaxOptions.data.split('&'), i, split, oWidget;
-                    for( i in pairs ) {
-                        split = pairs[i].split( '=' );
-                        request[decodeURIComponent( split[0] )] = decodeURIComponent( split[1] );
+                    var _aRequest   = {}, _iIndex, _aSplit, _oWidget;
+                    var _aPairs     = 'string' === typeof ajaxOptions.data
+                        ? ajaxOptions.data.split( '&' )
+                        : {};
+                    for( _iIndex in _aPairs ) {
+                        _aSplit = _aPairs[ _iIndex ].split( '=' );
+                        _aRequest[ decodeURIComponent( _aSplit[ 0 ] ) ] = decodeURIComponent( _aSplit[ 1 ] );
                     }
                     // only proceed if this was a widget-save request
-                    if( request.action && ( 'save-widget' === request.action ) ) {
+                    if( _aRequest.action && ( 'save-widget' === _aRequest.action ) ) {
                         
                         // locate the widget block
-                        oWidget = $( 'input.widget-id[value=\"' + request['widget-id'] + '\"]' ).parents( '.widget' );
+                        _oWidget = $( 'input.widget-id[value=\"' + _aRequest['widget-id'] + '\"]' ).parents( '.widget' );
 
                         // trigger manual save, if this was the save request 
                         // and if we didn't get the form html response (the wp bug)
                         if( ! XMLHttpRequest.responseText )  {
                             
-                            wpWidgets.save( oWidget, 0, 1, 0);
+                            wpWidgets.save( _oWidget, 0, 1, 0);
                             return;
                             
                         } 
     
                         // Check if it is the framework widget.
-                        if ( $( oWidget ).find( '.admin-page-framework-sectionset' ).length <= 0 ) { 
+                        if ( $( _oWidget ).find( '.admin-page-framework-sectionset' ).length <= 0 ) { 
                             return; 
                         } 
                         
                         // we got an response, this could be either our request above,
                         // or a correct widget-save call, so fire an event on which we can hook our js                        
-                        $( document ).trigger( 'admin_page_framework_saved_widget', oWidget );
-                
+                        $( document ).trigger( 'admin_page_framework_saved_widget', _oWidget );
+console.log( 'saved widget' );
                     }
                 });           
            
