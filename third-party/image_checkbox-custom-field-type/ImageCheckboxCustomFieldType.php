2e56274669daf800b1b5f6ad66301d61c9571f2d
Michael Uno <michael@michaeluno.jp>
2014-10-10 03:18:45 +0900

    Converted tabs into white spaces.

--- third-party/image_checkbox-custom-field-type/ImageCheckboxCustomFieldType.php
+++ third-party/image_checkbox-custom-field-type/ImageCheckboxCustomFieldType.php
@@ -9,123 +9,122 @@ if ( ! class_exists( 'ImageCheckboxCustomFieldType' ) ) :
  * @internal
  */
 class ImageCheckboxCustomFieldType extends AdminPageFramework_FieldType {
-		
-	/**
-	 * Defines the field type slugs used for this field type.
-	 * 
-	 * The slug is used for the type key in a field definition array.
-	 * 	$this->addSettingFields(
-			array(
-				'section_id'	=> ...,
-				'type'          => 'image_checkbox',		// <--- THIS PART
-				'field_id'      => ...,
-				'title'		    => ...,
-			)
-		);
-	 */
-	public $aFieldTypeSlugs = array( 'image_checkbox', );
-	
-	/**
-	 * Defines the default key-values of this field type. 
-	 * 
-	 * The keys are used for the field definition array.
-	 * 	$this->addSettingFields(
-			array(
-				'section_id'    => ...,
-				'type'          => ...,
-				'field_id'      => ...,
-				'my_custom_key' => ...,	// <-- THIS PART
-			)
-		);
-	 * @remark			$_aDefaultKeys holds shared default key-values defined in the base class.
-	 */
-	protected $aDefaultKeys = array(
+        
+    /**
+     * Defines the field type slugs used for this field type.
+     * 
+     * The slug is used for the type key in a field definition array.
+     * <code>$this->addSettingFields(
+     *      array(
+     *          'section_id'    => ...,
+     *          'type'          => 'image_checkbox',        // <--- THIS PART
+     *          'field_id'      => ...,
+     *          'title'         => ...,
+     *      )
+     *  );</code>
+     */
+    public $aFieldTypeSlugs = array( 'image_checkbox', );
+    
+    /**
+     * Defines the default key-values of this field type. 
+     * 
+     * The keys are used for the field definition array.
+     * <code>$this->addSettingFields(
+     *      array(
+     *          'section_id'    => ...,
+     *          'type'          => ...,
+     *          'field_id'      => ...,
+     *          'my_custom_key' => ...,    // <-- THIS PART
+     *      )
+     *  );</code>
+     * @remark            $_aDefaultKeys holds shared default key-values defined in the base class.
+     */
+    protected $aDefaultKeys = array(
         'label_min_width'   => '',
         'width'             => 64,
         'height'            => 64,
-		'attributes'	    =>	array(),	
-	);
+        'attributes'        => array(),    
+    );
 
-	/**
-	 * The user constructor.
-	 * 
-	 * Loaded at the end of the constructor.
-	 */
-	public function construct() {}
-		
-	/**
-	 * Loads the field type necessary components.
-	 * 
-	 * This method is triggered when a field definition array that calls this field type is parsed. 
-	 */ 
-	public function setUp() {}	
+    /**
+     * The user constructor.
+     * 
+     * Loaded at the end of the constructor.
+     */
+    public function construct() {}
+        
+    /**
+     * Loads the field type necessary components.
+     * 
+     * This method is triggered when a field definition array that calls this field type is parsed. 
+     */ 
+    public function setUp() {}    
 
-	/**
-	 * Returns an array holding the urls of enqueuing scripts.
-	 * 
-	 * The returning array should be composed with all numeric keys. Each element can be either a string( the url or the path of the source file) or an array of custom argument.
-	 * 
-	 * <h4>Custom Argument Array</h4>
-	 * <ul>
-	 * 	<li><strong>src</strong> - ( required, string ) The url or path of the target source file</li>
-	 * 	<li><strong>handle_id</strong> - ( optional, string ) The handle ID of the script.</li>
-	 * 	<li><strong>dependencies</strong> - ( optional, array ) The dependency array. For more information, see <a href="http://codex.wordpress.org/Function_Reference/wp_enqueue_script">codex</a>.</li>
-	 * 	<li><strong>version</strong> - ( optional, string ) The stylesheet version number.</li>
-	 * 	<li><strong>translation</strong> - ( optional, array ) The translation array. The handle ID will be used for the object name.</li>
-	 * 	<li><strong>in_footer</strong> - ( optional, boolean ) Whether to enqueue the script before < / head > or before < / body > Default: <code>false</code>.</li>
-	 * </ul>	 
-	 */
-	protected function getEnqueuingScripts() { 
-		return array();
-	}
-	
-	/**
-	 * Returns an array holding the urls of enqueuing styles.
-	 * 
-	 * <h4>Custom Argument Array</h4>
-	 * <ul>
-	 * 	<li><strong>src</strong> - ( required, string ) The url or path of the target source file</li>
-	 * 	<li><strong>handle_id</strong> - ( optional, string ) The handle ID of the stylesheet.</li>
-	 * 	<li><strong>dependencies</strong> - ( optional, array ) The dependency array. For more information, see <a href="http://codex.wordpress.org/Function_Reference/wp_enqueue_style">codex</a>.</li>
-	 * 	<li><strong>version</strong> - ( optional, string ) The stylesheet version number.</li>
-	 * 	<li><strong>media</strong> - ( optional, string ) the description of the field which is inserted into the after the input field tag.</li>
-	 * </ul>
-	 */
-	protected function getEnqueuingStyles() { 
-		return array();
-	}			
+    /**
+     * Returns an array holding the urls of enqueuing scripts.
+     * 
+     * The returning array should be composed with all numeric keys. Each element can be either a string( the url or the path of the source file) or an array of custom argument.
+     * 
+     * <h4>Custom Argument Array</h4>
+     * <ul>
+     *     <li><strong>src</strong> - ( required, string ) The url or path of the target source file</li>
+     *     <li><strong>handle_id</strong> - ( optional, string ) The handle ID of the script.</li>
+     *     <li><strong>dependencies</strong> - ( optional, array ) The dependency array. For more information, see <a href="http://codex.wordpress.org/Function_Reference/wp_enqueue_script">codex</a>.</li>
+     *     <li><strong>version</strong> - ( optional, string ) The stylesheet version number.</li>
+     *     <li><strong>translation</strong> - ( optional, array ) The translation array. The handle ID will be used for the object name.</li>
+     *     <li><strong>in_footer</strong> - ( optional, boolean ) Whether to enqueue the script before < / head > or before < / body > Default: <code>false</code>.</li>
+     * </ul>     
+     */
+    protected function getEnqueuingScripts() { 
+        return array();
+    }
+    
+    /**
+     * Returns an array holding the urls of enqueuing styles.
+     * 
+     * <h4>Custom Argument Array</h4>
+     * <ul>
+     *     <li><strong>src</strong> - ( required, string ) The url or path of the target source file</li>
+     *     <li><strong>handle_id</strong> - ( optional, string ) The handle ID of the stylesheet.</li>
+     *     <li><strong>dependencies</strong> - ( optional, array ) The dependency array. For more information, see <a href="http://codex.wordpress.org/Function_Reference/wp_enqueue_style">codex</a>.</li>
+     *     <li><strong>version</strong> - ( optional, string ) The stylesheet version number.</li>
+     *     <li><strong>media</strong> - ( optional, string ) the description of the field which is inserted into the after the input field tag.</li>
+     * </ul>
+     */
+    protected function getEnqueuingStyles() { 
+        return array();
+    }            
 
 
-	/**
-	 * Returns the field type specific JavaScript script.
-	 */ 
-	protected function getScripts() { 
+    /**
+     * Returns the field type specific JavaScript script.
+     */ 
+    protected function getScripts() { 
 
-		$aJSArray = json_encode( $this->aFieldTypeSlugs );
-		/*	
-		 * The below function will be triggered when a new repeatable field is added. 
-		 * 
-		 * Use the registerAPFCallback method to register a callback.
-		 * Available callbacks are:
-		 * 	added_repeatable_field - triggered when a repeatable field gets repeated. Parameters 1. (object) the jQuery element object. 2. (string) the field type slug. 3. (string) the field tag id.
-		 * 	removed_repeatable_field - triggered when a repeatable field gets removed. Parameters 1. (object) the jQuery element object. 2. (string) the field type slug. 3. (string) the field tag id.
-		 * 	sorted_fields - triggered when a sortable field gets sorted. Parameters 1. (object) the jQuery element object. 2. (string) the field type slug. 3. (string) the field tag id.
-		 * */
-		return "" . PHP_EOL;
-		
-	}
+        $aJSArray = json_encode( $this->aFieldTypeSlugs );
+        /*    
+         * The below function will be triggered when a new repeatable field is added. 
+         * 
+         * Use the registerAPFCallback method to register a callback.
+         * Available callbacks are:
+         *     added_repeatable_field - triggered when a repeatable field gets repeated. Parameters 1. (object) the jQuery element object. 2. (string) the field type slug. 3. (string) the field tag id.
+         *     removed_repeatable_field - triggered when a repeatable field gets removed. Parameters 1. (object) the jQuery element object. 2. (string) the field type slug. 3. (string) the field tag id.
+         *     sorted_fields - triggered when a sortable field gets sorted. Parameters 1. (object) the jQuery element object. 2. (string) the field type slug. 3. (string) the field tag id.
+         * */
+        return "" . PHP_EOL;
+        
+    }
 
-	/**
-	 * Returns IE specific CSS rules.
-	 */
-	protected function getIEStyles() { return ''; }
+    /**
+     * Returns IE specific CSS rules.
+     */
+    protected function getIEStyles() { return ''; }
 
-	/**
-	 * Returns the field type specific CSS rules.
-	 */ 
-	protected function getStyles() {
-		return "
-
+    /**
+     * Returns the field type specific CSS rules.
+     */ 
+    protected function getStyles() {
+        return "
 .admin-page-framework-field-image_checkbox input[type='checkbox'] {
     display: none;
 } 
@@ -147,10 +146,7 @@ class ImageCheckboxCustomFieldType extends AdminPageFramework_FieldType {
     margin-bottom: 1em;
 }
         ";
-        
-
-        
-	}
+    }
 
     /**
      * The class selector to indicate that the input tag is a admin page framework checkbox.
@@ -159,16 +155,16 @@ class ImageCheckboxCustomFieldType extends AdminPageFramework_FieldType {
      * @since   3.1.7
      */
     protected $_sCheckboxClassSelector = 'apf_checkbox';
-	
-	/**
-	 * Returns the output of the geometry custom field type.
-	 * 
-	 */
-	/**
-	 * Returns the output of the field type.
-	 */
-	protected function getField( $aField ) { 
-			
+    
+    /**
+     * Returns the output of the geometry custom field type.
+     * 
+     */
+    /**
+     * Returns the output of the field type.
+     */
+    protected function getField( $aField ) { 
+            
         $_aOutput = array();
         $_asValue = $aField['attributes']['value'];
 
@@ -188,7 +184,7 @@ class ImageCheckboxCustomFieldType extends AdminPageFramework_FieldType {
             $_aInputAttributesIE8OrBelow = $_aInputAttributes + array(
                 'style'     => $this->generateInlineCSS(
                     array(
-                        'display'               => 'inline-block',
+                        'display' => 'inline-block',
                     )
                 ),       
             );
@@ -252,9 +248,8 @@ class ImageCheckboxCustomFieldType extends AdminPageFramework_FieldType {
                 
         }    
         return implode( PHP_EOL, $_aOutput );
-		
-	}	
-  
-            
+        
+    }    
+     
 }
 endif;
