13dc9b0a960fc7ea9763c519ab49e12f0ce9310e
Michael Uno <michael@michaeluno.jp>
2014-10-11 18:18:45 +0900

    Optimized the process of triggering widget saving event.

--- development/_view/AdminPageFramework_Script/AdminPageFramework_Script_Widget.php
+++ development/_view/AdminPageFramework_Script/AdminPageFramework_Script_Widget.php
@@ -24,13 +24,13 @@ class AdminPageFramework_Script_Widget {
          * 
          * @since   3.2.0
          */     
-        return "(function ( $ ) {
-            
+        return "(function ( $ ) {         
+   
             $( document ).ready( function() {
-                 
+
                 $( document ).ajaxComplete( function( event, XMLHttpRequest, ajaxOptions ) {
        
-                    // determine which ajax request is this (we're after \"save-widget\")
+                    // Determine which ajax request this is (we're after \"save-widget\")
                     var _aRequest   = {}, _iIndex, _aSplit, _oWidget;
                     var _aPairs     = 'string' === typeof ajaxOptions.data
                         ? ajaxOptions.data.split( '&' )
@@ -39,28 +39,24 @@ class AdminPageFramework_Script_Widget {
                         _aSplit = _aPairs[ _iIndex ].split( '=' );
                         _aRequest[ decodeURIComponent( _aSplit[ 0 ] ) ] = decodeURIComponent( _aSplit[ 1 ] );
                     }
-                    // only proceed if this was a widget-save request
+                    // Only proceed if this was a widget-save request
                     if( _aRequest.action && ( 'save-widget' === _aRequest.action ) ) {
                         
-                        // locate the widget block
+                        // Locate the widget block
                         _oWidget = $( 'input.widget-id[value=\"' + _aRequest['widget-id'] + '\"]' ).parents( '.widget' );
-
-                        // trigger manual save, if this was the save request 
-                        // and if we didn't get the form html response (the wp bug)
-                        if( ! XMLHttpRequest.responseText )  {
-                            
-                            wpWidgets.save( _oWidget, 0, 1, 0);
-                            return;
-                            
-                        } 
-    
+        
                         // Check if it is the framework widget.
                         if ( $( _oWidget ).find( '.admin-page-framework-sectionset' ).length <= 0 ) { 
                             return; 
-                        } 
-
-                        // we got an response, this could be either our request above,
-                        // or a correct widget-save call, so fire an event on which we can hook our js                        
+                        }   
+                        
+                        // Trigger manual save, if this was the save request and if we didn't get the form html response (the wp bug)
+                        if( ! XMLHttpRequest.responseText )  {                         
+                            wpWidgets.save( _oWidget, 0, 1, 0 );
+                            return;                            
+                        }
+              
+                        // We got an response, this could be either our request above, or a correct widget-save call, so fire an event on which we can hook our js.
                         $( document ).trigger( 'admin_page_framework_saved_widget', _oWidget );
 
                     }
