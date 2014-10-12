488631d481c042f21efb77e2885fc95cc3bde3f1
Michael Uno <michael@michaeluno.jp>
2014-10-12 16:29:36 +0900

    Updated inline documentation.

--- development/factory/AdminPageFramework_Factory/AdminPageFramework_Factory.php
+++ development/factory/AdminPageFramework_Factory/AdminPageFramework_Factory.php
@@ -10,7 +10,12 @@ if ( ! class_exists( 'AdminPageFramework_Factory' ) ) :
 /**
  * The factory class for creating Admin Page Framework objects.
  * 
- * This class defines public methods that the user will interact with.
+ * This factory class consists of four componential classes by extending them.
+ * 
+ * - AdminPageFramework_Factory - The router class that deals with redirecting function calls and instantiation of classes to the appropriate object based on the factory type.
+ * - AdminPageFramework_Model - The model class taht deals with retrieving data from the database or the framework properties.
+ * - AdminPageFramework_View - The view class taht deals with displaying outputs.
+ * - AdminPageFramework_Controller - The controller class which provides public methods that the framework users will interact with. They use methods to set up forms and admin messages etc.
  * 
  * @abstract
  * @since       3.0.4
