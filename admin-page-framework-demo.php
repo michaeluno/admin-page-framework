dc2e379d54e452cc5b69b869cfafa8a658db1d86
Michael Uno <michael@michaeluno.jp>
2014-10-14 16:20:57 +0900

    - Advanced the version number. - Applied the changes to the minified version.

--- admin-page-framework-demo.php
+++ admin-page-framework-demo.php
@@ -5,7 +5,7 @@
     Description:    Demonstrates the features of the Admin Page Framework class.
     Author:         Michael Uno
     Author URI:     http://michaeluno.jp
-    Version:        3.3.0b10
+    Version:        3.3.0b11
     Requirements:   PHP 5.2.4 or above, WordPress 3.3 or above.
 */ 
 
