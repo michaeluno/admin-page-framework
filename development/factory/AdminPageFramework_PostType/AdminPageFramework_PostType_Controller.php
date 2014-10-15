7232b8022f59c5da00287cec64fcf1664257a58d
Michael Uno <michael@michaeluno.jp>
2014-10-15 12:28:07 +0900

    Fixed inline code formatting.

--- development/factory/AdminPageFramework_PostType/AdminPageFramework_PostType_Controller.php
+++ development/factory/AdminPageFramework_PostType/AdminPageFramework_PostType_Controller.php
@@ -13,12 +13,17 @@ if ( ! class_exists( 'AdminPageFramework_PostType_Controller' ) ) :
  * Those methods are public and provides means for users to set property values.
  * 
  * @abstract
- * @since 3.0.4
- * @package AdminPageFramework
- * @subpackage PostType
+ * @since       3.0.4
+ * @package     AdminPageFramework
+ * @subpackage  PostType
  */
 abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework_PostType_View {    
 
+    /**
+     * Sets up hooks and properties.
+     * 
+     * @internal
+     */
     function __construct( $oProp ) {
         
         parent::__construct( $oProp );
@@ -36,7 +41,7 @@ abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework
     }
 
     /**
-    * The method for all necessary set-ups.
+    * The method for necessary set-ups.
     * 
     * <h4>Example</h4>
     * <code>public function setUp() {
@@ -45,26 +50,26 @@ abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework
     *         $this->addTaxonomy( 
     *             'sample_taxonomy', // taxonomy slug
     *             array( // argument - for the argument array keys, refer to : http://codex.wordpress.org/Function_Reference/register_taxonomy#Arguments
-    *                 'labels' => array(
-    *                     'name' => 'Genre',
-    *                     'add_new_item' => 'Add New Genre',
-    *                     'new_item_name' => "New Genre"
+    *                 'labels'              => array(
+    *                     'name'            => 'Genre',
+    *                     'add_new_item'    => 'Add New Genre',
+    *                     'new_item_name'   => "New Genre"
     *                 ),
-    *                 'show_ui' => true,
-    *                 'show_tagcloud' => false,
-    *                 'hierarchical' => true,
-    *                 'show_admin_column' => true,
-    *                 'show_in_nav_menus' => true,
-    *                 'show_table_filter' => true, // framework specific key
-    *                 'show_in_sidebar_menus' => false, // framework specific key
+    *                 'show_ui'                 => true,
+    *                 'show_tagcloud'           => false,
+    *                 'hierarchical'            => true,
+    *                 'show_admin_column'       => true,
+    *                 'show_in_nav_menus'       => true,
+    *                 'show_table_filter'       => true, // framework specific key
+    *                 'show_in_sidebar_menus'   => false, // framework specific key
     *             )
     *         );
     *     }</code>
     * 
     * @abstract
-    * @since 2.0.0
-    * @remark The user should override this method in their class definition.
-    * @remark A callback for the <em>wp_loaded</em> hook.
+    * @since        2.0.0
+    * @remark       The user should override this method in their class definition.
+    * @remark       A callback for the `wp_loaded` hook.
     */
     public function setUp() {}    
         
@@ -74,8 +79,8 @@ abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework
     /**
      * Enqueues styles by page slug and tab slug.
      * 
-     * @since 3.0.0
-     * @return array An array holding the handle IDs of queued items.
+     * @since       3.0.0
+     * @return      array       An array holding the handle IDs of queued items.
      */
     public function enqueueStyles( $aSRCs, $aCustomArgs=array() ) {     
         if ( method_exists( $this->oResource, '_enqueueStyles' ) ) {
@@ -85,18 +90,18 @@ abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework
     /**
      * Enqueues a style by page slug and tab slug.
      * 
-     * @since 3.0.0
-     * @see http://codex.wordpress.org/Function_Reference/wp_enqueue_style
-     * @param string The URL of the stylesheet to enqueue, the absolute file path, or the relative path to the root directory of WordPress. Example: '/css/mystyle.css'.
-     * @param             array (optional) The argument array for more advanced parameters.
+     * @since       3.0.0
+     * @see         http://codex.wordpress.org/Function_Reference/wp_enqueue_style
+     * @param       string The URL of the stylesheet to enqueue, the absolute file path, or the relative path to the root directory of WordPress. Example: `/css/mystyle.css`.
+     * @param       array           $sSRC           (optional) The argument array for more advanced parameters.
      * <h4>Custom Argument Array</h4>
      * <ul>
-     *     <li><strong>handle_id</strong> - ( optional, string ) The handle ID of the stylesheet.</li>
-     *     <li><strong>dependencies</strong> - ( optional, array ) The dependency array. For more information, see <a href="http://codex.wordpress.org/Function_Reference/wp_enqueue_style">codex</a>.</li>
-     *     <li><strong>version</strong> - ( optional, string ) The stylesheet version number.</li>
-     *     <li><strong>media</strong> - ( optional, string ) the description of the field which is inserted into after the input field tag.</li>
+     *     <li>**handle_id** - (optional, string) The handle ID of the stylesheet.</li>
+     *     <li>**dependencies** - (optional, array) The dependency array. For more information, see <a href="http://codex.wordpress.org/Function_Reference/wp_enqueue_style">codex</a>.</li>
+     *     <li>**version** - (optional, string) The stylesheet version number.</li>
+     *     <li>**media** - (optional, string) the description of the field which is inserted into after the input field tag.</li>
      * </ul>
-     * @return string The script handle ID. If the passed url is not a valid url string, an empty string will be returned.
+     * @return      string          The script handle ID. If the passed url is not a valid url string, an empty string will be returned.
      */    
     public function enqueueStyle( $sSRC, $aCustomArgs=array() ) {
         if ( method_exists( $this->oResource, '_enqueueStyle' ) ) {
@@ -106,8 +111,8 @@ abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework
     /**
      * Enqueues scripts by page slug and tab slug.
      * 
-     * @since 3.0.0
-     * @return array An array holding the handle IDs of queued items.
+     * @since       3.0.0
+     * @return      array       An array holding the handle IDs of queued items.
      */
     public function enqueueScripts( $aSRCs, $aCustomArgs=array() ) {
         if ( method_exists( $this->oResource, '_enqueueScripts' ) ) {
@@ -119,29 +124,29 @@ abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework
      *  
      * <h4>Example</h4>
      * <code>$this->enqueueScript(  
-     * plugins_url( 'asset/js/test.js' , __FILE__ ), // source url or path
-     * array(
-     * 'handle_id' => 'my_script', // this handle ID also is used as the object name for the translation array below.
-     * 'translation' => array( 
-     * 'a' => 'hello world!',
-     * 'style_handle_id' => $sStyleHandle, // check the enqueued style handle ID here.
-     * ),
-     * )
+     *      plugins_url( 'asset/js/test.js' , __FILE__ ), // source url or path
+     *      array(
+     *          'handle_id'     => 'my_script', // this handle ID also is used as the object name for the translation array below.
+     *          'translation'   => array( 
+     *              'a'                 => 'hello world!',
+     *              'style_handle_id'   => $sStyleHandle, // check the enqueued style handle ID here.
+     *          ),
+     *      )
      * );</code>
      * 
-     * @since 3.0.0
-     * @see http://codex.wordpress.org/Function_Reference/wp_enqueue_script
-     * @param string The URL of the stylesheet to enqueue, the absolute file path, or the relative path to the root directory of WordPress. Example: '/js/myscript.js'.
-     * @param             array (optional) The argument array for more advanced parameters.
+     * @since       3.0.0
+     * @see         http://codex.wordpress.org/Function_Reference/wp_enqueue_script
+     * @param       string      The URL of the stylesheet to enqueue, the absolute file path, or the relative path to the root directory of WordPress. Example: `/js/myscript.js`.
+     * @param       array       (optional) The argument array for more advanced parameters.
      * <h4>Custom Argument Array</h4>
      * <ul>
-     *     <li><strong>handle_id</strong> - ( optional, string ) The handle ID of the script.</li>
-     *     <li><strong>dependencies</strong> - ( optional, array ) The dependency array. For more information, see <a href="http://codex.wordpress.org/Function_Reference/wp_enqueue_script">codex</a>.</li>
-     *     <li><strong>version</strong> - ( optional, string ) The stylesheet version number.</li>
-     *     <li><strong>translation</strong> - ( optional, array ) The translation array. The handle ID will be used for the object name.</li>
-     *     <li><strong>in_footer</strong> - ( optional, boolean ) Whether to enqueue the script before <code></head ></code> or before <code></body></code> Default: <em>false</em>.</li>
+     *     <li>**handle_id** - (optional, string) The handle ID of the script.</li>
+     *     <li>**dependencies** - (optional, array) The dependency array. For more information, see <a href="http://codex.wordpress.org/Function_Reference/wp_enqueue_script">codex</a>.</li>
+     *     <li>**version** - (optional, string) The stylesheet version number.</li>
+     *     <li>**translation** - (optional, array) The translation array. The handle ID will be used for the object name.</li>
+     *     <li>**in_footer** - (optional, boolean) Whether to enqueue the script before `</head >` or before `</body>` Default: `false`.</li>
      * </ul>
-     * @return string The script handle ID. If the passed url is not a valid url string, an empty string will be returned.
+     * @return      string The script handle ID. If the passed url is not a valid url string, an empty string will be returned.
      */
     public function enqueueScript( $sSRC, $aCustomArgs=array() ) {    
         if ( method_exists( $this->oResource, '_enqueueScript' ) ) {
@@ -159,9 +164,9 @@ abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework
     * <code>$this->setAutoSave( false );
     * </code>
     * 
-    * @since 2.0.0
-    * @param boolean If true, it enables the auto-save; otherwise, it disables it.
-    * return void
+    * @since        2.0.0
+    * @param        boolean         If true, it enables the auto-save; otherwise, it disables it.
+    * return        void
     */ 
     protected function setAutoSave( $bEnableAutoSave=True ) {
         $this->oProp->bEnableAutoSave = $bEnableAutoSave;     
@@ -188,12 +193,12 @@ abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework
     *   )
     * );</code>
     * 
-    * @see http://codex.wordpress.org/Function_Reference/register_taxonomy#Arguments
+    * @see      http://codex.wordpress.org/Function_Reference/register_taxonomy#Arguments
     * @since    2.0.0
     * @since    3.1.1       Added the third parameter.
     * @param    string      $sTaxonomySlug              The taxonomy slug.
     * @param    array       $aArgs                      The taxonomy argument array passed to the second parameter of the <a href="http://codex.wordpress.org/Function_Reference/register_taxonomy#Arguments">register_taxonomy()</a> function.
-    * @param    array       $aAdditionalObjectTypes     Additional object types(post types) besides the caller post type.
+    * @param    array       $aAdditionalObjectTypes     Additional object types (post types) besides the caller post type.
     * @return   void
     */ 
     protected function addTaxonomy( $sTaxonomySlug, array $aArgs, array $aAdditionalObjectTypes=array() ) {
@@ -239,9 +244,9 @@ abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework
     * <code>$this->setAuthorTableFilter( true );
     * </code>
     * 
-    * @since 2.0.0
-    * @param boolean If true, it enables the author filter; otherwise, it disables it.
-    * @return void
+    * @since        2.0.0
+    * @param        boolean     $bEnableAuthorTableFileter      If true, it enables the author filter; otherwise, it disables it.
+    * @return       void
     */ 
     protected function setAuthorTableFilter( $bEnableAuthorTableFileter=false ) {
         $this->oProp->bEnableAuthorTableFileter = $bEnableAuthorTableFileter;
@@ -252,9 +257,9 @@ abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework
      * 
      * This is only necessary if it is not set in the constructor.
      * 
-     * @since 2.0.0
-     * @deprecated      3.2.0   Use the setArguments() method instead.
-     * @return void
+     * @since           2.0.0
+     * @deprecated      3.2.0           Use the setArguments() method instead.
+     * @return          void
      */ 
     protected function setPostTypeArgs( $aArgs ) {
         $this->setArguments( ( array ) $aArgs );
@@ -263,9 +268,9 @@ abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework
     /**
      * Sets the post type arguments.
      * 
-     * @remark      The alias of setPostTypeArgs().
-     * @see http://codex.wordpress.org/Function_Reference/register_post_type#Arguments
-     * @param array The <a href="http://codex.wordpress.org/Function_Reference/register_post_type#Arguments">array of arguments</a> to be passed to the second parameter of the <em>register_post_type()</em> function.
+     * @remark      The alias of `setPostTypeArgs()`.
+     * @see         http://codex.wordpress.org/Function_Reference/register_post_type#Arguments
+     * @param       array       $aArguments     The <a href="http://codex.wordpress.org/Function_Reference/register_post_type#Arguments">array of arguments</a> to be passed to the second parameter of the `register_post_type()` function.
      * @since       3.2.0
      */
     protected function setArguments( array $aArguments=array() ) {
@@ -279,10 +284,10 @@ abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework
      * <code>$this->setFooterInfoLeft( '<br />Custom Text on the left hand side.' );
      * </code>
      * 
-     * @since 2.0.0
-     * @param string The HTML code to insert.
-     * @param boolean If true, the text will be appended; otherwise, it will replace the default text.
-     * @return void
+     * @since       2.0.0
+     * @param       string      $sHTML      The HTML code to insert.
+     * @param       boolean     $bAppend    If true, the text will be appended; otherwise, it will replace the default text.
+     * @return      void
      */    
     protected function setFooterInfoLeft( $sHTML, $bAppend=true ) {
         if ( isset( $this->oLink ) ) // check if the object is set to ensure it won't trigger a warning message in non-admin pages.
@@ -298,10 +303,10 @@ abstract class AdminPageFramework_PostType_Controller extends AdminPageFramework
      * <code>$this->setFooterInfoRight( '<br />Custom Text on the right hand side.' );
      * </code>
      * 
-     * @since 2.0.0
-     * @param string The HTML code to insert.
-     * @param boolean If true, the text will be appended; otherwise, it will replace the default text.
-     * @return void
+     * @since       2.0.0
+     * @param       string      $sHTML      The HTML code to insert.
+     * @param       boolean     $bAppend    If true, the text will be appended; otherwise, it will replace the default text.
+     * @return      void
      */     
     protected function setFooterInfoRight( $sHTML, $bAppend=true ) {
         if ( isset( $this->oLink ) ) // check if the object is set to ensure it won't trigger a warning message in non-admin pages.    
