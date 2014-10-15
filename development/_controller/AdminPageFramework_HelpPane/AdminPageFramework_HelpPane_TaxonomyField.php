a25f26b1c9fd2c2f849c864fe3a93c407d17bc76
Michael Uno <michael@michaeluno.jp>
2014-10-15 14:35:26 +0900

    Fixed inline documentation formatting.

--- development/_controller/AdminPageFramework_HelpPane/AdminPageFramework_HelpPane_TaxonomyField.php
+++ development/_controller/AdminPageFramework_HelpPane/AdminPageFramework_HelpPane_TaxonomyField.php
@@ -10,10 +10,10 @@ if ( ! class_exists( 'AdminPageFramework_HelpPane_TaxonomyField' ) ) :
 /**
  * Provides methods to manipulate the contextual help tab for the taxonomy field class.
  *
- * @package AdminPageFramework
- * @subpackage HelpPane
- * @since 3.0.0     
- * @extends AdminPageFramework_HelpPane_MetaBox
+ * @package     AdminPageFramework
+ * @subpackage  HelpPane
+ * @since       3.0.0     
+ * @extends     AdminPageFramework_HelpPane_MetaBox
  * @internal
  */
 class AdminPageFramework_HelpPane_TaxonomyField extends AdminPageFramework_HelpPane_MetaBox {
@@ -22,14 +22,14 @@ class AdminPageFramework_HelpPane_TaxonomyField extends AdminPageFramework_HelpP
      * Registers the contextual help tab contents.
      * 
      * @internal
-     * @since 3.0.0
-     * @remark A call back for the <em>load-{page hook}</em> action hook.
-     * @remark The method name implies that this is for meta boxes. This does not mean this method is only for meta box form fields. Extra help text can be added with the <em>addHelpText()</em> method.
+     * @since       3.0.0
+     * @remark      A call back for the `load-{page hook}` action hook.
+     * @remark      The method name implies that this is for meta boxes. This does not mean this method is only for meta box form fields. Extra help text can be added with the `addHelpText()` method.
      * @internal
      */ 
     public function _replyToRegisterHelpTabTextForMetaBox() {
             
-        $this->_setHelpTab(     // this method is defined in the base class.
+        $this->_setHelpTab(     // defined in the base class.
             $this->oProp->sMetaBoxID, 
             $this->oProp->sTitle, 
             $this->oProp->aHelpTabText, 
