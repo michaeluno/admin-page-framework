6f9a5b4f4c69fcd3419f1905824f99966d8a1d16
Michael Uno <michael@michaeluno.jp>
2014-10-16 04:07:57 +0900

    Made the log() method indicate the passed value type.

--- development/utility/AdminPageFramework_Debug.php
+++ development/utility/AdminPageFramework_Debug.php
@@ -10,10 +10,10 @@ if ( ! class_exists( 'AdminPageFramework_Debug' ) ) :
 /**
  * Provides debugging methods.
  *
- * @since 2.0.0
- * @since 3.1.3 Extends AdminPageFramework_WPUtility
- * @package AdminPageFramework
- * @subpackage Debug
+ * @since           2.0.0
+ * @since           3.1.3       Extends AdminPageFramework_WPUtility
+ * @package         AdminPageFramework
+ * @subpackage      Debug
  * @internal
  */
 class AdminPageFramework_Debug extends AdminPageFramework_WPUtility {
@@ -74,43 +74,46 @@ class AdminPageFramework_Debug extends AdminPageFramework_WPUtility {
     /**
      * Logs the given variable output to a file.
      * 
-     * @remark The alias of the logArray() method.
-     * @since 3.1.0
-     * @since 3.1.3 Made it leave milliseconds and elapsed time from the last call of the method.
+     * @remark      The alias of the `logArray()` method.
+     * @since       3.1.0
+     * @since       3.1.3       Made it leave milliseconds and elapsed time from the last call of the method.
+     * @since       3.3.0       Made it indicate the data type.
      **/
-    static public function log( $v, $sFilePath=null ) {
+    static public function log( $vValue, $sFilePath=null ) {
                 
         static $_iPageLoadID; // identifies the page load.
         static $_nGMTOffset;
         static $_fPreviousTimeStamp = 0;
-        $_iPageLoadID = $_iPageLoadID ? $_iPageLoadID : uniqid();     
-        $_oCallerInfo = debug_backtrace();
-        $_sCallerFunction = isset( $_oCallerInfo[ 1 ]['function'] ) ? $_oCallerInfo[ 1 ]['function'] : '';
-        $_sCallerClasss = isset( $_oCallerInfo[ 1 ]['class'] ) ? $_oCallerInfo[ 1 ]['class'] : '';
-        $sFilePath             = ! $sFilePath
+        $_iPageLoadID       = $_iPageLoadID ? $_iPageLoadID : uniqid();     
+        $_oCallerInfo       = debug_backtrace();
+        $_sCallerFunction   = isset( $_oCallerInfo[ 1 ]['function'] ) ? $_oCallerInfo[ 1 ]['function'] : '';
+        $_sCallerClasss     = isset( $_oCallerInfo[ 1 ]['class'] ) ? $_oCallerInfo[ 1 ]['class'] : '';
+        $sFilePath          = ! $sFilePath
             ? WP_CONTENT_DIR . DIRECTORY_SEPARATOR . get_class() . '_' . $_sCallerClasss . '_' . date( "Ymd" ) . '.log'
             : ( true === $sFilePath
                 ? WP_CONTENT_DIR . DIRECTORY_SEPARATOR . get_class() . '_' . date( "Ymd" ) . '.log'
                 : $sFilePath
             );
-        $_nGMTOffset = isset( $_nGMTOffset ) ? $_nGMTOffset : get_option( 'gmt_offset' );
-        $_fCurrentTimeStamp = microtime( true );
-        $_nNow = $_fCurrentTimeStamp + ( $_nGMTOffset * 60 * 60 );
-        $_nMicroseconds = round( ( $_nNow - floor( $_nNow ) ) * 10000 );
-        $_nMicroseconds = str_pad( $_nMicroseconds, 4, '0' );
-        $_nElapsed = round( $_fCurrentTimeStamp - $_fPreviousTimeStamp, 3 );
-        $_aElapsedParts = explode( ".", ( string ) $_nElapsed );
-        $_sElapsedFloat = str_pad( isset( $_aElapsedParts[ 1 ] ) ? $_aElapsedParts[ 1 ] : 0, 3, '0' );
-        $_sElapsed = isset( $_aElapsedParts[ 0 ] ) ? $_aElapsedParts[ 0 ] : 0;
-        $_sElapsed = strlen( $_sElapsed ) > 1 ? '+' . substr( $_sElapsed, -1, 2 ) : ' ' . $_sElapsed;
-        $_sHeading = date( "Y/m/d H:i:s", $_nNow ) . '.' . $_nMicroseconds . ' ' 
+        $_nGMTOffset            = isset( $_nGMTOffset ) ? $_nGMTOffset : get_option( 'gmt_offset' );
+        $_fCurrentTimeStamp     = microtime( true );
+        $_nNow                  = $_fCurrentTimeStamp + ( $_nGMTOffset * 60 * 60 );
+        $_nMicroseconds         = round( ( $_nNow - floor( $_nNow ) ) * 10000 );
+        $_nMicroseconds         = str_pad( $_nMicroseconds, 4, '0' );
+        $_nElapsed              = round( $_fCurrentTimeStamp - $_fPreviousTimeStamp, 3 );
+        $_aElapsedParts         = explode( ".", ( string ) $_nElapsed );
+        $_sElapsedFloat         = str_pad( isset( $_aElapsedParts[ 1 ] ) ? $_aElapsedParts[ 1 ] : 0, 3, '0' );
+        $_sElapsed              = isset( $_aElapsedParts[ 0 ] ) ? $_aElapsedParts[ 0 ] : 0;
+        $_sElapsed              = strlen( $_sElapsed ) > 1 ? '+' . substr( $_sElapsed, -1, 2 ) : ' ' . $_sElapsed;
+        $_sHeading              = date( "Y/m/d H:i:s", $_nNow ) . '.' . $_nMicroseconds . ' ' 
             . $_sElapsed . '.' . $_sElapsedFloat . ' '    
             . "{$_iPageLoadID} {$_sCallerClasss}::{$_sCallerFunction} " 
             . current_filter() . ' '
             . self::getCurrentURL();
         file_put_contents( 
             $sFilePath, 
-            $_sHeading . PHP_EOL . print_r( $v, true ) . PHP_EOL . PHP_EOL,
+            $_sHeading . PHP_EOL 
+                . '(' . gettype( $vValue ) . ') ' 
+                . print_r( $vValue, true ) . PHP_EOL . PHP_EOL,
             FILE_APPEND 
         );     
         $_fPreviousTimeStamp = $_fCurrentTimeStamp;
@@ -121,7 +124,7 @@ class AdminPageFramework_Debug extends AdminPageFramework_WPUtility {
          * 
          * @since       2.1.1
          * @since       3.0.3 Changed the default log location and file name.
-         * @deprecated  3.1.0 Use the log() method instead
+         * @deprecated  3.1.0 Use the `log()` method instead.
          */
         static public function logArray( $asArray, $sFilePath=null ) {
             self::log( $asArray, $sFilePath );     
