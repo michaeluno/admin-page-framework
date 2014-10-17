520b493c3662b37a4018cec531eb7d63e60371f8
Michael Uno <michael@michaeluno.jp>
2014-10-17 19:59:14 +0900

    - Separated the documentation part. - Fixed inline documentation formatting.

--- development/factory/AdminPageFramework_MetaBox_Page/AdminPageFramework_MetaBox_Page.php
+++ development/factory/AdminPageFramework_MetaBox_Page/AdminPageFramework_MetaBox_Page.php
@@ -9,39 +9,11 @@
 if ( ! class_exists( 'AdminPageFramework_MetaBox_Page' ) ) :
 /**
  * Provides methods for creating meta boxes in pages added by the framework.
- *
- * <h2>Hooks</h2>
- * <p>The class automatically creates WordPress action and filter hooks associated with the class methods.
- * The class methods corresponding to the name of the below actions and filters can be extended to modify the page output. Those methods are the callbacks of the filters and actions.</p>
- * <h3>Methods and Action Hooks</h3>
- * <ul>
- *     <li><strong>start_{instantiated class name}</strong> – triggered at the end of the class constructor. This receives the class object in the first parameter.</li>
- *     <li><strong>set_up{instantiated class name}</strong> – triggered afther the setUp() method is called. This receives the class object in the first parameter.</li>
- *     <li><strong>do_{instantiated class name}</strong> – triggered when the meta box gets rendered. The first parameter: the class object [3.1.3+].</li>
- * </ul>
- * <h3>Methods and Filter Hooks</h3>
- * <ul>
- *     <li><strong>field_types_{instantiated class name}</strong> – receives the field type definition array. The first parameter: the field type definition array.</li>
- *     <li><strong>field_{instantiated class name}_{field ID}</strong> – receives the form input field output of the given input field ID. The first parameter: output string. The second parameter: the array of option.</li>
- *     <li><strong>content_{instantiated class name}</strong> – receives the entire output of the meta box. The first parameter: the output HTML string.</li>
- *     <li><strong>style_common_admin_page_framework</strong> –  [3.2.1+] receives the output of the base CSS rules applied to common CSS rules shared by the framework.</li>
- *     <li><strong>style_common_{instantiated class name}</strong> –  receives the output of the base CSS rules applied to the pages of the associated post types with the meta box.</li>
- *     <li><strong>style_ie_common_{instantiated class name}</strong> –  receives the output of the base CSS rules for Internet Explorer applied to the pages of the associated post types with the meta box.</li>
- *     <li><strong>style_{instantiated class name}</strong> –  receives the output of the CSS rules applied to the pages of the associated post types with the meta box.</li>
- *     <li><strong>style_ie_{instantiated class name}</strong> –  receives the output of the CSS rules for Internet Explorer applied to the pages of the associated post types with the meta box.</li>
- *     <li><strong>script_common_{instantiated class name}</strong> – receives the output of the base JavaScript scripts applied to the pages of the associated post types with the meta box.</li>
- *     <li><strong>script_{instantiated class name}</strong> – receives the output of the JavaScript scripts applied to the pages of the associated post types with the meta box.</li>
- *     <li><strong>validation_{instantiated class name}</strong> – receives the form submission values as array. The first parameter: submitted input array. The second parameter: the original array stored in the database.</li>
- * </ul>
  * 
  * @abstract
- * @since 3.0.0
- * @use AdminPageFramework_Utility
- * @use AdminPageFramework_Message
- * @use AdminPageFramework_Debug
- * @use AdminPageFramework_Property_Page
- * @package AdminPageFramework
- * @subpackage PageMetaBox
+ * @since       3.0.0
+ * @package     AdminPageFramework
+ * @subpackage  PageMetaBox
  */
 abstract class AdminPageFramework_MetaBox_Page extends AdminPageFramework_MetaBox_Page_Controller {
     
@@ -74,24 +46,24 @@ abstract class AdminPageFramework_MetaBox_Page extends AdminPageFramework_MetaBo
      *         'default' // priority
      *     );     
      * </code>
-     * @since 3.0.0
+     * @since       3.0.0
      * 
-     * @param string $sMetaBoxID The meta box ID to be created.
-     * @param string $sTitle The meta box title.
-     * @param array|string $asPageSlugs     the page slug(s) that the meta box belongs to. If the element is an array, it will be considered as a tab array.
+     * @param       string          $sMetaBoxID     The meta box ID to be created.
+     * @param       string          $sTitle         The meta box title.
+     * @param       array|string    $asPageSlugs    the page slug(s) that the meta box belongs to. If the element is an array, it will be considered as a tab array.
      * <code>
         $asPageSlugs = array(     
             'settings' => array(     // if the key is not numeric and the value is an array, it will be considered as a tab array.
                 'help',         // enabled in the tab whose slug is 'help' which belongs to the page whose slug is 'settings'
-                'about',         // enabled in the tab whose slug is 'about' which belongs to the page whose slug is 'settings'
-                'general', // enabled in the tab whose slug is 'general' which belongs to the page whose slug is 'settings'
+                'about',        // enabled in the tab whose slug is 'about' which belongs to the page whose slug is 'settings'
+                'general',      // enabled in the tab whose slug is 'general' which belongs to the page whose slug is 'settings'
             ),
             'manage', // if the numeric key with a string value is given, the condition applies to the page slug of this string value.
         );
      * </code>
-     * @param string $sContext The context, either 'normal', 'advanced', or 'side'.
-     * @param string $sPriority The priority, either 'high', 'core', 'default' or 'low'.
-     * @param string $sCapability The capability. See <a href="https://codex.wordpress.org/Roles_and_Capabilities" target="_blank">Roles and Capabilities</a>.
+     * @param       string          $sContext       The context, either `normal`, `advanced`, or `side`.
+     * @param       string          $sPriority      The priority, either `high`, `core`, `default` or `low`.
+     * @param       string          $sCapability    The capability. See <a href="https://codex.wordpress.org/Roles_and_Capabilities" target="_blank">Roles and Capabilities</a>.
      */
     function __construct( $sMetaBoxID, $sTitle, $asPageSlugs=array(), $sContext='normal', $sPriority='default', $sCapability='manage_options', $sTextDomain='admin-page-framework' ) {     
         
---
+++ development/factory/AdminPageFramework_MetaBox_Page/AdminPageFramework_MetaBox_Page_Documentation.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Admin Page Framework
+ * 
+ * http://en.michaeluno.jp/admin-page-framework/
+ * Copyright (c) 2013-2014 Michael Uno; Licensed MIT
+ * 
+ */
+if ( ! class_exists( 'AAdminPageFramework_MetaBox_Page_Documentation' ) ) :
+/**
+ * Provides an abstract base class to create meta boxes in generic admin pages created by the framework.
+ * 
+ * <h2>Hooks</h2>
+ * <p>The class automatically creates WordPress action and filter hooks associated with the class methods.
+ * The class methods corresponding to the name of the below actions and filters can be extended to modify the page output. Those methods are the callbacks of the filters and actions.</p>
+ * <h3>Methods and Action Hooks</h3>
+ * <ul>
+ *     <li>**start_{instantiated class name}** – triggered at the end of the class constructor. This receives the class object in the first parameter.</li>
+ *     <li>**set_up{instantiated class name}** – triggered afther the setUp() method is called. This receives the class object in the first parameter.</li>
+ *     <li>**do_{instantiated class name}** – triggered when the meta box gets rendered. The first parameter: the class object [3.1.3+].</li>
+ * </ul>
+ * <h3>Methods and Filter Hooks</h3>
+ * <ul>
+ *     <li>**field_types_{instantiated class name}** – receives the field type definition array. The first parameter: the field type definition array.</li>
+ *     <li>**field_{instantiated class name}_{field ID}** – receives the form input field output of the given input field ID. The first parameter: output string. The second parameter: the array of option.</li>
+ *     <li>**content_{instantiated class name}** – receives the entire output of the meta box. The first parameter: the output HTML string.</li>
+ *     <li>**style_common_admin_page_framework** –  [3.2.1+] receives the output of the base CSS rules applied to common CSS rules shared by the framework.</li>
+ *     <li>**style_common_{instantiated class name}** –  receives the output of the base CSS rules applied to the pages of the associated post types with the meta box.</li>
+ *     <li>**style_ie_common_{instantiated class name}** –  receives the output of the base CSS rules for Internet Explorer applied to the pages of the associated post types with the meta box.</li>
+ *     <li>**style_{instantiated class name}** –  receives the output of the CSS rules applied to the pages of the associated post types with the meta box.</li>
+ *     <li>**style_ie_{instantiated class name}** –  receives the output of the CSS rules for Internet Explorer applied to the pages of the associated post types with the meta box.</li>
+ *     <li>**script_common_{instantiated class name}** – receives the output of the base JavaScript scripts applied to the pages of the associated post types with the meta box.</li>
+ *     <li>**script_{instantiated class name}** – receives the output of the JavaScript scripts applied to the pages of the associated post types with the meta box.</li>
+ *     <li>**validation_{instantiated class name}** – receives the form submission values as array. The first parameter: submitted input array. The second parameter: the original array stored in the database.</li>
+ * </ul>
+ * 
+ * @since       3.3.0
+ * @package     AdminPageFramework
+ * @subpackage  PageMetaBox
+ * @heading
+ */
+abstract class AAdminPageFramework_MetaBox_Page_Documentation {}
+endif;
