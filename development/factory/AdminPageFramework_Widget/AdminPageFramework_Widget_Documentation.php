37f7cd4d361bc087c34c0d964ec055afbc3e15e9
Michael Uno <michael@michaeluno.jp>
2014-10-17 20:29:27 +0900

    Added a documentation class.

---
+++ development/factory/AdminPageFramework_Widget/AdminPageFramework_Widget_Documentation.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Admin Page Framework
+ * 
+ * http://en.michaeluno.jp/admin-page-framework/
+ * Copyright (c) 2013-2014 Michael Uno; Licensed MIT
+ * 
+ */
+if ( ! class_exists( 'AdminPageFramework_Widget_Documentation' ) ) :
+/**
+ * Provides an abstract base to create widgets and their forms.
+ * 
+ * @abstract
+ * @since       3.3.0
+ * @package     AdminPageFramework
+ * @subpackage  Widget
+ * @heading     
+ */
+abstract class AdminPageFramework_Widget_Documentation {}
+endif;
