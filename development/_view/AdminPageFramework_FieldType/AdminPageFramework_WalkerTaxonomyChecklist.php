d6e34963ec717d5b514142b03a2928e2643c7a62
Michael Uno <michael@michaeluno.jp>
2014-10-22 22:35:15 +0900

    - Fixed code formatting. - Updated inline documentation.

--- development/_view/AdminPageFramework_FieldType/AdminPageFramework_WalkerTaxonomyChecklist.php
+++ development/_view/AdminPageFramework_FieldType/AdminPageFramework_WalkerTaxonomyChecklist.php
@@ -12,49 +12,53 @@ if ( ! class_exists( 'AdminPageFramework_WalkerTaxonomyChecklist' ) ) :
  * 
  * Used for the wp_list_categories() function to render category hierarchical checklist.
  * 
- * @see Walker : wp-includes/class-wp-walker.php
- * @see Walker_Category : wp-includes/category-template.php
- * @since 2.0.0
- * @since 2.1.5 Added the tag_id key to the argument array. Changed the format of 'id' and 'for' attribute of the input and label tags.
- * @extends Walker_Category
- * @package AdminPageFramework
- * @subpackage FieldType
+ * @see             Walker : wp-includes/class-wp-walker.php
+ * @see             Walker_Category : wp-includes/category-template.php
+ * @since           2.0.0
+ * @since           2.1.5     Added the tag_id key to the argument array. Changed the format of 'id' and 'for' attribute of the input and label tags.
+ * @extends         Walker_Category
+ * @package         AdminPageFramework
+ * @subpackage      FieldType
  * @internal
  */
 class AdminPageFramework_WalkerTaxonomyChecklist extends Walker_Category {
     
     /**
-     * Modifies the variable stirng the opening 'li' tag of the list.
+     * Modifies the variable string the opening 'li' tag of the list.
+     * 
+     * @param       string      $sOutput        
+     * @param       object      $oTerm        
+     * @param       integer     $iDepth
+     * @param       array       $aArgs          The argument passed from the field output.
+     * <h4>Structure</h4>
+     * <code>
+     *      'show_option_all' => '', 
+     *      'show_option_none' => __('No categories'),
+     *      'orderby' => 'name', 
+     *      'order' => 'ASC',
+     *      'style' => 'list',
+     *      'show_count' => 0, 
+     *      'hide_empty' => 1,
+     *      'use_desc_for_title' => 1, 
+     *      'child_of' => 0,
+     *      'feed' => '', 
+     *      'feed_type' => '',
+     *      'feed_image' => '', 
+     *      'exclude' => '',
+     *      'exclude_tree' => '', 
+     *      'current_category' => 0,
+     *      'hierarchical' => true, 
+     *      'title_li' => __( 'Categories' ),
+     *      'echo' => 1, 
+     *      'depth' => 0,
+     *      'taxonomy' => 'category' // 'post_tag' or any other registered taxonomy slug will work.
+     *      [class] => categories
+     *      [has_children] => 1
+     * </code>
+     * @param       integer     $iCurrentObjectID
      */
-    function start_el( &$sOutput, $oCategory, $iDepth=0, $aArgs=array(), $iCurrentObjectID=0 ) {
-        
-        /*    
-             $aArgs keys:
-            'show_option_all' => '', 
-            'show_option_none' => __('No categories'),
-            'orderby' => 'name', 
-            'order' => 'ASC',
-            'style' => 'list',
-            'show_count' => 0, 
-            'hide_empty' => 1,
-            'use_desc_for_title' => 1, 
-            'child_of' => 0,
-            'feed' => '', 
-            'feed_type' => '',
-            'feed_image' => '', 
-            'exclude' => '',
-            'exclude_tree' => '', 
-            'current_category' => 0,
-            'hierarchical' => true, 
-            'title_li' => __( 'Categories' ),
-            'echo' => 1, 
-            'depth' => 0,
-            'taxonomy' => 'category' // 'post_tag' or any other registered taxonomy slug will work.
-
-            [class] => categories
-            [has_children] => 1
-        */
-        
+    function start_el( &$sOutput, $oTerm, $iDepth=0, $aArgs=array(), $iCurrentObjectID=0 ) {
+       
         $aArgs = $aArgs + array(
             'name'              => null,
             'disabled'          => null,
@@ -66,12 +70,12 @@ class AdminPageFramework_WalkerTaxonomyChecklist extends Walker_Category {
         );
         
         // Local variables
-        $_iID            = $oCategory->term_id;
+        $_iID            = $oTerm->term_id;
         $_sTaxonomySlug  = empty( $aArgs['taxonomy'] ) ? 'category' : $aArgs['taxonomy'];
         $_sID            = "{$aArgs['input_id']}_{$_sTaxonomySlug}_{$_iID}";
 
         // Post count
-        $_sPostCount     = $aArgs['show_post_count'] ? " <span class='font-lighter'>(" . $oCategory->count . ")</span>" : '';
+        $_sPostCount     = $aArgs['show_post_count'] ? " <span class='font-lighter'>(" . $oTerm->count . ")</span>" : '';
         
         // Attributes
         $_aInputAttributes = isset( $_aInputAttributes[ $_iID ] ) 
@@ -93,7 +97,7 @@ class AdminPageFramework_WalkerTaxonomyChecklist extends Walker_Category {
                 . "<label for='{$_sID}' class='taxonomy-checklist-label'>"
                     . "<input value='0' type='hidden' name='{$aArgs['name']}[{$_iID}]' class='apf_checkbox' />"
                     . "<input " . AdminPageFramework_WPUtility::generateAttributes( $_aInputAttributes ) . " />"
-                    . esc_html( apply_filters( 'the_category', $oCategory->name ) ) 
+                    . esc_html( apply_filters( 'the_category', $oTerm->name ) ) 
                     . $_sPostCount
                 . "</label>";    
             /* no need to close the </li> tag since it is dealt in the end_el() method. */
