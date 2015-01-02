beeee49e8fed2bbf21fc742f6417a8b98bc7c787
Michael Uno <michael@michaeluno.jp>
2015-01-02 17:59:13 +0900

    Added a warning to use the old minified version.

--- library/admin-page-framework.min.php
+++ library/admin-page-framework.min.php
@@ -1,4 +1,8 @@
 <?php 
-// 3.4.5.1+ The minified verion is removed.
+// 3.4.5.1+ The minified verion was removed.
+trigger_error( 
+    'Admin Page Framework: The minified script is temporarily deprecated due to an issue with wordpress.org. Use the minifier via Dashboard -> Admin Page Framework -> Tool -< Minified Version and place the file in your convenient location.',
+    E_USER_WARNING  // E_USER_NOTICEs
+);
 include_once( dirname( dirname( __FILE__ ) ) . '/development/admin-page-framework.php' );
 	
