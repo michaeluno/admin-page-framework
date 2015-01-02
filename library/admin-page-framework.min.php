72774898c18d55a98db245f0c9cef8354ce88a7c
Michael Uno <michael@michaeluno.jp>
2015-01-02 22:17:42 +0900

    Made the warning only appear in the admin area and after the header is sent as it was causing a situation that the user cannot navigate  the pages.

--- library/admin-page-framework.min.php
+++ library/admin-page-framework.min.php
@@ -1,8 +1,17 @@
-<?php 
-// 3.4.5.1+ The minified verion was removed.
-trigger_error( 
-    'Admin Page Framework: The minified script is temporarily deprecated due to an issue with wordpress.org. Use the minifier via Dashboard -> Admin Page Framework -> Tool -< Minified Version and place the file in your convenient location.',
-    E_USER_WARNING  // E_USER_NOTICEs
-);
+<?php
+// 3.4.5.1+ The minified version was removed.
+class AdminPageFramework_DeprecatedWarning_MinifiedVersion {
+
+    public function __construct() {
+        add_action( 'admin_notices', array( $this, '_replyToShowWarnings' ) );
+    }
+    public function _replyToShowWarnings() {
+        trigger_error( 
+            'Admin Page Framework: The minified script is temporarily deprecated due to an issue with wordpress.org. Use the minifier via Dashboard -> Admin Page Framework -> Tool -> Minifier and place the file in your convenient location.',
+            E_USER_WARNING  // E_USER_NOTICEs
+        );    
+    }
+}
+new AdminPageFramework_DeprecatedWarning_MinifiedVersion;
 include_once( dirname( dirname( __FILE__ ) ) . '/development/admin-page-framework.php' );
 	
