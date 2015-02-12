5f9ba0566e6ae940d88b050bea4fecda1618881f
Michael Uno <michael@michaeluno.jp>
2015-02-12 16:48:11 +0900

    Refactored a method to reduce complexity.

--- development/factory/AdminPageFramework/controller/AdminPageFramework_Resource_Page.php
+++ development/factory/AdminPageFramework/controller/AdminPageFramework_Resource_Page.php
@@ -154,32 +154,7 @@ class AdminPageFramework_Resource_Page extends AdminPageFramework_Resource_Base 
      * @internal
      */    
     public function _enqueueStyle( $sSRC, $sPageSlug='', $sTabSlug='', $aCustomArgs=array() ) {
-        
-        $sSRC = trim( $sSRC );
-        if ( empty( $sSRC ) ) { return ''; }
-        $sSRC       = $this->oUtil->resolveSRC( $sSRC );
-        
-        // Setting the key based on the url prevents duplicate items
-        $_sSRCHash  = md5( $sSRC ); 
-        if ( isset( $this->oProp->aEnqueuingStyles[ $_sSRCHash ] ) ) { return ''; } 
-
-        $this->oProp->aEnqueuingStyles[ $_sSRCHash ] = $this->oUtil->uniteArrays( 
-            ( array ) $aCustomArgs,
-            array(     
-                'sSRC'      => $sSRC,
-                'sPageSlug' => $sPageSlug,
-                'sTabSlug'  => $sTabSlug,
-                'sType'     => 'style',
-                'handle_id' => 'style_' . $this->oProp->sClassName . '_' .  ( ++$this->oProp->iEnqueuedStyleIndex ),
-            ),
-            self::$_aStructure_EnqueuingResources
-        );
-        
-        // Store the attributes in another container by url.
-        $this->oProp->aResourceAttributes[ $this->oProp->aEnqueuingStyles[ $_sSRCHash ]['handle_id'] ] = $this->oProp->aEnqueuingStyles[ $_sSRCHash ]['attributes'];
-        
-        return $this->oProp->aEnqueuingStyles[ $_sSRCHash ][ 'handle_id' ];
-        
+        return $this->_enqueueResourceByType( $sSRC, $sPageSlug, $sTabSlug, $aCustomArgs, 'style' );        
     }
     
     /**
@@ -219,34 +194,80 @@ class AdminPageFramework_Resource_Page extends AdminPageFramework_Resource_Base 
      * @internal
      */
     public function _enqueueScript( $sSRC, $sPageSlug='', $sTabSlug='', $aCustomArgs=array() ) {
-        
-        $sSRC       = trim( $sSRC );
-        if ( empty( $sSRC ) ) { return ''; }
-        $sSRC       = $this->oUtil->resolveSRC( $sSRC );
-
-        // setting the key based on the url prevents duplicate items
-        $_sSRCHash  = md5( $sSRC );
-        if ( isset( $this->oProp->aEnqueuingScripts[ $_sSRCHash ] ) ) { return ''; } 
-        
-        $this->oProp->aEnqueuingScripts[ $_sSRCHash ] = $this->oUtil->uniteArrays( 
-            ( array ) $aCustomArgs,
-            array(     
-                'sPageSlug' => $sPageSlug,
-                'sTabSlug'  => $sTabSlug,
-                'sSRC'      => $sSRC,
-                'sType'     => 'script',
-                'handle_id' => 'script_' . $this->oProp->sClassName . '_' .  ( ++$this->oProp->iEnqueuedScriptIndex ),
-            ),
-            self::$_aStructure_EnqueuingResources
-        );
-
-        // Store the attributes in another container by url.
-        $this->oProp->aResourceAttributes[ $this->oProp->aEnqueuingScripts[ $_sSRCHash ]['handle_id'] ] = $this->oProp->aEnqueuingScripts[ $_sSRCHash ]['attributes'];
-        
-        return $this->oProp->aEnqueuingScripts[ $_sSRCHash ][ 'handle_id' ];
-        
+        return $this->_enqueueResourceByType( $sSRC, $sPageSlug, $sTabSlug, $aCustomArgs, 'script' );
     }
+        /**
+         * Enqueues a resouce.
+         * 
+         * @since       3.5.3
+         * @param       string      $sSRC           The source path or url.
+         * @param       string      $sPageSlug      The page slug that the item gets enqueued.
+         * @param       string      $sTabSlug       The page slug that the item gets enqueued.
+         * @param       array       $aCustomArgs    A custom argument array.
+         * @param       string      $sType          Accepts 'style' or 'script'
+         */
+        private function _enqueueResourceByType( $sSRC, $sPageSlug='', $sTabSlug='', $aCustomArgs=array(), $sType='style' ) {
+            
+            $sSRC       = trim( $sSRC );
+            if ( empty( $sSRC ) ) { return ''; }
+            $sSRC       = $this->oUtil->resolveSRC( $sSRC );
 
+            // Get the property name for the type
+            $_sContainerPropertyName     = $this->_getContainerPropertyNameByType( $sType );
+            $_sEnqueuedIndexPropertyName = $this->_getEnqueuedIndexPropertyNameByType( $sType );
+            
+            // setting the key based on the url prevents duplicate items
+            $_sSRCHash  = md5( $sSRC );
+            if ( isset( $this->oProp->{$_sContainerPropertyName}[ $_sSRCHash ] ) ) { 
+                return ''; 
+            } 
+            
+            $this->oProp->{$_sContainerPropertyName}[ $_sSRCHash ] = $this->oUtil->uniteArrays( 
+                ( array ) $aCustomArgs,
+                array(     
+                    'sPageSlug' => $sPageSlug,
+                    'sTabSlug'  => $sTabSlug,
+                    'sSRC'      => $sSRC,
+                    'sType'     => $sType,
+                    'handle_id' => $sType . '_' . $this->oProp->sClassName . '_' .  ( ++$this->oProp->{$_sEnqueuedIndexPropertyName} ),
+                )
+                + self::$_aStructure_EnqueuingResources
+            );
+
+            // Store the attributes in another container by url.
+            $this->oProp->aResourceAttributes[ $this->oProp->{$_sContainerPropertyName}[ $_sSRCHash ]['handle_id'] ] = $this->oProp->{$_sContainerPropertyName}[ $_sSRCHash ]['attributes'];
+            
+            return $this->oProp->{$_sContainerPropertyName}[ $_sSRCHash ][ 'handle_id' ];
+            
+        }
+            /**
+             * Returns the property name that contains the information of resources by type.
+             * @since       3.5.3
+             * @return      string      the property name that contains the information of resources by type.
+             */
+            private function _getContainerPropertyNameByType( $sType ) {
+                switch ( $sType ) {
+                    default:
+                    case 'style':
+                        return 'aEnqueuingStyles';
+                    case 'script':
+                        return 'aEnqueuingScripts';
+                }
+            }
+            /**
+             * Returns the property name that contains the added count of resources by type.
+             * @since       3.5.3
+             * @return      string      the property name that contains the added count of resources by type.
+             */            
+            private function _getEnqueuedIndexPropertyNameByType( $sType ) {
+                switch ( $sType ) {
+                    default:
+                    case 'style':
+                        return 'iEnqueuedStyleIndex';
+                    case 'script':
+                        return 'iEnqueuedScriptIndex';
+                }
+            }            
     /**
      * Enqueues a style source without conditions.
      * 
