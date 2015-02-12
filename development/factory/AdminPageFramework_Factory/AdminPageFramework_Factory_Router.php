5212e65c614b37321823f02596d67de270bfe40a
Michael Uno <michael@michaeluno.jp>
2015-02-13 02:00:55 +0900

    Fixed an issue that using list() caused undefined index warnings.

--- development/factory/AdminPageFramework_Factory/AdminPageFramework_Factory_Router.php
+++ development/factory/AdminPageFramework_Factory/AdminPageFramework_Factory_Router.php
@@ -3,7 +3,7 @@
  * Admin Page Framework
  * 
  * http://en.michaeluno.jp/admin-page-framework/
- * Copyright (c) 2013-2014 Michael Uno; Licensed MIT
+ * Copyright (c) 2013-2015 Michael Uno; Licensed MIT
  * 
  */
 
@@ -371,7 +371,7 @@ abstract class AdminPageFramework_Factory_Router {
      */
     public function __call( $sMethodName, $aArgs=null ) {    
          
-        list( $_mFirstArg )     = $aArgs;
+        $_mFirstArg = $this->oUtil->getElement( $aArgs, 0 );
         
         switch ( $sMethodName ) {
             case 'validate':
