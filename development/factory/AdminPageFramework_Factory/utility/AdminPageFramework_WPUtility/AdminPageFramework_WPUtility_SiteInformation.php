de5a9c7d85a99a524c5020baf6464a3f52ee3010
Michael Uno <michael@michaeluno.jp>
2015-02-15 12:23:01 +0900

    Added some methods that can help reduce conditional statements.

--- development/factory/AdminPageFramework_Factory/utility/AdminPageFramework_WPUtility/AdminPageFramework_WPUtility_SiteInformation.php
+++ development/factory/AdminPageFramework_Factory/utility/AdminPageFramework_WPUtility/AdminPageFramework_WPUtility_SiteInformation.php
@@ -24,9 +24,36 @@ class AdminPageFramework_WPUtility_SiteInformation extends AdminPageFramework_WP
      * @since       3.5.3
      * @return      boolean
      */
-    static public function isDebugMode() {
-        return defined( 'WP_DEBUG' ) && WP_DEBUG;
+    static public function isDebugModeEnabled() {
+        return ( bool ) defined( 'WP_DEBUG' ) && WP_DEBUG;
     }
-       
+     
+    /**
+     * Checks if the site enables debug logs.
+     * @since       3.5.3
+     * @return      boolean
+     */
+    static public function isDebugLogEnabled() {
+        return ( bool ) defined( 'WP_DEBUG_LOG' ) && WP_DEBUG_LOG;
+    }    
     
+    /**
+     * Checks if the site debug display mode is enabled.
+     * @since       3.5.3
+     * @return      boolean
+     */
+    static public function isDebugDisplayEnabled() {
+        return ( bool ) defined( 'WP_DEBUG_DISPLAY' ) && WP_DEBUG_DISPLAY;
+    }
+    
+    /**
+     * Returns the site language.
+     * 
+     * @since       3.5.3
+     * @return      string      The site language.
+     */
+    static public function getSiteLanguage( $sDefault='en_US' ) {
+        return defined( 'WPLANG' ) && WPLANG ? WPLANG : $sDefault;
+    }
+        
 }
