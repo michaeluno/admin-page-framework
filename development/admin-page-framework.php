3432a6b7aa81ac1113bba308d3bd930c554ccbd7
Michael Uno <michael@michaeluno.jp>
2015-05-21 14:48:58 +0900

    - Advanced the version to 3.5.8b03 to help identify the changes. - Beautified the code.

--- development/admin-page-framework.php
+++ development/admin-page-framework.php
@@ -29,11 +29,11 @@
  * @download_latest     https://github.com/michaeluno/admin-page-framework/archive/master.zip
  * @download_stable     http://downloads.wordpress.org/plugin/admin-page-framework.latest-stable.zip
  * @catchcopy           The framework for all WordPress developers.
- * @version             3.5.8b02
+ * @version             3.5.8b03
  */
 abstract class AdminPageFramework_Registry_Base {
     
-    const VERSION       = '3.5.8b02'; // <--- DON'T FORGET TO CHANGE THIS AS WELL!!
+    const VERSION       = '3.5.8b03'; // <--- DON'T FORGET TO CHANGE THIS AS WELL!!
     const NAME          = 'Admin Page Framework';
     const DESCRIPTION   = 'Facilitates WordPress plugin and theme development.';
     const URI           = 'http://en.michaeluno.jp/admin-page-framework';
