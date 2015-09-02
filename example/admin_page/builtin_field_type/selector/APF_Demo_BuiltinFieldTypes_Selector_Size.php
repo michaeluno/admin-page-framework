04adf2e58845940a2d8d7301e139b34af732f06b
Michael Uno <michael@michaeluno.jp>
2015-09-02 20:34:19 +0900

    Tweaked the size field output with the `after_label` argument.

--- example/admin_page/builtin_field_type/selector/APF_Demo_BuiltinFieldTypes_Selector_Size.php
+++ example/admin_page/builtin_field_type/selector/APF_Demo_BuiltinFieldTypes_Selector_Size.php
@@ -150,24 +150,29 @@ class APF_Demo_BuiltinFieldTypes_Selector_Size {
                     'default'   => array( 'size' => 30, 'unit' => 'mb' ),
                 ),     
             ),
-            array( // Multiple Size Fields
-                'field_id'      => 'sizes_field_multiple_labels',
-                'title'         => __( 'Sub-fields by Label', 'admin-page-framework-loader' ),
-                'type'          => 'size',
-                'label'         => array(
-                    'weight'        => __( 'Weight', 'admin-page-framework-loader' ),
-                    'length'        => __( 'Length', 'admin-page-framework-loader' ),
-                    'size'          => __( 'File Size', 'admin-page-framework-loader' ),
+            array( // Multiple Size Fields by Label
+                'field_id'          => 'sizes_field_multiple_labels',
+                'title'             => __( 'Sub-fields by Label', 'admin-page-framework-loader' ),
+                'type'              => 'size',
+                'label'             => array(
+                    'weight'    => __( 'Weight', 'admin-page-framework-loader' ),
+                    'length'    => __( 'Length', 'admin-page-framework-loader' ),
+                    'size'      => __( 'File Size', 'admin-page-framework-loader' ),
                 ),
-                'units'         => array(
-                    'weight'        => array( 'mg'=>'mg', 'g'=>'g', 'kg'=>'kg' ),
-                    'length'        => array( 'cm'=>'cm', 'mm'=>'mm', 'm'=>'m' ),
-                    'size'          => array( 'b'=>'b', 'kb'=>'kb', 'mb'=>'mb', 'gb' => 'gb', 'tb' => 'tb' ),
+                'after_label'       => array(
+                    'weight'    => '<br />',
+                    'length'    => '<br />',
+                    'size'      => '<br />',
+                ),                
+                'units'             => array(
+                    'weight'    => array( 'mg'=>'mg', 'g'=>'g', 'kg'=>'kg' ),
+                    'length'    => array( 'cm'=>'cm', 'mm'=>'mm', 'm'=>'m' ),
+                    'size'      => array( 'b'=>'b', 'kb'=>'kb', 'mb'=>'mb', 'gb' => 'gb', 'tb' => 'tb' ),
                 ),
-                'default'       => array(
-                    'weight'         => array( 'size' => 15, 'unit' => 'g' ),
-                    'length'        => array( 'size' => 100, 'unit' => 'mm' ),               
-                    'size'          => array( 'size' => 30, 'unit' => 'mb' ),
+                'default'           => array(
+                    'weight'    => array( 'size' => 15, 'unit' => 'g' ),
+                    'length'    => array( 'size' => 100, 'unit' => 'mm' ),               
+                    'size'      => array( 'size' => 30, 'unit' => 'mb' ),
                 ),                
             ),            
             array( // Repeatable Size Fields
