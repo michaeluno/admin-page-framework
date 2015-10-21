da253cf985e23fa70c42bcdc4ff29876412eecf1
Michael Uno <michael@michaeluno.jp>
2015-10-21 14:48:59 +0900

    - Advanced the version to help identify the changes. - Generated the beautified code.

--- library/admin-page-framework/admin-page-framework.php
+++ library/admin-page-framework/admin-page-framework.php
@@ -1,6 +1,6 @@
 <?php
 abstract class AdminPageFramework_Registry_Base {
-    const VERSION = '3.6.0b14';
+    const VERSION = '3.6.0b15';
     const NAME = 'Admin Page Framework';
     const DESCRIPTION = 'Facilitates WordPress plugin and theme development.';
     const URI = 'http://en.michaeluno.jp/admin-page-framework';
