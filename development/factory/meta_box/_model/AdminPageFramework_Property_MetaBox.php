d77ef03248945ecd2819527869a3721112bd31ea
Michael Uno <michael@michaeluno.jp>
2015-11-13 08:57:04 +0900

    - Cleaned doc-blocks by aligning indentations.

--- development/factory/meta_box/_model/AdminPageFramework_Property_MetaBox.php
+++ development/factory/meta_box/_model/AdminPageFramework_Property_MetaBox.php
@@ -22,8 +22,8 @@ class AdminPageFramework_Property_MetaBox extends AdminPageFramework_Property_Ba
     
     /**
      * Defines the property type.
-     * @remark Setting the property type helps to check whether some components are loaded such as scripts that can be reused per a class type basis.
-     * @since 3.0.0
+     * @remark      Setting the property type helps to check whether some components are loaded such as scripts that can be reused per a class type basis.
+     * @since       3.0.0
      * @internal
      */
     public $_sPropertyType = 'post_meta_box';
@@ -31,8 +31,8 @@ class AdminPageFramework_Property_MetaBox extends AdminPageFramework_Property_Ba
     /**
      * Stores the meta box id(slug).
      * 
-     * @since 2.0.0
-     * @since 2.1.0 Moved from the meta box class.
+     * @since       2.0.0
+     * @since       2.1.0       Moved from the meta box class.
      * @var string
      */     
     public $sMetaBoxID ='';
@@ -40,8 +40,8 @@ class AdminPageFramework_Property_MetaBox extends AdminPageFramework_Property_Ba
     /**
      * Stores the meta box title.
      * 
-     * @since 2.0.0
-     * @since 2.1.0 Moved from the meta box class.
+     * @since       2.0.0
+     * @since       2.1.0       Moved from the meta box class.
      * @var string
      */ 
     public $sTitle = '';
@@ -51,8 +51,8 @@ class AdminPageFramework_Property_MetaBox extends AdminPageFramework_Property_Ba
      * 
      * This is used in the meta box class for post type pages.
      * 
-     * @since 2.0.0
-     * @since 2.1.0 Moved from the meta box class.
+     * @since       2.0.0
+     * @since       2.1.0       Moved from the meta box class.
      * @var array
      */     
     public $aPostTypes = array();
@@ -62,52 +62,52 @@ class AdminPageFramework_Property_MetaBox extends AdminPageFramework_Property_Ba
      * 
      * This is used in the meta box class for pages.
      * 
-     * @since 3.0.0
+     * @since       3.0.0
      */
     public $aPages = array();
     
     /**
      * Stores the parameter value, context, for the add_meta_box() function. 
      * 
-     * @since 2.0.0
-     * @since 2.1.0 Moved from the meta box class.
-     * @remark The value can be either 'normal', 'advanced', or 'side'.
-     * @var string
-     * @see http://codex.wordpress.org/Function_Reference/add_meta_box#Parameters
+     * @since       2.0.0
+     * @since       2.1.0       Moved from the meta box class.
+     * @remark      The value can be either 'normal', 'advanced', or 'side'.
+     * @var         string
+     * @see         http://codex.wordpress.org/Function_Reference/add_meta_box#Parameters
      */ 
     public $sContext = 'normal';
 
     /**
      * Stores the parameter value, priority, for the add_meta_box() function. 
      * 
-     * @since 2.0.0
-     * @since 2.1.0 Moved from the meta box class.
-     * @remark The value can be either 'high', 'core', 'default' or 'low'.
-     * @var string
-     * @see http://codex.wordpress.org/Function_Reference/add_meta_box#Parameters
+     * @since       2.0.0
+     * @since       2.1.0       Moved from the meta box class.
+     * @remark      The value can be either 'high', 'core', 'default' or 'low'.
+     * @var         string
+     * @see         http://codex.wordpress.org/Function_Reference/add_meta_box#Parameters
      */     
     public $sPriority = 'default';
     
     /**
      * Stores the extended class name.
      * 
-     * @since 2.0.0
-     * @since 2.1.0 Moved from the meta box class.
+     * @since       2.0.0
+     * @since       2.1.0       Moved from the meta box class.
      */ 
     public $sClassName = '';
     
     /**
      * Stores the capability for displayable elements.
      * 
-     * @since 2.0.0
-     * @since 2.1.0 Moved from the meta box class.
+     * @since       2.0.0
+     * @since       2.1.0      Moved from the meta box class.
      */     
     public $sCapability = 'edit_posts';
         
     /**
      * Stores option values for form fields.
-     * @since 2.0.0
-     * @since 2.1.0 Moved from the meta box class.
+     * @since       2.0.0
+     * @since       2.1.0       Moved from the meta box class.
      * @internal
      * @remark      Do not set this here to let the overload method _get() to be triggered when it is called.
      */  
@@ -115,42 +115,50 @@ class AdminPageFramework_Property_MetaBox extends AdminPageFramework_Property_Ba
     
     /**
      * Stores the media uploader box's title.
-     * @since 2.0.0
-     * @since 2.1.0 Moved from the meta box class.
+     * @since       2.0.0
+     * @since       2.1.0       Moved from the meta box class.
      * @internal
      */ 
     public $sThickBoxTitle = '';
     
     /**
      * Stores the label for for the "Insert to Post" button in the media uploader box.
-     * @since 2.0.0
-     * @since 2.1.0 Moved from the meta box class.
+     * @since       2.0.0
+     * @since       2.1.0       Moved from the meta box class.
      * @internal
      */     
     public $sThickBoxButtonUseThis = '';
 
     /**
      * Stores text to insert into the contextual help tab.
-     * @since 2.1.0
+     * @since       2.1.0
      */ 
     public $aHelpTabText = array();
     
     /**
      * Stores text to insert into the sidebar of a contextual help tab.
-     * @since 2.1.0
+     * @since       2.1.0
      */ 
     public $aHelpTabTextSide = array();
         
     /**
      * Defines the fields type.
-     * @since 3.0.4
+     * @since       3.0.4
      * @internal
      */
     public  $sFieldsType = 'post_meta_box';
     
     public function __construct( $oCaller, $sClassName, $sCapability='edit_posts', $sTextDomain='admin-page-framework', $sFieldsType='post_meta_box' ) {
         
-        parent::__construct( $oCaller, null, $sClassName, $sCapability, $sTextDomain, $sFieldsType ); // meta boxes don't need the caller script path.
+        // meta boxes don't need the caller script path.
+        parent::__construct( 
+            $oCaller, 
+            null, 
+            $sClassName, 
+            $sCapability, 
+            $sTextDomain, 
+            $sFieldsType 
+        ); 
             
     }
     
