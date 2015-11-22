c468d295d9d3bf8fc8e5da4b65ee8ba834fe25cd
Michael Uno <michael@michaeluno.jp>
2015-11-22 14:59:44 +0900

    - Added an smooth animation effect to admin notices.

--- development/factory/_abstract/utility/admin_notice/AdminPageFramework_AdminNotice___Script.php
+++ development/factory/_abstract/utility/admin_notice/AdminPageFramework_AdminNotice___Script.php
@@ -43,11 +43,48 @@ class AdminPageFramework_AdminNotice___Script extends AdminPageFramework_Factory
         return <<<JAVASCRIPTS
 ( function( $ ) {
 
+    $.fn.slideDownTransition = function() {
+        return this.each(function() {
+            var _el = $( this );
+            _el.removeClass('height-transition-hidden');
+
+            // temporarily make visible to get the size
+            _el.css('max-height', 'none');
+            var height = _el.outerHeight();
+
+            // reset to 0 then animate with small delay
+            _el.css('max-height', '0');
+
+            setTimeout(function() {
+                _el.css({
+                    'max-height': height
+                });
+            }, 1 );
+        });
+    };
+
     jQuery( document ).ready( function() {         
 
         var _oAdminNotices = jQuery( '.admin-page-framework-settings-notice-message' );
         if ( _oAdminNotices.length ) {
-            jQuery( _oAdminNotices ).fadeIn( 'slow' );
+                    
+            // animation of the `slideDown()` method does not work well when the target element has a margin
+            // so enclose the elemnet in a new container and apply the margin to it.
+            var _iMarginTop     = jQuery( _oAdminNotices ).css( 'margin-top' );
+            var _iMarginBottom  = jQuery( _oAdminNotices ).css( 'margin-bottom' );
+            var _oContainer     = jQuery( _oAdminNotices )
+                .css( 'margin', '0' )   // prevents jumpy animation
+                .wrap( "<div class='admin-page-framework-admin-notice-animation-container'></div>" );
+            _oContainer.css( 'margin-top', _iMarginTop );
+            _oContainer.css( 'margin-bottom', _iMarginBottom );
+            
+            // Now animate.
+            jQuery( _oAdminNotices )
+                .css( 'visibility', 'hidden' )
+                .slideDown( 800 )
+                .css( {opacity: 0, visibility: 'visible'})
+                .animate( {opacity: 1}, 800 );
+                
         }
 
     });              
--- readme.txt
+++ readme.txt
@@ -431,7 +431,7 @@ Check out [the issues](https://github.com/michaeluno/admin-page-framework/issues
 
 = 3.7.0 =
 - Added the ability of nesting sections.
-- Tweaked the style of admin messages.
+- Added an smooth animation effect to admin notices.
 - Changed the factory class name of the page meta box from `AdminPageFramework_MetaBox_Page` to `AdminPageFramework_PageMetaBox`.
 
 = 3.6.5 - 2015/11/21 =
