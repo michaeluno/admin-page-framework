c4b9a159a53bc15d2761a0b8369e68704991eead
Michael Uno <michael@michaeluno.jp>
2015-11-24 08:26:39 +0900

    - Tweaked the style of tool tips.

--- development/factory/_abstract/form/_view/css/AdminPageFramework_Form_View___CSS_ToolTip.php
+++ development/factory/_abstract/form/_view/css/AdminPageFramework_Form_View___CSS_ToolTip.php
@@ -20,6 +20,7 @@ class AdminPageFramework_Form_View___CSS_ToolTip extends AdminPageFramework_Form
     /**
      * @since       DEVVER
      * @return      string
+     * @see         http://www.menucool.com/tooltip/css-tooltip
      */
     protected function _get() {
 
@@ -27,6 +28,7 @@ class AdminPageFramework_Form_View___CSS_ToolTip extends AdminPageFramework_Form
 
 /* Inside Field Title */        
 th > label > span > .admin-page-framework-form-tooltip {
+    margin-top: 1px;
     float: right;
 }
 .postbox-container th > label > span > .admin-page-framework-form-tooltip {
@@ -39,10 +41,6 @@ th > label > span > .admin-page-framework-form-tooltip {
 .admin-page-framework-collapsible-title a.admin-page-framework-form-tooltip {
     margin-left: 1em;
 }
-/* Section title fields */
-/* .admin-page-framework-collapsible-section-title > .admin-page-framework-form-tooltip-container {
-    margin-top: 0.32em;
-} */
 
 /* When it is placed inside h2, h3, h4, the tooltip text becomes large so avoid that */
 a.admin-page-framework-form-tooltip > .admin-page-framework-form-tooltip-content {
@@ -58,16 +56,13 @@ a.admin-page-framework-form-tooltip > .admin-page-framework-form-tooltip-content
 .admin-page-framework-section-tab.nav-tab.active a.admin-page-framework-form-tooltip {
     color: #A8A8A8;
 }
-/* .admin-page-framework-form-tooltip-container {
-    display: inline;
-} */
+
 /* Dashicon vertical alignment */
 .admin-page-framework-form-tooltip > span {
+    margin-bottom: 1px;
     vertical-align: middle;
-    
 }
 
-/* @see http://www.menucool.com/tooltip/css-tooltip */
 a.admin-page-framework-form-tooltip {
     outline: none; 
     text-decoration: none;
@@ -84,13 +79,17 @@ a.admin-page-framework-form-tooltip:hover {
     text-decoration: none;
 } 
 a.admin-page-framework-form-tooltip > span.admin-page-framework-form-tooltip-content {
-    z-index: 100000;
+
     display: none; 
     padding: 14px 20px 14px;
     margin-top: -30px; 
     margin-left: 28px;
     width: 400px; 
     line-height:16px;
+    
+    /* High z-index is required to appear over the left side bar menu */
+    z-index: 100000;
+    
 }
 a.admin-page-framework-form-tooltip:hover > span.admin-page-framework-form-tooltip-content{
     display: inline; 
@@ -98,17 +97,22 @@ a.admin-page-framework-form-tooltip:hover > span.admin-page-framework-form-toolt
     color: #111;
     border:1px solid #DCA; 
     background: #FFFFF4;
+    
+    /* Adjust the position of the tooltip here */
     /* margin-left: -300px; */
 }
-.callout {
+
+/* Balloon Style */
+/* .callout {
     z-index: 200000;
     position: absolute;
     top: 30px;
     border: 0;
     left: -12px;
 }
+ */
 
-/* CSS3 extras */
+/* Tooltip Box Shadow */
 a.admin-page-framework-form-tooltip > span.admin-page-framework-form-tooltip-content {
     border-radius:4px;
     box-shadow: 5px 5px 8px #CCC;
