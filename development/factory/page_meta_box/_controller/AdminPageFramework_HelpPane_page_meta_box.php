e754b300bf3047e94d75aa8a4f777d8cdb6d57a0
Michael Uno <michael@michaeluno.jp>
2016-01-09 23:40:26 +0900

    - Cleaned unused lines.

--- development/factory/page_meta_box/_controller/AdminPageFramework_HelpPane_page_meta_box.php
+++ development/factory/page_meta_box/_controller/AdminPageFramework_HelpPane_page_meta_box.php
@@ -10,41 +10,10 @@
 /**
  * Provides methods to manipulate the contextual help tab .
  *
- * @package AdminPageFramework
- * @subpackage HelpPane
- * @since 3.0.4    
- * @extends AdminPageFramework_HelpPane_Base
+ * @package     AdminPageFramework
+ * @subpackage  HelpPane
+ * @since       3.0.4    
+ * @extends     AdminPageFramework_HelpPane_Base
  * @internal
  */
-class AdminPageFramework_HelpPane_page_meta_box extends AdminPageFramework_HelpPane_Base {
-
-    /**
-     * Determines whether the currently loaded page belongs to the meta box page.
-     * 
-     * @sicne       3.0.4
-     * @internal
-     * @deprecated  3.7.10
-     */
-/*     protected function _isInThePage() {
-
-        if ( ! $this->oProp->bIsAdmin ) {
-            return false;
-        }
-
-        if ( ! isset( $_GET[ 'page' ] ) ) {
-            return false;
-        }
-        
-        if ( ! $this->oProp->isPageAdded( $_GET[ 'page' ] ) ) {
-            return false;
-        }
-        
-        if ( ! isset( $_GET[ 'tab' ] ) ) {
-            return true;
-        }
-        
-        return $this->oProp->isCurrentTab( $_GET[ 'tab' ] );
-        
-    }
-     */
-}
+class AdminPageFramework_HelpPane_page_meta_box extends AdminPageFramework_HelpPane_Base {}
