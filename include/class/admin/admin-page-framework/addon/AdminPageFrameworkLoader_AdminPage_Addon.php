c25565e274449af2c57f55ad296f4265d3fea299
Michael Uno <michael@michaeluno.jp>
2016-01-15 03:35:47 +0900

    Changed the timing to add tabs to improve performance in the common admin area.

--- include/class/admin/admin-page-framework/addon/AdminPageFrameworkLoader_AdminPage_Addon.php
+++ include/class/admin/admin-page-framework/addon/AdminPageFrameworkLoader_AdminPage_Addon.php
@@ -15,24 +15,18 @@
  * @extends     AdminPageFrameworkLoader_AdminPage_Page_Base
  */
 class AdminPageFrameworkLoader_AdminPage_Addon extends AdminPageFrameworkLoader_AdminPage_Page_Base {
-
-    /**
-     * A user constructor.
-     * 
-     * @since       3.5.3
-     * @return      void
-     */
-    public function construct( $oFactory ) {
+    
+    public function replyToLoadPage( $oFactory ) {
         
         new AdminPageFrameworkLoader_AdminPage_Addon_Top( 
-            $oFactory,        // factory object
-            $this->sPageSlug,       // page slug
-            array(                  // tab definition
+            $oFactory,          // factory object
+            $this->sPageSlug,   // page slug
+            array(              // tab definition
                 'tab_slug'      => 'top',
                 'title'         => __( 'Add Ons', 'admin-page-framework-loader' ),
             )
-        );
-            
+        );       
+        
     }
         
 }
