02982a9293e4d7cd7752862d04353dd6d0122903
Michael Uno <michael@michaeluno.jp>
2016-02-24 19:13:56 +0900

    - Increased the time out of cURL as tests time out on Travis.

--- test/install.sh
+++ test/install.sh
@@ -320,7 +320,9 @@ modules:
         PhpBrowser:
             url: '$WP_URL'
         curl: 
-            CURLOPT_TIMEOUT: 180
+            CURLOPT_TIMEOUT: 3000 
+            CURLOPT_TIMEOUT_MS: 3000
+            CURLOPT_HTTP_VERSION: CURL_HTTP_VERSION_1_0    
 coverage:
     # acceptance tests fail if this value is true
     enabled: false            
