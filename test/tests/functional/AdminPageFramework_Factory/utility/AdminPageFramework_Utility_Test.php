0b348a31e51909affc37f092bdec4a66f7d65ffe
Michael Uno <michael@michaeluno.jp>
2016-09-24 19:14:23 +0900

    Added some functional tests for utility classes.

--- test/tests/functional/AdminPageFramework_Factory/utility/AdminPageFramework_Utility_Test.php
+++ test/tests/functional/AdminPageFramework_Factory/utility/AdminPageFramework_Utility_Test.php
@@ -12,6 +12,8 @@ include_once( dirname( dirname( dirname( __FILE__ ) ) ) . '/_bootstrap.php' );
  */
 class AdminPageFramework_Utility_Test extends \WP_UnitTestCase {
     
+    use \Codeception\Specify;
+    
     /**
      * Sores the utility object.
      */
@@ -29,13 +31,100 @@ class AdminPageFramework_Utility_Test extends \WP_UnitTestCase {
         parent::tearDown();
     }
     
-    public function test_sanitizeLength() {
+    public function test_callBack() {
+               
+        $this->specify(
+            'When a callback is not callable, the first parameter value is returned.', 
+            function() {
+                $this->assertEquals( 
+                    "first_value", 
+                    $this->oUtil->callBack( array(), array( "first_value", "second_value" ) )
+                );
+            }
+        );
         
-        $this->assertEquals( '80px', $this->oUtil->sanitizeLength( 80 ) );
-        $this->assertEquals( '80em', $this->oUtil->sanitizeLength( 80 , 'em' ) );
-        $this->assertEquals( '0%', $this->oUtil->sanitizeLength( 0 , '%' ) );
+        $this->specify( 
+            'When a callback is not callable, the first parameter value is returned.', 
+            function() {
+                $this->assertEquals( 
+                    'returned_value_from_callback', 
+                    $this->oUtil->callBack( array( $this, '_replyToTest' ), array( 'first_value', 'second_value' ) )
+                );
+            }
+        );
         
-    }   
+    }
+        public function _replyToTest( $sParam1, $sParam2 ) {
+            return 'returned_value_from_callback';
+        }
     
-
+   
+    public function test_hasBeenCalled() {
+        
+        $this->specify(
+            'Checking the default behavior.', 
+            function() {
+                $this->assertEquals( 
+                    false, 
+                    $this->oUtil->hasBeenCalled( 'foo' )
+                );
+                $this->assertEquals( 
+                    true, 
+                    $this->oUtil->hasBeenCalled( 'foo' )
+                );
+            }
+        );    
+    
+    }
+    
+    public function test_getOutputBuffer() {
+        
+        $this->specify(
+            'Checking the default behavior.', 
+            function() {
+                $this->assertEquals( 
+                    'Printing: Testing', 
+                    $this->oUtil->getOutputBuffer( array( $this, '_replyToPrintSomething' ), array( 'Testing' ) )
+                );
+            }
+        );                    
+        
+    }
+        public function _replyToPrintSomething( $sParameter ) {
+            echo 'Printing: ' . $sParameter;
+        }
+    
+    public function test_getObjectInfo() {
+        
+        $this->specify(
+            'Checking the default behavior.', 
+            function() {
+                $this->assertEquals( 
+                    '(object) AdminPageFramework_ArrayHandler: 2 properties.', 
+                    $this->oUtil->getObjectInfo( new AdminPageFramework_ArrayHandler )
+                );
+            }
+        );           
+        
+    }
+    
+    public function test_getAOrB() {
+        
+        $this->specify(
+            'Checking the default behavior.', 
+            function() {
+                $this->assertEquals( 
+                    'A', 
+                    $this->oUtil->getAOrB( true, 'A', 'B' )
+                );
+                $this->assertEquals( 
+                    'B', 
+                    $this->oUtil->getAOrB( false, 'A', 'B' )
+                );                
+            }
+        );                   
+        
+    }
+    
+    
 }
