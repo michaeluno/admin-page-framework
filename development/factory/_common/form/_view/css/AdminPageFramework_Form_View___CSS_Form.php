01d19e083cde2c111e4d84fac6a29e73fb8d2fd1
Michael Uno <michael@michaeluno.jp>
2016-10-09 07:14:30 +0900

    Tweaked the style of `noscript` elements.

--- development/factory/_common/form/_view/css/AdminPageFramework_Form_View___CSS_Form.php
+++ development/factory/_common/form/_view/css/AdminPageFramework_Form_View___CSS_Form.php
@@ -26,9 +26,7 @@ class AdminPageFramework_Form_View___CSS_Form extends AdminPageFramework_Form_Vi
         $_sSpinnerURL  = esc_url( admin_url( '/images/wpspin_light-2x.gif' ) );
         return <<<CSSRULES
 .admin-page-framework-form-warning {
-    font-weight: bold;
-    color: red;
-    font-size: 1.32em;
+    font-size: 1em;
 }
 CSSRULES;
             
