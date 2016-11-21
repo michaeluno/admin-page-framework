de4ff5cbe1398076dc167e931e96c140bd3fe3e0
Michael Uno <michael@michaeluno.jp>
2016-11-21 19:22:04 +0900

    Made the version being appended to the archive file name before the file extension.

--- tool/export/export.sh
+++ tool/export/export.sh
@@ -1,24 +1,39 @@
 #!/usr/bin/env bash
 
-if [ ! -f export.cfg ]; then
+# Override these default settings with the configuration file.
+PROJECT_SLUG="sample-slug"
+LOCAL_WORKING_COPY_DIRECTORY_PATH="../../"
+OUTPUT_DIRECTORY_PATH="../../../output/"
+if [ ! -f settings.cfg ]; then
     echo The setting file could not be loaded.
     exit 1
 fi
+source settings.cfg
 
-source export.cfg
-
+# Variables
 WORKING_DIRECTORY_PATH=$(pwd)
 OUTPUT_DIRECTORY_PATH=${OUTPUT_DIRECTORY_PATH%/} #remove trailing slash
 PATH_TO_CALCULATE=$WORKING_DIRECTORY_PATH/$OUTPUT_DIRECTORY_PATH
 OUTPUT_DIRECTORY_PATH=$(cd $PATH_TO_CALCULATE; pwd)    # convert it to absolute path
 
-ARCHIVE_FILE_PATH=$OUTPUT_DIRECTORY_PATH/$PROJECT_SLUG.zip
-ARCHIVE_DIRECTORY_PATH=$OUTPUT_DIRECTORY_PATH/$PROJECT_SLUG
+
+## Start 
+
+cd $LOCAL_WORKING_COPY_DIRECTORY_PATH
+
+### Retrieve the current version
+PROJECT_DIRECTORY_PATH=`pwd`
+MAIN_FILE_NAME=$([ -z "$MAIN_FILE_NAME" ] && echo "$PROJECT_SLUG.php" || echo "$MAIN_FILE_NAME")
+MAIN_FILE_CONTENTS=$(<$PROJECT_DIRECTORY_PATH/$MAIN_FILE_NAME)
+VERSION=$(echo $MAIN_FILE_CONTENTS | grep "Version:" $PROJECT_DIRECTORY_PATH/$MAIN_FILE_NAME | awk -F' ' '{print $NF}')
+echo $VERSION
+
+### Archive Paths
+ARCHIVE_FILE_PATH=$OUTPUT_DIRECTORY_PATH/$PROJECT_SLUG.$VERSION.zip
+ARCHIVE_DIRECTORY_PATH=$OUTPUT_DIRECTORY_PATH/$PROJECT_SLUG.$VERSION
 
 # Exit if a command fails and print each command line
 set -ex
-
-cd $LOCAL_WORKING_COPY_DIRECTORY_PATH
 
 # Create a zip file from the project files.
 rm -f "$ARCHIVE_FILE_PATH"  # remove the archive file if exists
@@ -28,6 +43,8 @@ git archive --format zip --output "$ARCHIVE_FILE_PATH" --prefix=$PROJECT_SLUG/ H
 rm -rf "$ARCHIVE_DIRECTORY_PATH"    # remove the specified directory if exists
 mkdir "$ARCHIVE_DIRECTORY_PATH"
 unzip -qo "$ARCHIVE_FILE_PATH" -d "$ARCHIVE_DIRECTORY_PATH"   
+
+## End
 
 # Open the directory
 start "$OUTPUT_DIRECTORY_PATH"
