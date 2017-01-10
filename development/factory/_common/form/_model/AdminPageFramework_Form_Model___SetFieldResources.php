b4408ec6ca97c1bdd197d7d6b6df1bbfc90483c3
Michael Uno <michael@michaeluno.jp>
2017-01-11 06:30:55 +0900

    Refactored code.

--- development/factory/_common/form/_model/AdminPageFramework_Form_Model___SetFieldResources.php
+++ development/factory/_common/form/_model/AdminPageFramework_Form_Model___SetFieldResources.php
@@ -10,8 +10,7 @@
 /**
  * Provides methods to load field resources such as style-sheets and JavaScript scripts.
  * 
- * @package     AdminPageFramework
- * @subpackage  Common/Form/Model
+ * @package     AdminPageFramework/Common/Form/Model
  * @since       3.7.0
  * @internal
  */
@@ -59,30 +58,29 @@ class AdminPageFramework_Form_Model___SetFieldResources extends AdminPageFramewo
      * @return      array
      */
     public function get() {
-        $this->_setCommon();
-        $this->_set();
+        $this->___setCommon();
+        $this->___set( $this->aFieldsets );
         return $this->aResources;
     }
-        private static $_bCalled = false;
+
         /**
          * 
          */
-        private function _setCommon() {
-            if ( self::$_bCalled ) {
+        private function ___setCommon() {
+
+            if ( $this->hasBeenCalled( __METHOD__ ) ) {
                 return;
             }
-            self::$_bCalled = true;
             
             new AdminPageFramework_Form_View___Script_RegisterCallback;
             
-            $this->_setCommonFormInternalCSSRules();
+            $this->___setCommonFormInternalCSSRules();
             
         }
         /**
-         * 
-         * 
+         *
          */
-        private function _setCommonFormInternalCSSRules() {
+        private function ___setCommonFormInternalCSSRules() {
             
             $_aClassNames = array(
                 'AdminPageFramework_Form_View___CSS_Form',
@@ -114,153 +112,167 @@ class AdminPageFramework_Form_Model___SetFieldResources extends AdminPageFramewo
          * @since       3.0.0
          * @since       3.7.0      Moved from `AdminPageFramework_Factory_Model`. Changed the name from `_registerFields()`.
          * Removed the 1st parameter.
+         * @since       3.8.14      Changed the scope to private. Added the `$aFieldsets` parameter.
          * @internal
          * @return      void
          */
-        protected function _set( ) {
+        private function ___set( $aAllFieldsets ) {
+            foreach( $aAllFieldsets as $_sSecitonID => $_aFieldsets ) {
+                $this->___setFieldResourcesBySection( $_aFieldsets );
+            }
+        }
+            /**
+             *
+             * @param       array       $_aFieldsets
+             * @since       3.8.14
+             */
+            private function ___setFieldResourcesBySection( $_aFieldsets ) {
 
-            // Parse all added fieldsets and check associated resources.
-            foreach( $this->aFieldsets as $_sSecitonID => $_aFieldsets ) {
-                
                 $_bIsSubSectionLoaded = false;
                 foreach( $_aFieldsets as $_iSubSectionIndexOrFieldID => $_aSubSectionOrField )  {
-// @todo Examine if this structure is correct or not. 
+// @todo Examine if this structure is correct or not.
 // It may not be necessary to check the sub-section dimensions as this is not the saved options array.
                     // if it's a sub-section
                     if ( $this->isNumericInteger( $_iSubSectionIndexOrFieldID ) ) {
 
                         // no need to repeat the same set of fields
-                        if ( $_bIsSubSectionLoaded ) { 
+                        if ( $_bIsSubSectionLoaded ) {
                             continue;
                         }
                         $_bIsSubSectionLoaded = true;
                         foreach( $_aSubSectionOrField as $_aField ) {
-                            $this->_setFieldResources( $_aField );     
+                            $this->___setFieldResources( $_aField );
                         }
                         continue;
                     }
-                        
+
                     $_aField = $_aSubSectionOrField;
-                    $this->_setFieldResources( $_aField );
-                
+                    $this->___setFieldResources( $_aField );
+
                 }
+
             }
-            
-        }
-            /**
-             * Registers a field.
-             * 
-             * @since       3.0.4
-             * @since       3.5.0      Changed the scope to protected as the admin page factory class overrides it.
-             * @since       3.7.0      Moved from `AdminPageFramework_Factory_Model`. Changed the name from `_registerField()`.
-             * @internal
-             * @return      void
-             */
-            private function _setFieldResources( array $aFieldset ) {
-                                
-                // Check the field conditions here.
-                if ( ! $this->_isFieldsetAllowed( $aFieldset ) ) {
-                    return;
-                }               
-                
-                // 3.8.0+ Support nested fields.
-                if ( $this->hasFieldDefinitionsInContent( $aFieldset ) ) {
-                    foreach( $aFieldset[ 'content' ] as $_asNestedFieldset ) {
-                        if ( is_scalar( $_asNestedFieldset ) ) {
-                            continue;
-                        }
-                        $this->_setFieldResources( $_asNestedFieldset );
-                    }
-                }                
-                
-                // 3.8.0+ Set the internal field type.
-                if ( $this->hasNestedFields( $aFieldset ) ) {
-                    $aFieldset[ 'type' ] = '_nested';
-                }
-                
-                $_sFieldtype            = $this->getElement( $aFieldset, 'type' );
-                $_aFieldTypeDefinition  = $this->getElementAsArray(
-                    $this->aFieldTypeDefinitions,
-                    $_sFieldtype
-                );                
 
-                // If the field type is not defined, it is not possible to load resources.
-                if ( empty( $_aFieldTypeDefinition ) ) {
-                    return;
-                }                
-                
-                // Call the callback method to let the field type know a fieldset of the field type is registered.
-                // This is supposed to be done before form validations so that custom filed types add own routines for the validation.
-                if ( is_callable( $_aFieldTypeDefinition[ 'hfDoOnRegistration' ] ) ) {
-                    call_user_func_array( 
-                        $_aFieldTypeDefinition[ 'hfDoOnRegistration' ], 
-                        array( $aFieldset )
-                    );
-                }
-                
-                // Let the main routine do something upon adding fieldset resources such as adding help pane items.
-                $this->callBack(
-                    $this->aCallbacks[ 'load_fieldset_resource' ], 
-                    array(
-                        $aFieldset,   // 1st parameter 
-                    )
-                );
-                
-// @todo [3.7.0+] retrieve fieldset resources set to the `style` and `script` arguments.
-// Be careful not to add duplicate items as currently the sub-field items are parsed.
-
-                // Check the cache
-                if ( $this->_isAlreadyRegistered( $_sFieldtype, $this->aArguments[ 'structure_type' ] ) ) {
-                    return;
-                }
-                
-                // Initialize the filed type - triggers callbacks defined in the field type.
-                new AdminPageFramework_Form_Model___FieldTypeRegistration(
-                    $_aFieldTypeDefinition,
-                    $this->aArguments[ 'structure_type' ] // used for caching - the values will be cached by fields type
-                );
-                
-                // Get resource items. The set resources to the property will be inserted later by the method defined in the View class component.
-                $_oFieldTypeResources = new AdminPageFramework_Form_Model___FieldTypeResource(
-                    $_aFieldTypeDefinition,
-                    $this->aResources
-                );
-                $this->aResources = $_oFieldTypeResources->get();
-                                                  
-            }     
-                
                 /**
-                 * Checks if the given field type has been registered already by the given fields type.
-                 * @since       3.7.0
-                 * @return      boolean
+                 * Registers a field.
+                 *
+                 * @since       3.0.4
+                 * @since       3.5.0      Changed the scope to protected as the admin page factory class overrides it.
+                 * @since       3.7.0      Moved from `AdminPageFramework_Factory_Model`. Changed the name from `_registerField()`.
+                 * @internal
+                 * @return      void
                  */
-                private function _isAlreadyRegistered( $sFieldtype, $sStructureType ) {
-                    if ( isset( self::$_aRegisteredFieldTypes[ $sFieldtype . '_' .$sStructureType ] ) ) {
-                        return true;
-                    }
-                    self::$_aRegisteredFieldTypes[ $sFieldtype . '_' .$sStructureType ] = true;
-                    return false;
-                }
-                    /**
-                     * @since       3.7.0
-                     */
-                    static private $_aRegisteredFieldTypes = array();
-    
+                private function ___setFieldResources( $aFieldset ) {
 
-        
-        /**
-         * Decides whether the field set should be registered or not.
-         * @since       3.7.0
-         * @return      boolean
-         */
-        private function _isFieldsetAllowed( array $aFieldset ) {
-            return $this->callBack(
-                $this->aCallbacks[ 'is_fieldset_registration_allowed' ], 
-                array(
-                    true,   // 1st parameter 
-                    $aFieldset, // 2nd parameter
-                )
-            );
-        }            
+                    // Check the field conditions.
+                    if ( ! $this->___isFieldsetAllowed( $aFieldset ) ) {
+                        return;
+                    }
+
+                    // 3.8.0+ Parse nested fields.
+                    $this->___setResourcesOfNestedFields( $aFieldset );
+
+                    // 3.8.0+ Set the internal field type.
+                    if ( $this->hasNestedFields( $aFieldset ) ) {
+                        $aFieldset[ 'type' ] = '_nested';
+                    }
+
+                    $_sFieldtype            = $this->getElement( $aFieldset, 'type' );
+                    $_aFieldTypeDefinition  = $this->getElementAsArray(
+                        $this->aFieldTypeDefinitions,
+                        $_sFieldtype
+                    );
+
+                    $this->___setFieldResourcesByFieldTypeDefinition( $aFieldset, $_sFieldtype, $_aFieldTypeDefinition );
+
+                }
+
+                    /**
+                     * Decides whether the field-set should be registered or not.
+                     * @since       3.7.0
+                     * @return      boolean
+                     */
+                    private function ___isFieldsetAllowed( $aFieldset ) {
+                        return $this->callBack(
+                            $this->aCallbacks[ 'is_fieldset_registration_allowed' ],
+                            array(
+                                true,   // 1st parameter
+                                $aFieldset, // 2nd parameter
+                            )
+                        );
+                    }
+
+                    /**
+                     * @param       array   $aFieldset
+                     * @since       3.8.14
+                     */
+                    private function ___setResourcesOfNestedFields( $aFieldset ) {
+
+                        if ( ! $this->hasFieldDefinitionsInContent( $aFieldset ) ) {
+                            return;
+                        }
+                        foreach ( $aFieldset[ 'content' ] as $_asNestedFieldset ) {
+                            if ( is_scalar( $_asNestedFieldset ) ) {
+                                continue;
+                            }
+                            $this->___setFieldResources( $_asNestedFieldset );
+                        }
+
+                    }
+
+                    /**
+                     * @param       string  $_sFieldtype
+                     * @param       array   $_aFieldTypeDefinition
+                     * @since       3.8.14
+                     */
+                    private function ___setFieldResourcesByFieldTypeDefinition( $aFieldset, $_sFieldtype, $_aFieldTypeDefinition ) {
+
+                        // If the field type is not defined, it is not possible to load resources.
+                        if ( empty( $_aFieldTypeDefinition ) ) {
+                            return;
+                        }
+
+                        /**
+                         * Let the field type know a fieldset of the field type is registered.
+                         * This is supposed to be done before form validations
+                         * so that custom filed types add own routines for the validation.
+                         */
+                        $this->callback(
+                            $_aFieldTypeDefinition[ 'hfDoOnRegistration' ],
+                            array(
+                                $aFieldset  // 1st parameter
+                            )
+                        );
+
+                        // Let the main routine do something upon adding fieldset resources such as adding help pane items.
+                        $this->callBack(
+                            $this->aCallbacks[ 'load_fieldset_resource' ],
+                            array(
+                                $aFieldset,   // 1st parameter
+                            )
+                        );
+
+        // @todo [3.7.0+] retrieve fieldset resources set to the `style` and `script` arguments.
+        // Be careful not to add duplicate items as currently the sub-field items are parsed.
+
+                        // Is already registered?
+                        if ( $this->hasBeenCalled( 'registered_' . $_sFieldtype . '_' . $this->aArguments[ 'structure_type' ] ) ) {
+                            return;
+                        }
+
+                        // Initialize the filed type - triggers callbacks defined in the field type.
+                        new AdminPageFramework_Form_Model___FieldTypeRegistration(
+                            $_aFieldTypeDefinition,
+                            $this->aArguments[ 'structure_type' ] // used for caching - the values will be cached by fields type
+                        );
+
+                        // Get resource items. The set resources to the property will be inserted later by the method defined in the View class component.
+                        $_oFieldTypeResources = new AdminPageFramework_Form_Model___FieldTypeResource(
+                            $_aFieldTypeDefinition,
+                            $this->aResources
+                        );
+                        $this->aResources     = $_oFieldTypeResources->get();
+
+                    }
 
 }
