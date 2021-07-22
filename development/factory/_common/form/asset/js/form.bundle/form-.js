7285eda721af9f976fab319fd2d6cadcc4d443a3
Michael Uno <michael2@michaeluno.jp>
2021-07-23 06:01:28 +0900

    Chore: Regenerate minified and bundle JavaScript files
    Deprecate the method to parse directories first and then files. This change now directly parses files so that the `watch` task will detect the file change and trigger the callback event.

--- development/factory/_common/form/asset/js/form.bundle/form-.js
+++ development/factory/_common/form/asset/js/form.bundle/form-.js
@@ -1,4 +1,4 @@
-/*! Admin Page Framework - Form Main 1.0.3 */
+/*! Admin Page Framework - Form Main 1.0.4 */
 /**
  * This script should be empty and provide the banner (header comment) for the concatenated bundled script (form.bundle.js).
  */
