6b468473a0d1cb44fa91fe02a625ec6a7a7a11c3
Michael Uno <michael2@michaeluno.jp>
2021-07-23 03:54:33 +0900

    Chore: Clean: Give proper spacing

--- tool/gulp/tasks/js-concat.js
+++ tool/gulp/tasks/js-concat.js
@@ -5,7 +5,7 @@ const concat = require( 'gulp-concat' );
 const using  = require( 'gulp-using' );
 const cache  = require( 'gulp-cached' );
 const uglify = require( 'gulp-uglify' );
-const path = require('path');
+const path   = require( 'path' );
 
 
 const flatmap = require( 'gulp-flatmap' );
@@ -20,7 +20,7 @@ module.exports = class GulpTaskJSConcat {
         let _basename = path.basename( dir.path );
         return src( [ dir.path + '/*.js', '!' + dir.path + '/*.bundle.js', '!' + dir.path + '/*.min.js' ] )
           .pipe( using() )
-          .pipe( cache('js-concat' ) )
+          .pipe( cache( 'js-concat' ) )
           .pipe( concat( _basename + '.bundle.js' ) )
           .pipe( dest( path.dirname( dir.path ) ) );
     }));
@@ -30,7 +30,7 @@ module.exports = class GulpTaskJSConcat {
       .pipe( flatmap(function( stream, dir ) {
         let _basename = path.basename( dir.path );
         return src( [ dir.path + '/*.min.js', '!' + dir.path + '/*.bundle.min.js' ] )
-          .pipe( cache('js-concat-min' ) )
+          .pipe( cache( 'js-concat-min' ) )
           .pipe( using() )
           .pipe( concat( _basename + '.bundle.min.js' ) )
           .pipe( uglify({
