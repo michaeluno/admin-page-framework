54a606100c24cfb54444b2af7924f308dcb96e30
Michael Uno <michael2@michaeluno.jp>
2021-07-23 17:45:15 +0900

    Tweak: Clean: Remove the deprecated autoloader

--- library/admin-page-framework/admin-page-framework.php
+++
@@ -1,7 +0,0 @@
-<?php
-trigger_error( 
-    'Admin Page Framework:'
-        . ' The file is moved to: ' . dirname( dirname( __FILE__ ) ) . '/apf/admin-page-framework.php',
-    E_USER_WARNING 
-);
-include( dirname( dirname( __FILE__ ) ) . '/apf/admin-page-framework.php' );
