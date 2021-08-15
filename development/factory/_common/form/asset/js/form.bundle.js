15ee30b3f86532ea130be4e0caf81be72d8da0e6
Michael Uno <michael2@michaeluno.jp>
2021-08-15 21:04:17 +0900

    Feature: Add options to the form tooltip
    `content` - tooltip content
    `shown` - the initial visibility state
    `oneOff` - whether the tooltip is only for one time
    `width` - the width of the pop-up tooltip

--- development/factory/_common/form/asset/js/form.bundle.js
+++ development/factory/_common/form/asset/js/form.bundle.js
@@ -1,4 +1,4 @@
-/*! Admin Page Framework - Form Main 1.2.1 */
+/*! Admin Page Framework - Form Main 1.2.2 */
 /**
  * This script should be empty and provide the banner (header comment) for the concatenated bundled script (form.bundle.js).
  */
@@ -1691,7 +1691,11 @@
  * It provides the $[ 'admin-page-framework-tooltip' ]( options ) jQuery plugin method.
  * The name uses hyphens for the user's text domain to be replaced. So the compiled framework script will be entirely ported with without the keyword of `admin-page-framework`.
  *
- * To use the method, have a text message enclosed in an element with a class `admin-page-framework-form-tooltip-content`.
+ * To use the method,
+ * ```
+ * $( '.my-tooltip' )[ 'admin-page-framework-form-tooltip' ]( 'Hi, this is a tooltip content!' );
+ * ```
+ * Or have a text message enclosed in an element with a class `admin-page-framework-form-tooltip-content`.
  * ```
  *    <span class="my-tooltip dashicons dashicons-editor-help">
  *      <span class="admin-page-framework-form-tooltip-content">
@@ -1704,6 +1708,7 @@
  * $( '.my-tooltip' )[ 'admin-page-framework-form-tooltip' ]();
  * ```
  *
+ *
  * When the framework file is compiled, replace the keyword `admin-page-framework` with your text domain.
  *
  */
@@ -1715,77 +1720,122 @@
   } );
 
   $.fn[ 'admin-page-framework-form-tooltip' ] = function( options ) {
+    if ( 'string' === typeof options ) {
+      options = {
+        content: isHTML( options ) ? options : "<span>" + options + "</span>"
+      }
+    }
     initialize( this, options )
   };
 
   function initialize( target, options ) {
 
     var _this = $( target );
+
+    // Format options
     options = 'undefined' === typeof options ? {} : options;
+    options = $.extend( {}, {
+      width: $( target ).data( 'width' ) || options.width || 340,
+      shown: false,        // initial visibility
+      oneOff: false,
+      content: undefined,
+    }, options );
 
     // Disable the CSS default tooltip
     $( _this ).removeClass( 'no-js' );
+
+    if ( options.shown ) {
+      handleTooltip( target, options );
+    }
 
     var _pointerTooltip = $( _this );
     _pointerTooltip.on( 'click', function() {
       return false; // disable click
     });
-    _pointerTooltip.on( 'mouseover touchend', function( event ) {
+    if ( ! options.oneOff ) {
+      _pointerTooltip.on( 'mouseover touchend', options, handleTooltipCallback );
+    }
 
-      var _body      = $( 'body' );
-      var _width     = $( this ).data( 'width' ) || 340;
-      var _content   = $( this ).find( '.admin-page-framework-form-tooltip-content' ).clone();
+  }
 
-      var _offscreen  = $( this ).offset().left + $( this ).width() + _width > _body.offset().left + _body.width();
-      // Open the tooltip
-      $( this ).pointerTooltip( $.extend( true, {}, {
-        pointerClass: 'admin-page-framework-form-tooltip-balloon' + ( _offscreen ? ' offscreen' : '' ),
-        pointerWidth: _width,
-        content: function() {
-          return _content.html();
-        },
-        position: {
-          edge: _offscreen ? 'top' : 'left',
-          align: _offscreen ? 'center' : 'left',
-          within: _offscreen ? _body : $( this ).closest( '.admin-page-framework-field, .admin-page-framework-fieldrow, .admin-page-framework-section' ),
-          collision: 'fit',
-        },
-        buttons: function() {},
-        close: function() {},
-      }, options ) )
-        .pointerTooltip( 'open' );
+  function handleTooltipCallback( event ) {
+    handleTooltip( this, event.data );
+  }
+  function handleTooltip( self, options ) {
 
-      // Handle toolitip closing
-      var _self    = this;
-      /// For non-mobile devices
-      $( this ).add( '.admin-page-framework-form-tooltip-balloon' ).on( 'mouseleave', function( event ){
-        var _selfMouseLeave = this;
-        // Set a timeout for the tooltip to close, allowing us to clear this trigger if the mouse comes back over
-        var _timeoutId = setTimeout(function(){
-          $( _self ).pointerTooltip( 'close' );
-          _content.remove();
-          $( _self ).off( 'mouseleave' );
-          $( _selfMouseLeave ).off( 'mouseleave' );
-          $( _self ).off( 'mouseenter' );
-          $( _selfMouseLeave ).off( 'mouseenter' );
-        }, 650 );
-        $( _self ).data( 'timeoutId', _timeoutId );
+    var _body      = $( 'body' );
+    var _width     = options.width;
+    var _content   = 'undefined' !== typeof options.content
+      ? $( isHTML( options.content ) ? options.content : "<span>" + options.content + "</span>" )
+      : $( self ).find( '.admin-page-framework-form-tooltip-content' ).clone();
+    var _offscreen  = $( self ).offset().left + $( self ).width() + _width > _body.offset().left + _body.width();
+    // Open the tooltip
+    var _options    = $.extend( true, {}, options, {
+      pointerClass: 'admin-page-framework-form-tooltip-balloon' + ( _offscreen ? ' offscreen' : '' ),
+      pointerWidth: _width,
+      content: function() {
+        return _content.html();
+      },
+      position: {
+        edge: _offscreen ? 'top' : 'left',
+        align: _offscreen ? 'center' : 'left',
+        within: _offscreen ? _body : $( self ).closest( '.admin-page-framework-field, .admin-page-framework-fieldrow, .admin-page-framework-section' ),
+        collision: 'fit',
+      },
+      buttons: function() {},
+      close: function() {},
+    }, options );
+    debugLog( 'options', _options );
+    $( self ).pointerTooltip( _options )
+      .pointerTooltip( 'open' );
 
-      } );
-      $( this ).add( '.admin-page-framework-form-tooltip-balloon' ).on( 'mouseenter', function(){
-        clearTimeout( $( _self ).data('timeoutId' ) );
-      });
-      /// For mobile devices
-      setTimeout( function() {
-        _body.on( 'touchstart', closeTooltipMobile );
-        function closeTooltipMobile( event ) {
-          $( 'body' ).off( 'touchstart', closeTooltipMobile );
-          $( _self ).pointerTooltip( 'close' );
-        }
-      }, 200 );
+    // Handle toolitip closing
+    var _self    = self;
+    /// For non-mobile devices
+    $( this ).add( '.admin-page-framework-form-tooltip-balloon' ).on( 'mouseleave', function( event ){
+      var _selfMouseLeave = this;
+      // Set a timeout for the tooltip to close, allowing us to clear this trigger if the mouse comes back over
+      var _timeoutId = setTimeout(function(){
+        $( _self ).pointerTooltip( 'close' );
+        _content.remove();
+        $( _self ).off( 'mouseleave' );
+        $( _selfMouseLeave ).off( 'mouseleave' );
+        $( _self ).off( 'mouseenter' );
+        $( _selfMouseLeave ).off( 'mouseenter' );
+      }, 650 );
+      $( _self ).data( 'timeoutId', _timeoutId );
 
     } );
+    $( this ).add( '.admin-page-framework-form-tooltip-balloon' ).on( 'mouseenter', function(){
+      clearTimeout( $( _self ).data('timeoutId' ) );
+    });
+    /// For mobile devices
+    setTimeout( function() {
+      _body.on( 'touchstart', closeTooltipMobile );
+      function closeTooltipMobile( event ) {
+        $( 'body' ).off( 'touchstart', closeTooltipMobile );
+        $( _self ).pointerTooltip( 'close' );
+      }
+    }, 200 );
 
+  }
+
+  function isHTML(str) {
+    var a = document.createElement('div');
+    a.innerHTML = str;
+    for (var c = a.childNodes, i = c.length; i--; ) {
+      if ( 1 === c[ i ].nodeType) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function debugLog( ...message ) {
+    if ( ! parseInt( AdminPageFrameworkScriptFormMain.debugMode ) ) {
+      return;
+    }
+    console.log( '[APF]', ...message );
   }
 
 }(jQuery));
