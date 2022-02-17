04a56533fefec2e236ea2beff884b18a1a4ecdca
Michael Uno <michael2@michaeluno.jp>
2022-02-17 12:41:47 +0900

    Feature: Add the ability to parse linear arrays
    Currently, only tables consisting of key-value pairs with two columns can be generated from a multi-dimensional associative array, mainly used to inspect array data. This change gives the ability to generate multi-column tables from a numerically indexed (linear) array so CSV-like table data can be passed to generate tables which may be suitable for most general use cases.

--- development/factory/_common/utility/base_utility/AdminPageFramework_Utility_Interpreter.php
+++ development/factory/_common/utility/base_utility/AdminPageFramework_Utility_Interpreter.php
@@ -128,6 +128,46 @@ class AdminPageFramework_Utility_Interpreter extends AdminPageFramework_Utility_
     }
 
     /**
+     * Generates an HTML table from a given array.
+     *
+     * Similar to `getTableOfArray()` but this does not support multiple columns in a single row.
+     * All rows consist of key-value pair, representing the array structure.
+     *
+     * @param  array   $aArray
+     * @param  array   $aAllAttributes
+     * @param  array   $aHeader
+     * @param  array   $aFooter
+     * @param  boolean $bEscape
+     * @param  string  $sCaption
+     * @return string
+     * @since  3.9.0
+     */
+    static public function getTableOfKeyValues( array $aArray, array $aAllAttributes=array(), array $aHeader=array(), array $aFooter=array(), $bEscape=true, $sCaption='' ) {
+        $_aAllAttributes = $aAllAttributes + array(
+            'table'   => array(),
+            'caption' => array(),
+            'tbody'   => array(),
+            'td'      => array(
+                array(),
+                array(),
+            ),
+            'tr'      => array(),
+            't'       => array(),
+            'ul'      => array(),
+            'li'      => array(),
+            'p'       => array(),
+        );
+        return "<table " . self::getAttributes( self::getElementAsArray( $_aAllAttributes, 'table' ) ) . ">"
+                . self::___getTableCaption( $sCaption, $_aAllAttributes, $bEscape )
+                . self::___getTableHeaderOfKeyValuePair( $aHeader, $aAllAttributes, $bEscape )
+                . "<tbody " . self::getAttributes( self::getElementAsArray( $_aAllAttributes, 'tbody' ) ) . ">"
+                    . self::___getRowsOfKeyValuePair( $aArray, $aAllAttributes, $bEscape )
+                . "</tbody>"
+                . self::___getTableFooterOfKeyValuePair( $aFooter, $aAllAttributes, $bEscape )
+            . "</table>";
+    }
+
+    /**
      * Generates a table output of a given array.
      * Designed to display key-value pairs in a table.
      *
@@ -185,64 +225,144 @@ class AdminPageFramework_Utility_Interpreter extends AdminPageFramework_Utility_
             if ( empty( $aHeader ) ) {
                 return '';
             }
-            $_aTRAttr  = self::getElementAsArray( $aAllAttributes, 'tr' );
-            $_aTHAttr  = self::getElementAsArray( $aAllAttributes, 'th' );
-            $_aTHAttr1 = self::getElementAsArray( $aAllAttributes, array( 'th', 0 ) ) + $_aTHAttr;
-            $_aTHAttr2 = self::getElementAsArray( $aAllAttributes, array( 'th', 1 ) ) + $_aTHAttr;
-            $_sOutput = '';
-            foreach( $aHeader as $_sKey => $_sValue ) {
-                $_sOutput = "<tr " . self::getAttributes( $_aTRAttr ) . ">"
-                        . "<th " . self::getAttributes( $_aTHAttr1 ) . ">" . self::___getHTMLEscaped( $_sKey, $bEscape ) . "</th>"
-                        . "<th " . self::getAttributes( $_aTHAttr2 ) . ">" . self::___getHTMLEscaped( $_sValue, $bEscape ) . "</th>"
-                    . "</tr>";
-            }
-            return "<thead>" . $_sOutput . "</thead>";
+            return self::isAssociative( $aHeader )
+                ? self::___getTableHeaderOfKeyValuePair( $aHeader, $aAllAttributes, $bEscape )
+                : self::___getTableHeaderOfMultiColumns( $aHeader, $aAllAttributes, $bEscape );
+
         }
+            static private function ___getTableHeaderOfKeyValuePair( array $aHeader, array $aAllAttributes, $bEscape ) {
+                $_aTRAttr  = self::getElementAsArray( $aAllAttributes, 'tr' );
+                $_aTRAttr[ 'class' ] = self::___addClass( 'key-value', self::getElement( $_aTRAttr, array( 'class' ), '' ) );
+                $_aTHAttr  = self::getElementAsArray( $aAllAttributes, 'th' );
+                $_aTHAttr1 = self::getElementAsArray( $aAllAttributes, array( 'th', 0 ) ) + $_aTHAttr;
+                $_aTHAttr2 = self::getElementAsArray( $aAllAttributes, array( 'th', 1 ) ) + $_aTHAttr;
+                $_sOutput = '';
+                foreach( $aHeader as $_sKey => $_sValue ) {
+                    $_sOutput .= "<tr " . self::getAttributes( $_aTRAttr ) . ">"
+                            . "<th " . self::getAttributes( $_aTHAttr1 ) . ">" . self::___getHTMLEscaped( $_sKey, $bEscape ) . "</th>"
+                            . "<th " . self::getAttributes( $_aTHAttr2 ) . ">" . self::___getHTMLEscaped( $_sValue, $bEscape ) . "</th>"
+                        . "</tr>";
+                }
+                return "<thead>" . $_sOutput . "</thead>";
+            }
+            static private function ___getTableHeaderOfMultiColumns( array $aHeader, array $aAllAttributes, $bEscape ) {
+                $_aTRAttr  = self::getElementAsArray( $aAllAttributes, 'tr' );
+                $_aTHAttr  = self::getElementAsArray( $aAllAttributes, 'th' );
+                $_sOutput  = "<tr " . self::getAttributes( $_aTRAttr ) . ">";
+                foreach( array_values( $aHeader ) as $_iIndex => $_sColumnName ) {
+                    $_aTHAttrNth  = self::getElementAsArray( $aAllAttributes, array( 'th', $_iIndex ) ) + $_aTHAttr;
+                    $_sOutput    .= "<th " . self::getAttributes( $_aTHAttrNth ) . ">" . self::___getHTMLEscaped( ( string ) $_sColumnName, $bEscape ) . "</th>";
+                }
+                $_sOutput .= "</tr>";
+                return "<thead>" . $_sOutput . "</thead>";
+            }
         static private function ___getTableFooter( array $aFooter, array $aAllAttributes, $bEscape ) {
             if ( empty( $aFooter ) ) {
                 return '';
             }
-            $_aTRAttr  = self::getElementAsArray( $aAllAttributes, 'tr' );
-            $_aTDAttr  = self::getElementAsArray( $aAllAttributes, 'td' );
-            $_aTDAttr1 = self::getElementAsArray( $aAllAttributes, array( 'td', 0 ) ) + $_aTDAttr;
-            $_aTDAttr2 = self::getElementAsArray( $aAllAttributes, array( 'td', 1 ) ) + $_aTDAttr;
-            $_sOutput = '';
-            foreach( $aFooter as $_sKey => $_sValue ) {
-                $_sOutput = "<tr " . self::getAttributes( $_aTRAttr ) . ">"
-                        . "<td " . self::getAttributes( $_aTDAttr1 ) . ">" . self::___getHTMLEscaped( $_sKey, $bEscape ) . "</td>"
-                        . "<td " . self::getAttributes( $_aTDAttr2 ) . ">" . self::___getHTMLEscaped( $_sValue, $bEscape ) . "</td>"
-                    . "</tr>";
-            }
-            return "<tfoot>" . $_sOutput . "</tfoot>";
-        }        
-        static private function ___getTableRows( array $aItem, array $aAllAttributes, $bEscape ) {
-            $_aTRAttr = self::getElementAsArray( $aAllAttributes, 'tr' );
-            $_aTDAttr = self::getElementAsArray( $aAllAttributes, 'td' );
-            $_aTDAttr = array_filter( $_aTDAttr, 'is_scalar' );
-            if ( empty( $aItem ) ) {
-                return '';
-                // @deprecaetd
-                // $_aTDAttr = array( 'colspan' => 2 ) + $_aTDAttr;
-                // return "<tr " . self::getAttributes( $_aTRAttr ) . ">"
-                //         . "<td " . self::getAttributes( $_aTDAttr ) . ">"
-                //             . __( 'No data found.', 'admin-page-framework' )
-                //         . "</td>"
-                //     . "</tr>";
-            }
-            $_aTDAttrFirst            = self::getElementAsArray( $aAllAttributes, array( 'td', 0 ) ) + $_aTDAttr;
-            $_aTDAttrFirst[ 'class' ] = self::___addClass( 'column-key', self::getElement( $_aTDAttrFirst, array( 'class' ), '' ) );
-            $_aPAttr                  = self::getElementAsArray( $aAllAttributes, array( 'p' ) );
-            $_sOutput = '';
-            foreach( $aItem as $_sColumnName => $_asValue ) {
-                $_sOutput .= "<tr " . self::getAttributes( $_aTRAttr ) . ">";
-                $_sOutput .= "<td " . self::getAttributes( $_aTDAttrFirst ) . ">"
-                        . "<p " . self::getAttributes( $_aPAttr )  . ">" . self::___getHTMLEscaped( $_sColumnName, $bEscape ) . "</p>"
-                     . "</td>";
-                $_sOutput .= self::___getColumnValue( $_asValue, $aAllAttributes, $bEscape );
-                $_sOutput .= "</tr>";
-            }
-            return $_sOutput;
+            return self::isAssociative( $aFooter )
+                ? self::___getTableFooterOfKeyValuePair( $aFooter, $aAllAttributes, $bEscape )
+                : self::___getTableFooterOfMultiColumns( $aFooter, $aAllAttributes, $bEscape );
         }
+            static private function ___getTableFooterOfKeyValuePair( array $aFooter, array $aAllAttributes, $bEscape ) {
+                $_aTRAttr  = self::getElementAsArray( $aAllAttributes, 'tr' );
+                $_aTDAttr  = self::getElementAsArray( $aAllAttributes, 'td' );
+                $_aTRAttr[ 'class' ] = self::___addClass( 'key-value', self::getElement( $_aTRAttr, array( 'class' ), '' ) );
+                $_aTDAttr1 = self::getElementAsArray( $aAllAttributes, array( 'td', 0 ) ) + $_aTDAttr;
+                $_aTDAttr2 = self::getElementAsArray( $aAllAttributes, array( 'td', 1 ) ) + $_aTDAttr;
+                $_sOutput = '';
+                foreach( $aFooter as $_sKey => $_sValue ) {
+                    $_sOutput = "<tr " . self::getAttributes( $_aTRAttr ) . ">"
+                            . "<td " . self::getAttributes( $_aTDAttr1 ) . ">" . self::___getHTMLEscaped( $_sKey, $bEscape ) . "</td>"
+                            . "<td " . self::getAttributes( $_aTDAttr2 ) . ">" . self::___getHTMLEscaped( $_sValue, $bEscape ) . "</td>"
+                        . "</tr>";
+                }
+                return "<tfoot>" . $_sOutput . "</tfoot>";                
+            }
+            static private function ___getTableFooterOfMultiColumns( array $aFooter, array $aAllAttributes, $bEscape ) {
+                $_aTRAttr  = self::getElementAsArray( $aAllAttributes, 'tr' );
+                $_aTDAttr  = self::getElementAsArray( $aAllAttributes, 'td' );
+                $_sOutput  = "<tr " . self::getAttributes( $_aTRAttr ) . ">";
+                foreach( array_values( $aFooter ) as $_iIndex => $_sColumnName ) {
+                    $_aTDAttrNth  = self::getElementAsArray( $aAllAttributes, array( 'td', $_iIndex ) ) + $_aTDAttr;
+                    $_sOutput    .= "<td " . self::getAttributes( $_aTDAttrNth ) . ">" . self::___getHTMLEscaped( ( string ) $_sColumnName, $bEscape ) . "</td>";
+                }
+                $_sOutput .= "</tr>";
+                return "<tfoot>" . $_sOutput . "</tfoot>";
+            }            
+        static private function ___getTableRows( array $aArray, array $aAllAttributes, $bEscape ) {
+            if ( empty( $aArray ) ) {
+                return '';
+            }
+            return self::___shouldKeyValuePair( $aArray )
+                ? self::___getRowsOfKeyValuePair( $aArray, $aAllAttributes, $bEscape )
+                : self::___getRowsOfMultiColumns( $aArray, $aAllAttributes, $bEscape );
+        }
+            static private function ___shouldKeyValuePair( array $aArray ) {
+                if ( self::isAssociative( $aArray ) ) {
+                    return true;
+                }
+                $_aFirstItem = self::getAsArray( self::getFirstElement( $aArray ) );
+                if ( self::isAssociative( $_aFirstItem ) || self::isMultiDimensional( $_aFirstItem ) ) {
+                    return true;
+                }
+                return false;
+            }
+
+            /**
+             * @param  array   $aItem
+             * @param  array   $aAllAttributes
+             * @param  boolean $bEscape
+             * @return string
+             * @since  3.9.0
+             */
+            static private function ___getRowsOfKeyValuePair( array $aItem, array $aAllAttributes, $bEscape ) {
+                $_aTRAttr                 = self::getElementAsArray( $aAllAttributes, 'tr' );
+                $_aTRAttr[ 'class' ]      = self::___addClass( 'key-value', self::getElement( $_aTRAttr, array( 'class' ), '' ) );
+                $_aTDAttr                 = self::getElementAsArray( $aAllAttributes, 'td' );
+                $_aTDAttr                 = array_filter( $_aTDAttr, 'is_scalar' );
+                $_aPAttr                  = self::getElementAsArray( $aAllAttributes, array( 'p' ) );
+                $_aTDAttrFirst            = self::getElementAsArray( $aAllAttributes, array( 'td', 0 ) ) + $_aTDAttr;
+                $_aTDAttrFirst[ 'class' ] = self::___addClass( 'column-key', self::getElement( $_aTDAttrFirst, array( 'class' ), '' ) );                
+                $_sOutput = '';
+                foreach( $aItem as $_sColumnName => $_asValue ) {
+                    $_sOutput .= "<tr " . self::getAttributes( $_aTRAttr ) . ">";
+                    $_sOutput .= "<td " . self::getAttributes( $_aTDAttrFirst ) . ">"
+                            . "<p " . self::getAttributes( $_aPAttr )  . ">" . self::___getHTMLEscaped( $_sColumnName, $bEscape ) . "</p>"
+                         . "</td>";
+                    $_sOutput .= self::___getColumnValue( $_asValue, $aAllAttributes, $bEscape, 1 );
+                    $_sOutput .= "</tr>";
+                }
+                return $_sOutput;
+            }
+            /**
+             * @return string
+             * @since  3.9.0
+             */
+            static private function ___getRowsOfMultiColumns( array $aArray, array $aAllAttributes, $bEscape ) {
+                $_aTRAttr = self::getElementAsArray( $aAllAttributes, 'tr' );
+                $_sOutput = '';
+                foreach( $aArray as $_iRowIndex => $_asValue ) {
+                    if ( is_scalar( $_asValue ) ) {
+                        $_sOutput .= "<tr " . self::getAttributes( $_aTRAttr ) . ">"
+                                . self::___getColumnValue( $_asValue, $aAllAttributes, $bEscape, 0 )
+                            . "</tr>";
+                        continue;
+                    }
+                    $_aColumns = self::getAsArray( $_asValue );
+                    $_sOutput .= "<tr " . self::getAttributes( $_aTRAttr ) . ">"
+                            . self::___getColumns( $_aColumns, $aAllAttributes, $bEscape )
+                        . "</tr>";
+                }
+                return $_sOutput;
+            }
+                static private function ___getColumns( array $aColumns, $aAllAttributes, $bEscape ) {
+                    $_sOutput = '';
+                    foreach( array_values( $aColumns ) as $_iIndex => $_asValue ) {
+                        $_sOutput .= self::___getColumnValue( $_asValue, $aAllAttributes, $bEscape, $_iIndex );
+                    }
+                    return $_sOutput;
+                }
 
             /**
              * @param  string $sClassToAdd
@@ -253,40 +373,51 @@ class AdminPageFramework_Utility_Interpreter extends AdminPageFramework_Utility_
             static private function ___addClass( $sClassToAdd, $sClasses ) {
                 $_aClasses    = explode( ' ', $sClasses );
                 $_aClasses[]  = $sClassToAdd;
-                return implode( ' ', array_unique( $_aClasses ) );
+                return trim( implode( ' ', array_unique( $_aClasses ) ) );
             }
-            static private function ___getColumnValue( $mValue, array $aAllAttributes, $bEscape ) {
-                $_aTDAttr       = self::getElementAsArray( $aAllAttributes, 'td' );
-                $_aTDAttr       = array_filter( $_aTDAttr, 'is_scalar' );
-                $_aTDAttrSecond = self::getElementAsArray( $aAllAttributes, array( 'td', 1 ) ) + $_aTDAttr;
-                $_aTDAttrSecond[ 'class' ] = self::___addClass( 'column-value', self::getElement( $_aTDAttrSecond, array( 'class' ), '' ) );
+
+            /**
+             * @param  mixed   $mValue
+             * @param  array   $aAllAttributes
+             * @param  boolean $bEscape
+             * @param  integer $iColumnIndex Zero-based column index
+             * @return string
+             * @since  3.9.0
+             */
+            static private function ___getColumnValue( $mValue, array $aAllAttributes, $bEscape, $iColumnIndex ) {
+                $_aTDAttr    = self::getElementAsArray( $aAllAttributes, 'td' );
+                $_aTDAttr    = array_filter( $_aTDAttr, 'is_scalar' );
+                $_aTDAttrNth = self::getElementAsArray( $aAllAttributes, array( 'td', $iColumnIndex ) ) + $_aTDAttr;
+                $_aTDAttrNth[ 'class' ] = self::___addClass( 'column-value', self::getElement( $_aTDAttrNth, array( 'class' ), '' ) );
                 if ( is_null( $mValue ) ) {
                     $mValue = '(null)';
                 }
                 $_aPAttr        = self::getElementAsArray( $aAllAttributes, 'p' );
                 if ( is_scalar( $mValue ) ) {
-                    return "<td " . self::getAttributes( $_aTDAttrSecond ) . ">"
+                    return "<td " . self::getAttributes( $_aTDAttrNth ) . ">"
                         . "<p " . self::getAttributes( $_aPAttr )  . ">" . self::___getHTMLEscaped( $mValue, $bEscape ) . "</p>"
                        . "</td>";
                 }
                 if ( is_array( $mValue ) ) {
                     return self::isAssociativeArray( $mValue ) || self::isMultiDimensional( $mValue )
-                        ? "<td " . self::getAttributes( $_aTDAttrSecond ) . ">"
-                            . self::getTableOfArray( $mValue, $aAllAttributes )
+                        ? "<td " . self::getAttributes( $_aTDAttrNth ) . ">"
+                            . self::getTableOfKeyValues( $mValue, $aAllAttributes )
                         . "</td>"
-                        : "<td " . self::getAttributes( $_aTDAttrSecond ) . ">"
+                        : "<td " . self::getAttributes( $_aTDAttrNth ) . ">"
+                            // @todo may ought to be numeric-rows table
                             . self::___getList( $mValue, $aAllAttributes, $bEscape )
                         . "</td>";
                 }
-                return "<td " . self::getAttributes( $_aTDAttrSecond ) . ">"
+                return "<td " . self::getAttributes( $_aTDAttrNth ) . ">"
                         . '(' . gettype( $mValue ) . ')' . ( is_object( $mValue ) ? get_class( $mValue ) : '' )
                     . "</td>";
             }
                 /**
-                 * @param array $aArray
-                 * @param array $aAllAttributes
+                 * @param  array $aArray
+                 * @param  array $aAllAttributes
+                 * @param  boolean $bEscape
                  * @return string
-                 * @since 3.9.0
+                 * @since  3.9.0
                  */
                 static private function ___getList( array $aArray, $aAllAttributes, $bEscape ) {
                     $_aULAttr = self::getElementAsArray( $aAllAttributes, 'ul' );
