michaeluno/admin-page-framework

commit 02c1d8e243f888180c2f8b8bb13777fb5499d1e0

Feature: Change the wording of "generate" to "compile" in UI for clarity

include/class/admin/admin-page-framework/tool/generator/AdminPageFrameworkLoader_AdminPage_Tool_Compiler_CustomFieldTypes.php
@@ -7,11 +7,11 @@
  */
 
 /**
- * Adds the 'custom_field_types' field to the 'Generator' section.
+ * Adds the 'custom_field_types' field to the 'Compiler' section.
  *
  * @since 3.6.0
  */
-class AdminPageFrameworkLoader_AdminPage_Tool_Generator_CustomFieldTypes {
+class AdminPageFrameworkLoader_AdminPage_Tool_Compiler_CustomFieldTypes {
 
     /**
      * Stores the admin page factory object.

asset/log/3.9.0.rm
@@ -9,5 +9,6 @@
 - Fixed an issue that field repeat buttons and labels of the `color` field type are hidden by the color picker pallet in recent WordPress versions.
 - Fixed an issue that tooltips created with the `tip` field argument made the document width wider than the initial width.
 - Fixed an issue that class names and text domains of sub-files of custom field types were not converted when building.  
-- Changed the behavior of loading framework resources (stylesheets and JavaScript scripts) from internally to externally for most cases.    
+- Changed the behavior of loading framework resources (stylesheets and JavaScript scripts) from internally to externally for most cases.
+- Changed the tab label `Generator` to `Compiler`.
 - Deprecated the `email` argument of the `submit` field type.

include/class/admin/admin-page-framework/tool/AdminPageFrameworkLoader_AdminPage_Tool.php
@@ -19,12 +19,12 @@ class AdminPageFrameworkLoader_AdminPage_Tool extends AdminPageFrameworkLoader_A
     public function replyToLoadPage( $oFactory ) {
 
         // Tabs
-        new AdminPageFrameworkLoader_AdminPage_Tool_Generator(
+        new AdminPageFrameworkLoader_AdminPage_Tool_Compiler(
             $this->oFactory,
             $this->sPageSlug,
             array(
-                'tab_slug'  => 'generator',
-                'title'     => __( 'Generator', 'admin-page-framework-loader' ),
+                'tab_slug'  => 'compiler',
+                'title'     => __( 'Compiler', 'admin-page-framework-loader' ),
             )
         );
 

include/class/admin/admin-page-framework/tool/generator/AdminPageFrameworkLoader_AdminPage_Tool_Compiler.php
@@ -15,14 +15,14 @@
  * @since       3.5.4
  * @extends     AdminPageFrameworkLoader_AdminPage_Tab_Base
  */
-class AdminPageFrameworkLoader_AdminPage_Tool_Generator extends AdminPageFrameworkLoader_AdminPage_Tab_Base {
+class AdminPageFrameworkLoader_AdminPage_Tool_Compiler extends AdminPageFrameworkLoader_AdminPage_Tab_Base {
 
     /**
      * Triggered when the tab is loaded.
      */
     public function replyToLoadTab( $oAdminPage ) {
 
-        new  AdminPageFrameworkLoader_AdminPage_Tool_Generator_Generator(
+        new AdminPageFrameworkLoader_AdminPage_Tool_Compiler_Compiler(
             $oAdminPage,
             $this->sPageSlug,
             array(
@@ -30,7 +30,7 @@ class AdminPageFrameworkLoader_AdminPage_Tool_Generator extends AdminPageFramewo
                 'tab_slug'      => $this->sTabSlug,
                 'title'         => __( 'Download Framework', 'admin-page-framework-loader' ),
                 'description'   => array(
-                    __( 'Generate your own version of the framework to avoid library conflicts.', 'admin-page-framework-loader' ),
+                    __( 'Compile your own version of the framework to avoid library conflicts.', 'admin-page-framework-loader' ),
                 ),
             )
         );

include/class/admin/admin-page-framework/tool/generator/AdminPageFrameworkLoader_AdminPage_Tool_Compiler_Compiler.php
@@ -7,11 +7,11 @@
  */
 
 /**
- * Adds the 'Generator' section to the 'Generator' tab.
+ * Adds the 'Compiler' section to the 'Compiler' tab.
  *
  * @since 3.5.4
  */
-class AdminPageFrameworkLoader_AdminPage_Tool_Generator_Generator extends AdminPageFrameworkLoader_AdminPage_Section_Base {
+class AdminPageFrameworkLoader_AdminPage_Tool_Compiler_Compiler extends AdminPageFrameworkLoader_AdminPage_Section_Base {
 
     /**
      * Stores the admin page factory object.
@@ -37,7 +37,7 @@ class AdminPageFrameworkLoader_AdminPage_Tool_Generator_Generator extends AdminP
             5
         );
         add_action(
-            // export_{instantiated clasa name}_{section id}_{field id}
+            // export_{instantiated class name}_{section id}_{field id}
             "export_{$oFactory->oProp->sClassName}_{$this->sSectionID}_download",
             array( $this, 'replyToDownloadFramework' ),
             10,
@@ -122,11 +122,11 @@ class AdminPageFrameworkLoader_AdminPage_Tool_Generator_Generator extends AdminP
             ),
             array(
                 'field_id'          => 'download',
-                'title'             => __( 'Download', 'admin-page-framework-loader' ),
+                'title'             => __( 'Compile', 'admin-page-framework-loader' ),
                 'type'              => 'export',
                 'label_min_width'   => 0,
                 'order'             => 100,
-                'value'             => __( 'Download', 'adimn-page-framework-demo' ),
+                'value'             => __( 'Download', 'admin-page-framework-demo' ),
                 'file_name'         => 'admin-page-framework.zip',  // the default file name. This will be modified by the filter.
                 'format'            => 'text',  // 'json', 'text', 'array'
                 'description'       => $oFactory->oUtil->getAOrB(
@@ -144,7 +144,7 @@ class AdminPageFrameworkLoader_AdminPage_Tool_Generator_Generator extends AdminP
             )
         );
 
-        new AdminPageFrameworkLoader_AdminPage_Tool_Generator_CustomFieldTypes( $oFactory, $sSectionID );
+        new AdminPageFrameworkLoader_AdminPage_Tool_Compiler_CustomFieldTypes( $oFactory, $sSectionID );
 
     }
         /**

include/loader-class-map.php
@@ -18,9 +18,9 @@ return array(
     "AdminPageFrameworkLoader_AdminPage_Help_Report_Report" => AdminPageFrameworkLoader_Registry::$sDirPath . "/include/class/admin/admin-page-framework/help/AdminPageFrameworkLoader_AdminPage_Help_Report_Report.php", 
     "AdminPageFrameworkLoader_AdminPage_Help_Tip" => AdminPageFrameworkLoader_Registry::$sDirPath . "/include/class/admin/admin-page-framework/help/AdminPageFrameworkLoader_AdminPage_Help_Tip.php", 
     "AdminPageFrameworkLoader_AdminPage_Tool" => AdminPageFrameworkLoader_Registry::$sDirPath . "/include/class/admin/admin-page-framework/tool/AdminPageFrameworkLoader_AdminPage_Tool.php", 
-    "AdminPageFrameworkLoader_AdminPage_Tool_Generator" => AdminPageFrameworkLoader_Registry::$sDirPath . "/include/class/admin/admin-page-framework/tool/generator/AdminPageFrameworkLoader_AdminPage_Tool_Generator.php", 
-    "AdminPageFrameworkLoader_AdminPage_Tool_Generator_CustomFieldTypes" => AdminPageFrameworkLoader_Registry::$sDirPath . "/include/class/admin/admin-page-framework/tool/generator/AdminPageFrameworkLoader_AdminPage_Tool_Generator_CustomFieldTypes.php", 
-    "AdminPageFrameworkLoader_AdminPage_Tool_Generator_Generator" => AdminPageFrameworkLoader_Registry::$sDirPath . "/include/class/admin/admin-page-framework/tool/generator/AdminPageFrameworkLoader_AdminPage_Tool_Generator_Generator.php", 
+    "AdminPageFrameworkLoader_AdminPage_Tool_Compiler" => AdminPageFrameworkLoader_Registry::$sDirPath . "/include/class/admin/admin-page-framework/tool/generator/AdminPageFrameworkLoader_AdminPage_Tool_Compiler.php", 
+    "AdminPageFrameworkLoader_AdminPage_Tool_Compiler_Compiler" => AdminPageFrameworkLoader_Registry::$sDirPath . "/include/class/admin/admin-page-framework/tool/generator/AdminPageFrameworkLoader_AdminPage_Tool_Compiler_Compiler.php", 
+    "AdminPageFrameworkLoader_AdminPage_Tool_Compiler_CustomFieldTypes" => AdminPageFrameworkLoader_Registry::$sDirPath . "/include/class/admin/admin-page-framework/tool/generator/AdminPageFrameworkLoader_AdminPage_Tool_Compiler_CustomFieldTypes.php", 
     "AdminPageFrameworkLoader_AdminPageWelcome" => AdminPageFrameworkLoader_Registry::$sDirPath . "/include/class/admin/dashboard/AdminPageFrameworkLoader_AdminPageWelcome.php", 
     "AdminPageFrameworkLoader_AdminPageWelcome_Welcome" => AdminPageFrameworkLoader_Registry::$sDirPath . "/include/class/admin/dashboard/welcome/AdminPageFrameworkLoader_AdminPageWelcome_Welcome.php", 
     "AdminPageFrameworkLoader_Demo" => AdminPageFrameworkLoader_Registry::$sDirPath . "/include/class/admin/demo/AdminPageFrameworkLoader_Demo.php", 

commit 98f59bfbb2329dc66212637fd091db4236c1e4d5

Fix: Fix a bug that custom field types are not included with the compiler
Introduced with 02c1d8e2.

include/class/admin/admin-page-framework/tool/generator/AdminPageFrameworkLoader_AdminPage_Tool_Compiler_CustomFieldTypes.php
@@ -234,7 +234,7 @@ class AdminPageFrameworkLoader_AdminPage_Tool_Compiler_CustomFieldTypes {
                             $_POST,
                             array(
                                 $this->oFactory->oProp->sOptionKey,
-                                'generator',    // section id
+                                'compiler',    // section id
                                 'class_prefix'  // field id
                             ),
                             ''
@@ -294,7 +294,7 @@ class AdminPageFrameworkLoader_AdminPage_Tool_Compiler_CustomFieldTypes {
                         $_POST,
                         array(
                             $this->oFactory->oProp->sOptionKey,
-                            'generator', // section id
+                            'compiler', // section id
                             'text_domain' // field id
                         ),
                         ''
@@ -368,7 +368,7 @@ class AdminPageFrameworkLoader_AdminPage_Tool_Compiler_CustomFieldTypes {
                                 $_POST,
                                 array(
                                     $this->oFactory->oProp->sOptionKey,
-                                    'generator', // section id
+                                    'compiler', // section id
                                     'class_prefix' // field id
                                 ),
                                 ''
@@ -404,24 +404,15 @@ class AdminPageFrameworkLoader_AdminPage_Tool_Compiler_CustomFieldTypes {
              * @callback    add_filter() admin_page_framework_loader_filter_generator_additional_source_directories
              */
             public function replyToSetAdditionalDirectoriesForGenerator( $aDirPaths ) {
-
-                $_aCheckedCustomFieldTypes = $this->___getSelectedCustomFieldTypes(
-                    $this->aCustomFieldTypes
-                );
-
-                $_aDirPathInfo = array();
+                $_aCheckedCustomFieldTypes = $this->___getSelectedCustomFieldTypes( $this->aCustomFieldTypes );
+                $_aDirPathInfo             = array();
                 foreach( $_aCheckedCustomFieldTypes as $_sKey => $_aCheckedCustomFieldType ) {
-
                     $_sArchiveDirPath = $this->oFactory->oUtil->getElement( $_aCheckedCustomFieldType, 'archive_dir_path' );
                     $_sSourceDirPath  = $this->oFactory->oUtil->getElement( $_aCheckedCustomFieldType, 'directory_path' );
                     $_aDirPathInfo[ $_sArchiveDirPath ] = $_sSourceDirPath;
-
                 }
-
                 return $aDirPaths + $_aDirPathInfo;
-
             }
-
                 /**
                  * @return array The array keys of the checked items.
                  * @since  3.6.0
@@ -433,7 +424,7 @@ class AdminPageFrameworkLoader_AdminPage_Tool_Compiler_CustomFieldTypes {
                             $_POST,
                             array(
                                 $this->oFactory->oProp->sOptionKey,
-                                'generator', // section id
+                                'compiler', // section id
                                 'custom_field_types' // field id
                             ),
                             array()