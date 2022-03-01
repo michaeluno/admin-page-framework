feaf2cd17fdd09ab9edfc0b61e964455cdadd3cb
Michael Uno <michael2@michaeluno.jp>
2022-03-01 21:02:25 +0900

    Chore: Update the copy right year

--- development/utility/toc/AdminPageFramework_TableOfContents.php
+++ development/utility/toc/AdminPageFramework_TableOfContents.php
@@ -3,7 +3,7 @@
  * Generates an HTML Table of Contents block.
  *
  * @package      Admin Page Framework
- * @copyright    Copyright (c) 2013-2021, Michael Uno
+ * @copyright    Copyright (c) 2013-2022, Michael Uno
  * @author       Michael Uno
  * @authorurl    http://michaeluno.jp
  */
